fa94fdab49f365b263b6551074957bb407aad533
midrovom <midrovo@callphoneecuador.com>
2024-08-12 10:31:21 -0500

    fix-add: añadiendo modulo pos receipt add fields test

---
+++ pos_receipt_add_fields_test/models/res_config_settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models, api
+
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    invoice_number_fact = fields.Boolean()
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        set_param = self.env['ir.config_parameter'].sudo().set_param
+        set_param('res.config.settings.invoice_number_fact', int(self.invoice_number_fact))
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        
+        res['invoice_number_fact'] = int(
+            get_param('res.config.settings.invoice_number_fact'))
+        return res
---
+++ pos_receipt_add_fields_test/static/src/js/payment_field.js
@@ -0,0 +1,40 @@
+odoo.define('pos_receipt_add_fields.PaymentScreen', function (require) {
+   'use strict';
+   var rpc = require('web.rpc')
+   const PaymentScreen = require('point_of_sale.PaymentScreen');
+   const Registries = require('point_of_sale.Registries');
+   const { onMounted } = owl;
+
+   const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
+         setup() {
+            super.setup();
+         }
+         async validateOrder(isForceValidate) {
+            let receipt_number = this.env.pos.selectedOrder.name;
+            var orders = this.env.pos.selectedOrder
+            const receipt_order = await super.validateOrder(...arguments);
+            var self = this;
+            rpc.query({
+               model: 'pos.order',
+               method: 'get_invoice_field',
+               args: [receipt_number]
+               }).then(function(result){
+                  console.log('data field 1');
+                  /* console.log(result) */
+                  if (result.invoice_name) {
+                     self.env.pos.invoice  = result.invoice_name
+                     self.env.pos.invoice_xml_key  = result.xml_key
+                     /* console.log('data');
+                     console.log(result.invoice_name)
+                     console.log(result.xml_key) */
+                  }
+               });
+               return receipt_order
+         }
+      }
+
+      Registries.Component.extend(PaymentScreen, PosPaymentReceiptExtend);
+
+      return PaymentScreen;
+   });
+
