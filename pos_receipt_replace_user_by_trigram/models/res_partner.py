4d91858f7e26f78ef64404fc3a795e012b24c598
midrovom <midrovo@callphoneecuador.com>
2024-08-12 12:35:44 -0500

    fix-update: actualizando modulo Res partner

--- pos_receipt_replace_user_by_trigram/models/res_partner.py
+++ pos_receipt_replace_user_by_trigram/models/res_partner.py
@@ -4,9 +4,6 @@
 from odoo import api, fields, models
 
 from .. import utils
-import logging
-_logger = logging.getLogger(__name__)
-
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
@@ -16,7 +13,4 @@ class ResPartner(models.Model):
     @api.depends("firstname", "lastname")
     def _compute_pos_trigram(self):
         for partner in self:
-            name = partner.firstname
-            lastname = partner.lastname
-            _logger.info(f'NAME >> { name } || LASTNAME >> { lastname }')
             partner.pos_trigram = utils.get_trigram(partner.firstname, partner.lastname)
