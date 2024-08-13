f39bebffca958d340a345f4379339d7055167bf6
midrovom <midrovo@callphoneecuador.com>
2024-08-13 11:12:33 -0500

    fix-update: agregando payload del payment fields para el modulo pos cashier

--- pos_receipt_add_fields_prueba/__manifest__.py
+++ pos_receipt_add_fields_prueba/__manifest__.py
@@ -15,7 +15,7 @@
     'assets': {
         'point_of_sale.assets': [
             'pos_receipt_add_fields_prueba/static/src/js/payment_field.js',
-            'pos_receipt_add_fields_prueba/static/src/xml/factura.xml',
+            # 'pos_receipt_add_fields_prueba/static/src/xml/factura.xml',
         ]
     },
     'license': 'LGPL-3',
