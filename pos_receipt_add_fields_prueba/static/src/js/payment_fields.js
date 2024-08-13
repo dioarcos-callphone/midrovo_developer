394b23462a7b88d3862900ab29ae6da8303cd811
midrovom <midrovo@callphoneecuador.com>
2024-08-13 09:37:29 -0500

    fix-update: agregando payload del payment fields para el modulo pos cashier

--- pos_receipt_add_fields_prueba/static/src/js/payment_fields.js
+++ pos_receipt_add_fields_prueba/static/src/js/payment_fields.js
@@ -1,31 +1,40 @@
-const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
-    setup() {
-       super.setup();
-    }
-    async validateOrder(isForceValidate) {
-       let receipt_number = this.env.pos.selectedOrder.name;
-       var orders = this.env.pos.selectedOrder
-       const receipt_order = await super.validateOrder(...arguments);
-       var self = this;
-       rpc.query({
-          model: 'pos.order',
-          method: 'get_invoice_field',
-          args: [receipt_number]
-          }).then(function(result){
-             console.log('data field 1');
-             /* console.log(result) */
-             if (result.invoice_name) {
-                console.log(`Mostrando el Cashier Name >>> ${result.cashier_name}`)
-                self.env.pos.user_id = result.cashier_name;
-                /* console.log('data');
-                console.log(result.invoice_name)
-                console.log(result.xml_key) */
-             }
-          });
-          return receipt_order
-    }
- }
+odoo.define('pos_receipt_add_fields_prueba.PaymentScreen', function (require) {
+    'use strict';
+    var rpc = require('web.rpc')
+    const PaymentScreen = require('point_of_sale.PaymentScreen');
+    const Registries = require('point_of_sale.Registries');
+    const { onMounted } = owl;
 
- Registries.Component.extend(PaymentScreen, PosPaymentReceiptExtend);
+    const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
+        setup() {
+        super.setup();
+        }
+        async validateOrder(isForceValidate) {
+        let receipt_number = this.env.pos.selectedOrder.name;
+        var orders = this.env.pos.selectedOrder
+        const receipt_order = await super.validateOrder(...arguments);
+        var self = this;
+        rpc.query({
+            model: 'pos.order',
+            method: 'get_invoice_field',
+            args: [receipt_number]
+            }).then(function(result){
+                console.log('data field 1');
+                /* console.log(result) */
+                if (result.invoice_name) {
+                    console.log(`Mostrando el Cashier Name >>> ${result.cashier_name}`)
+                    self.env.pos.user_id = result.cashier_name;
+                    /* console.log('data');
+                    console.log(result.invoice_name)
+                    console.log(result.xml_key) */
+                }
+            });
+            return receipt_order
+        }
+    }
 
- return PaymentScreen;
+    Registries.Component.extend(PaymentScreen, PosPaymentReceiptExtend);
+
+    return PaymentScreen;
+
+});
