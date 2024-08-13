d6848041cd23d3d1ef5e13380681a16528569a6c
midrovom <midrovo@callphoneecuador.com>
2024-08-13 09:59:14 -0500

    fix-update: agregando payload del payment fields para el modulo pos cashier

--- pos_receipt_add_fields_prueba/static/src/js/payment_field.js
+++ pos_receipt_add_fields_prueba/static/src/js/payment_field.js
@@ -22,7 +22,10 @@ odoo.define('pos_receipt_add_fields_prueba.PaymentScreen', function (require) {
                 console.log('Ingresa aqui >>>>');
                 /* console.log(result) */
                 if (result.invoice_name) {
-                    console.log(`Mostrando el Cashier Name >>> ${result.cashier_name}`)
+                    const pos = self.env.pos
+                    console.log(`MOSTRANDO EL POST >>> ${pos}`)
+                    console.log(`MOSTRANDO EL RESULT >>> ${result}`)
+                    console.log(`MOSTRANDO EL CASHIER NAME >>> ${result.cashier_name}`)
                     self.env.pos.user_id.name = result.cashier_name;
                     /* console.log('data');
                     console.log(result.invoice_name)
