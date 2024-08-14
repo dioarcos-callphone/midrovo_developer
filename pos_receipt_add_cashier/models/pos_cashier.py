4e4e94ab53b808beafe6b1221fe1f8c42ba2de0b
midrovom <midrovo@callphoneecuador.com>
2024-08-14 11:05:37 -0500

    fix-update: agregando modulo pos_receipt_add_cashier

--- pos_receipt_add_cashier/models/pos_cashier.py
+++ pos_receipt_add_cashier/models/pos_cashier.py
@@ -14,7 +14,7 @@ class PosCashier(models.Model):
         
         res = super(PosCashier, self).get_invoice_field(id)
         
-        pos_id.account_move._l10n_ec_get_invoice_additional_info()['Vendedor'] = cashier_name
+        # pos_id.account_move._l10n_ec_get_invoice_additional_info()['Vendedor'] = cashier_name
         # additional_info = pos_id.account_move._l10n_ec_get_invoice_additional_info()
         
         # _logger.info(f'INFORMACION ADICIONAL >>> { additional_info }')
