d50452da3c79c542322889b760b1885088fccb56
midrovom <midrovo@callphoneecuador.com>
2024-08-14 11:46:28 -0500

    fix-update: agregando modulo pos_receipt_add_cashier

--- pos_receipt_add_cashier/models/pos_cashier.py
+++ pos_receipt_add_cashier/models/pos_cashier.py
@@ -1,7 +1,4 @@
 from odoo import models, fields, api
-import logging 
-
-_logger = logging.getLogger(__name__)
 
 class PosCashier(models.Model):
     _inherit = 'pos.order'
@@ -13,11 +10,6 @@ class PosCashier(models.Model):
         cashier_name = pos_id.cashier
         
         res = super(PosCashier, self).get_invoice_field(id)
-        
-        # pos_id.account_move._l10n_ec_get_invoice_additional_info()['Vendedor'] = cashier_name
-        # additional_info = pos_id.account_move._l10n_ec_get_invoice_additional_info()
-        
-        # _logger.info(f'INFORMACION ADICIONAL >>> { additional_info }')
         
         res.update({
             'cashier_name': cashier_name,
