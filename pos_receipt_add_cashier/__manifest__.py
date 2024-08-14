5cc537a96ffb8a65c489ca78bf350680ade6ce7d
midrovom <midrovo@callphoneecuador.com>
2024-08-14 09:38:53 -0500

    fix-update: agregando modulo pos_receipt_add_cashier

---
+++ pos_receipt_add_cashier/__manifest__.py
@@ -0,0 +1,24 @@
+
+{
+    'name': "POS Receipt Add Cashier",
+    "description": """Agrega nombre del cajero logueado a la factura""",
+    "summary": "Agrega nombre del cajero",
+    "category": "Point of Sale",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone sa',
+    'website': "https://www.callphoneecuador.com",
+    'depends': ['pos_receipt_add_fields', 'point_of_sale', 'sale', 'account'],
+    'data': [
+        # 'views/report/factura.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_receipt_add_fields_prueba/static/src/js/payment_field.js',
+        ]
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ pos_receipt_add_cashier/models/pos_cashier.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+import logging 
+
+# _logger = logging.getLogger(__name__)
+
+class PosCashier(models.Model):
+    _inherit = 'pos.order'
+    
+    @api.model
+    def get_invoice_field(self, id):
+        pos_id = self.search([('pos_reference', '=', id)])
+        invoice_id = self.env['account.move'].search(
+            [('ref', '=', pos_id.name)])
+        
+        cashier_name = pos_id.cashier
+        invoice_id.write({'invoice_user_id': cashier_name})
+        
+        res = super(PosCashier, self).get_invoice_field(id)
+        
+        # _logger.info(f'NAME CASHIER >>> {cashier_name}')
+        
+        res.update({
+            'cashier_name': cashier_name,
+        })
+
+        return res
