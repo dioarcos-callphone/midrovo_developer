a174d8a4dfa762b35a6226fcacfff1dbe5a03b88
midrovom <midrovo@callphoneecuador.com>
2024-08-14 12:33:21 -0500

    fix-update: agregando modulo pos_receipt_add_cashier

--- pos_receipt_add_cashier/static/src/js/payment_field.js
+++ pos_receipt_add_cashier/static/src/js/payment_field.js
@@ -1,34 +1,33 @@
-odoo.define('pos_receipt_add_fields_prueba.PaymentScreen', function (require) {
-    'use strict';
-    var rpc = require('web.rpc')
-    const PaymentScreen = require('point_of_sale.PaymentScreen');
-    const Registries = require('point_of_sale.Registries');
-    const { onMounted } = owl;
- 
-    const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
-          setup() {
-             super.setup();
-          }
-          async validateOrder(isForceValidate) {
-             let receipt_number = this.env.pos.selectedOrder.name;
-             var orders = this.env.pos.selectedOrder
-             const receipt_order = await super.validateOrder(...arguments);
-             var self = this;
-             rpc.query({
-                model: 'pos.order',
-                method: 'get_invoice_field',
-                args: [receipt_number]
-                }).then(function(result){
-                   if (result.invoice_name) {
-                      self.env.pos.cashier  = result.cashier
-                   }
-                });
-                return receipt_order
-          }
-       }
- 
-       Registries.Component.extend(PaymentScreen, PosPaymentReceiptExtend);
- 
-       return PaymentScreen;
-});
- 
+odoo.define('pos_receipt_add_cashier.PaymentScreen', function (require) {
+   'use strict';
+   var rpc = require('web.rpc')
+   const PaymentScreen = require('point_of_sale.PaymentScreen');
+   const Registries = require('point_of_sale.Registries');
+   const { onMounted } = owl;
+
+   const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
+         setup() {
+            super.setup();
+         }
+         async validateOrder(isForceValidate) {
+            let receipt_number = this.env.pos.selectedOrder.name;
+            var orders = this.env.pos.selectedOrder
+            const receipt_order = await super.validateOrder(...arguments);
+            var self = this;
+            rpc.query({
+               model: 'pos.order',
+               method: 'get_invoice_field',
+               args: [receipt_number]
+               }).then(function(result){
+                  if (result.invoice_name) {
+                     self.env.pos.cashier  = result.cashier_name
+                  }
+               });
+               return receipt_order
+         }
+      }
+
+      Registries.Component.extend(PaymentScreen, PosPaymentReceiptExtend);
+
+      return PaymentScreen;
+   });
