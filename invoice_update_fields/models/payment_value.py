c15cec8846a9acd9f9964f1100c4e2df1cd2da19
midrovom <midrovo@callphoneecuador.com>
2024-08-16 17:35:15 -0500

    fix-update: actualizando la vista.

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -1,18 +1,18 @@
-# from odoo import models, fields, api
-# import logging
-# _logger = logging.getLogger(__name__)
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
-# class PaymentValue(models.Model):
-#     _inherit = 'account_move'
+class PaymentValue(models.Model):
+    _inherit = 'account_move'
     
-#     @api.model
-#     def _l10n_ec_get_payment_data(self):
-#         payment_data = super(PaymentValue, self)._l10n_ec_get_payment_data()
+    @api.model
+    def _l10n_ec_get_payment_data(self):
+        payment_data = super(PaymentValue, self)._l10n_ec_get_payment_data()
 
-#         for payment in payment_data:
-#             payment_name = payment['payment_name']
-#             payment_total = payment['payment_total']
-#             _logger.info(f'PAYMENT NAME >>> { payment_name }')
-#             _logger.info(f'PAYMENT TOTAL >>> { payment_total }')
+        for payment in payment_data:
+            payment_name = payment['payment_name']
+            payment_total = payment['payment_total']
+            _logger.info(f'PAYMENT NAME >>> { payment_name }')
+            _logger.info(f'PAYMENT TOTAL >>> { payment_total }')
 
-#         return payment_data
+        return payment_data
