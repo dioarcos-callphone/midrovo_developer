8c61c8b703cb35c23dcb9e9093a60fb0a2c4c9f3
midrovom <midrovo@callphoneecuador.com>
2024-08-19 17:39:37 -0500

    fix-update: se actualiza modelo y manifest.

--- invoice_update_fields/__manifest__.py
+++ invoice_update_fields/__manifest__.py
@@ -12,7 +12,7 @@
     'author': 'Mauricio Idrovo',
     'company': 'Callphone sa',
     'website': "https://www.callphoneecuador.com",
-    'depends': ['pos_receipt_add_fields', 'point_of_sale', 'sale', 'account',],
+    'depends': ['pos_receipt_add_fields', 'point_of_sale', 'sale', 'account', 'l10n_ec_edi'],
     'data': [
         'views/invoice_update.xml'
     ],
---
+++ invoice_update_fields/models/payment_value.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+class PaymentValue(models.Model):
+    _inherit = 'account.move'
+    
+    def _l10n_ec_get_payment_data(self):
+        payment_data = super(PaymentValue, self)._l10n_ec_get_payment_data()
+        
+        for payment in payment_data:
+            payment_total = payment['payment_total']
+            _logger.info(f'MOSTRANDO EL PAYMENTO TOTAL >>> { payment_total }')
+
+        return payment_data
