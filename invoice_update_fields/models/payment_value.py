461b54598793ffa29fe3bc347d8fb220b1523526
midrovom <midrovo@callphoneecuador.com>
2024-08-20 09:15:18 -0500

    fix-update: actualizando payment value

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -9,11 +9,18 @@ class PaymentValue(models.Model):
     def _l10n_ec_get_payment_data(self):
         payment_data = super(PaymentValue, self)._l10n_ec_get_payment_data()
         
-        # payment_total = payment_data['payment_total']
-        _logger.info(f'MOSTRANDO EL PAYMENTO TOTAL >>> { payment_data }')
+        payment_vals = {
+                'payment_code': self.line.l10n_ec_sri_payment_id.code,
+                'payment_total': self.line.payment_valor,
+                'payment_name':self.l10n_ec_sri_payment_id.name,
+        }
+        
+        payment_data.append(payment_vals)
         
         # for payment in payment_data:
         #     payment_total = payment['payment_total']
         #     _logger.info(f'MOSTRANDO EL PAYMENTO TOTAL >>> { payment_total }')
+        
+        _logger.info(f'MOSTRANDO EL PAYMENTO TOTAL >>> { payment_data }')
 
         return payment_data
