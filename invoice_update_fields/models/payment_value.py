ac4e34c44801fcab18f0e8e2fd1248efba289e9c
midrovom <midrovo@callphoneecuador.com>
2024-08-21 16:15:19 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -36,9 +36,9 @@ class PaymentValue(models.Model):
                 
         for line in pay_term_line_ids:
             payment_vals = {
-                    'payment_code': line.l10_ec_sri_payment_id.code,
-                    'payment_total': line.payment_valor,
-                    'payment_name': line.l10n_ec_sri_payment_id.name
+                'payment_code': line.l10n_ec_sri_payment_id.code,
+                'payment_total': line.payment_valor,
+                'payment_name':line.l10n_ec_sri_payment_id.name,
             }
         
             payment_data.append(payment_vals)
@@ -47,43 +47,43 @@ class PaymentValue(models.Model):
 
         return payment_data
     
-class InheritAccountMoveSriLines(models.Model):
-    _inherit = 'account.move.sri.lines'
+# class InheritAccountMoveSriLines(models.Model):
+#     _inherit = 'account.move.sri.lines'
 
-    def _get_default_forma_pago(self):
-        return self.env['l10n_ec.sri.payment'].search([('code', '=', '16')])
+#     def _get_default_forma_pago(self):
+#         return self.env['l10n_ec.sri.payment'].search([('code', '=', '16')])
     
 
-    l10n_ec_sri_payment_id = fields.Many2one(
-        comodel_name="l10n_ec.sri.payment",
-        string="Payment Method (SRI)",
-        required=True, 
-        ondelete='cascade', 
-        index=True
-    )
+#     l10n_ec_sri_payment_id = fields.Many2one(
+#         comodel_name="l10n_ec.sri.payment",
+#         string="Payment Method (SRI)",
+#         required=True, 
+#         ondelete='cascade', 
+#         index=True
+#     )
     
-    #parameter to One2many, 
-    move_id = fields.Many2one(  "account.move", 
-                                string="Account_key",
-                                required=True,
-                                readonly=True,
-                                index=True,
-                                auto_join=True,
-                                ondelete="cascade",
-                                check_company=True,
-                            )
+#     #parameter to One2many, 
+#     move_id = fields.Many2one(  "account.move", 
+#                                 string="Account_key",
+#                                 required=True,
+#                                 readonly=True,
+#                                 index=True,
+#                                 auto_join=True,
+#                                 ondelete="cascade",
+#                                 check_company=True,
+#                             )
     
-    payment_valor = fields.Float( string="Price value",
-                                    compute='_compute_payment_valor', store=True, readonly=False, precompute=True,
-                                    digits='Payment value',)
+#     payment_valor = fields.Float( string="Price value",
+#                                     compute='_compute_payment_valor', store=True, readonly=False, precompute=True,
+#                                     digits='Payment value',)
     
-    @api.model
-    @api.depends("payment_valor","move_id")
-    def _compute_payment_valor(self):
-        value = 0.00
-        for line in self:
-            if ( line.move_id[0]):
-                invoices = self.env["account.move"].browse([line.move_id[0].id])
-                _logger.info(f'MOSTRANDO INVOICES >>> { invoices }')
-                value = invoices._get_default_payment_valor()
-                line.payment_valor = value
+#     @api.model
+#     @api.depends("payment_valor","move_id")
+#     def _compute_payment_valor(self):
+#         value = 0.00
+#         for line in self:
+#             if ( line.move_id[0]):
+#                 invoices = self.env["account.move"].browse([line.move_id[0].id])
+#                 _logger.info(f'MOSTRANDO INVOICES >>> { invoices }')
+#                 value = invoices._get_default_payment_valor()
+#                 line.payment_valor = value
