815e5c27c90968f5efab9e86276c094cb5afb654
midrovom <midrovo@callphoneecuador.com>
2024-08-21 15:11:47 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -46,3 +46,43 @@ class PaymentValue(models.Model):
         _logger.info(f'MOSTRANDO EL PAYMENT DATA 1 >>> { payment_data }')
 
         return payment_data
+    
+class InheritAccountMoveSriLines(models.Model):
+    _name = 'account.move.sri.lines'
+
+    def _get_default_forma_pago(self):
+        return self.env['l10n_ec.sri.payment'].search([('code', '=', '01')])
+    
+
+    l10n_ec_sri_payment_id = fields.Many2one(
+        comodel_name="l10n_ec.sri.payment",
+        string="Payment Method (SRI)",
+        required=True, 
+        ondelete='cascade', 
+        index=True
+    )
+    
+    #parameter to One2many, 
+    move_id = fields.Many2one(  "account.move", 
+                                string="Account_key",
+                                required=True,
+                                readonly=True,
+                                index=True,
+                                auto_join=True,
+                                ondelete="cascade",
+                                check_company=True,
+                            )
+    
+    payment_valor = fields.Float( string="Price value",
+                                    compute='_compute_payment_valor', store=True, readonly=False, precompute=True,
+                                    digits='Payment value',)
+    
+    @api.depends("payment_valor","move_id")
+    def _compute_payment_valor(self):
+        value = 0.00
+        for line in self:
+            if ( line.move_id[0]):
+                invoices = self.env["account.move"].browse([line.move_id[0].id])
+                _logger.info(f'MOSTRANDO INVOICES >>> { invoices }')
+                value = invoices._get_default_payment_valor()
+                line.payment_valor = value
