1f21306400ff4089ec50bce492666e6ce077b424
midrovom <midrovo@callphoneecuador.com>
2024-08-21 14:48:34 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -37,7 +37,7 @@ class PaymentValue(models.Model):
         for line in pay_term_line_ids:
             payment_vals = {
                     'payment_code': 16,
-                    'payment_total': abs(line.balance),
+                    'payment_total': line.payment_valor,
             }
         
             payment_data.append(payment_vals)
