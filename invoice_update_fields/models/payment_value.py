16487ab4faae934b08edd9d109f3e05d7e441dad
midrovom <midrovo@callphoneecuador.com>
2024-08-22 16:33:11 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -49,6 +49,7 @@ class PosOrder(models.Model):
         }
         
     def _l10n_ec_get_payment_data(self, pos_reference):
+        _logger.info(f'OBTENIENDO DE LA VISTA >>> { pos_reference }')
         payment_data = []
         pos_id = self.search([('pos_reference', '=', pos_reference)])
         invoice_id = self.env['account.move'].search(
