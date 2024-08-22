f2278ad314ed8a9663511b475160bece7e838064
midrovom <midrovo@callphoneecuador.com>
2024-08-22 14:17:55 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -58,10 +58,9 @@ class AccountMoveSriLines(models.Model):
     
     @api.depends("payment_valor","move_id")
     def _compute_payment_valor(self):
-        value = 20.00
+        value = 0.00
         for line in self:
             if ( line.move_id[0]):
                 invoices = self.env["account.move"].browse([line.move_id[0].id])
                 value = invoices._get_default_payment_valor()
-                value = 20.00
                 line.payment_valor = value
