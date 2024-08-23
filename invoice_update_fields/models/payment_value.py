0f7afb8c8dc5c4c024f0bcdbd287efd1b5e02b15
midrovom <midrovo@callphoneecuador.com>
2024-08-23 18:00:42 -0500

    fix-update: actualizando modulo

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -37,7 +37,7 @@ class PaymentValue(models.Model):
             _logger.error("Ocurrió un error: %s", str(e))
     
     @api.model
-    def _l10n_ec_get_payment_data(self):
+    def _l10n_ec_get_payment_data_inherit(self):
         cr = self.env.cr
         payment_data = []
         
