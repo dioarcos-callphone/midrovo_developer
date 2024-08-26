9c97756ed852755cf15c4d5cfd4068db6788801e
midrovom <midrovo@callphoneecuador.com>
2024-08-26 12:54:06 -0500

    fix-update: actualizando la vista

--- invoice_update_fields/models/payment_value.py
+++ invoice_update_fields/models/payment_value.py
@@ -62,7 +62,7 @@ class PaymentValue(models.Model):
         
         data.clear()
         
-        payment_contable = super(PaymentValue, self)._l10_ec_gt_payment_data()
+        payment_contable = self._l10_ec_gt_payment_data()
         
         _logger.info(f'SE OBTIENE EL PAYMENT CONTABLE >>> { payment_contable }')
         
