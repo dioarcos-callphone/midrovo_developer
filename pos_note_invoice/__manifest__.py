d754f5620c3159ea6b37e679924cfc4359e70c2a
midrovom <midrovo@callphoneecuador.com>
2024-08-29 14:37:44 -0500

    fix-update: se agrega modulo pos_note_invoice

---
+++ pos_note_invoice/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Pos Note Invoice",
+    
+    "summary": "Visualización general del comentario o nota de la factura",
+    "description": """
+        Se añade nota general para que pueda ser visualizada en la factura
+    """,
+    
+    "category": "Point of Sale",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone sa',
+    'website': "https://www.callphoneecuador.com",
+    'depends': ['point_of_sale',],
+    'data': [],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
