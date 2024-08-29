3fe1f7932d072a6ef8ffe656bd967f864974683c
midrovom <midrovo@callphoneecuador.com>
2024-08-29 10:26:46 -0500

    fix-update: actualizando modulo javascript customer_validation desactivar input vat

--- pos_customer_validated/static/src/js/customer_validation.js
+++ pos_customer_validated/static/src/js/customer_validation.js
@@ -8,6 +8,12 @@ odoo.define('pos_customer_validated.customer_validation', (require) => {
     const PartnerListScreenExtend = PartnerListScreen => class extends PartnerListScreen {
         setup() {
             super.setup();
+            const partner = this.props.partner;
+
+            if(partner.vat) {
+                const vatInput = document.querySelector('input.detail.vat[name="vat"]');
+                vatInput.disabled = true;
+            }
         }
 
         async saveChanges(event) {
@@ -16,6 +22,10 @@ odoo.define('pos_customer_validated.customer_validation', (require) => {
                 method: "create_from_ui",
                 args: [event.detail.processedChanges],
             });
+
+            id = event.detail.processedChanges.id
+
+            console.log(`OBTENIENDO ID DEL FRONT >>> ${ id }`)
 
             await this.env.pos._loadPartners([partnerId]);
             this.state.selectedPartner = this.env.pos.db.get_partner_by_id(partnerId);
