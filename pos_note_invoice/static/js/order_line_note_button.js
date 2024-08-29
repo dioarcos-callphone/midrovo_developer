a1b0eda4ed761b3268478303de25e7bb899da90d
midrovom <midrovo@callphoneecuador.com>
2024-08-29 16:03:06 -0500

    fix-update: se actualiza javascript funcion onclick

---
+++ pos_note_invoice/static/js/order_line_note_button.js
@@ -0,0 +1,29 @@
+odoo.define('pos_note_invoice.order_line_note_button', (require) => {
+    "use strict";
+
+    const OrderlineCustomerNoteButton = require('point_of_sale.OrderlineCustomerNoteButton');
+    const Registries = require('point_of_sale.Registries');
+
+    const OrderlineCustomerNoteButtonExtend = OrderlineCustomerNoteButton => class extends OrderlineCustomerNoteButton {
+        setup() {
+            super.setup()
+        }
+
+        async onClick() {    
+            const { confirmed, payload: inputNote } = await this.showPopup("TextAreaPopup", {
+                startingValue: selectedOrderline.get_customer_note(),
+                title: this.env._t("Añadir Nota o Comentario"),
+            });
+    
+            if (confirmed) {
+                selectedOrderline.set_customer_note(inputNote);
+            }
+        }
+
+    }
+
+    Registries.Component.extend(OrderlineCustomerNoteButton, OrderlineCustomerNoteButtonExtend);
+
+    return OrderlineCustomerNoteButton;
+
+});
