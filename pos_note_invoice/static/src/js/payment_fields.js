d04685e868897c10c6f0624cc6b71eb2c8ac9b61
midrovom <midrovo@callphoneecuador.com>
2024-08-30 12:42:32 -0500

    fix-update: se actualiza javascript useBus

--- pos_note_invoice/static/src/js/payment_fields.js
+++ pos_note_invoice/static/src/js/payment_fields.js
@@ -9,7 +9,7 @@ odoo.define('pos_note_invoice.payment_fields', function (require) {
         setup() {
             super.setup();
             this.bus = useBus();  // Configura el bus
-            this.bus.on('inputNote:updated', this._onInputNoteUpdated.bind(this));
+            this.bus.on('inputNote:updated', this._onInputNoteUpdated);
                 
         }
 
