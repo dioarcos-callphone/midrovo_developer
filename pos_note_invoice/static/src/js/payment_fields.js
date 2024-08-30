c6468bebd7a07af2efec576d76dc3fe117ae4aac
midrovom <midrovo@callphoneecuador.com>
2024-08-30 10:37:06 -0500

    fix-update: se actualiza javascript eventBus

--- pos_note_invoice/static/src/js/payment_fields.js
+++ pos_note_invoice/static/src/js/payment_fields.js
@@ -4,7 +4,7 @@ odoo.define('pos_note_invoice.payment_fields', function (require) {
     const PaymentScreen = require('point_of_sale.PaymentScreen');
     const Registries = require('point_of_sale.Registries');
     const { onMounted } = owl;
-    const { useBus } = require('@web/core/utils/hooks'); 
+    const { useBus } = require('web.core.utils.hooks'); 
  
     const PosPaymentReceiptExtend = PaymentScreen => class extends PaymentScreen {
         setup() {
