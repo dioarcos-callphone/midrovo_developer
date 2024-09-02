f5741ffa7349599c4a632894123ca37a2d21e811
midrovom <midrovo@callphoneecuador.com>
2024-09-02 15:22:13 -0500

    fix-update: se actualiza javascript useBus

--- pos_note_invoice/models/invoice_update.py
+++ pos_note_invoice/models/invoice_update.py
@@ -3,20 +3,10 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class InvoiceUpdate(models.Model):
-    _inherit = 'pos.order'
+    _inherit = 'account.move'
     
     @api.model
-    def get_invoice_field(self, id):
-        pos_id = self.search([('pos_reference', '=', id)])
-        invoice_id = self.env['account.move'].search(
-            [('ref', '=', pos_id.name)])
+    def create(self, vals):
+        vals['narration'] = 'ACTUALIZANDO NOTA DE POS'
         
-        invoice_id.write({ 'narration': 'ACTUALIZANDO NOTA DEL INVOICE' })
-        _logger.info('________ | INVOICES >>> %s' % invoice_id)
-
-        return {
-            'invoice_id': invoice_id.id,
-            'invoice_name': invoice_id.name,
-            'invoice_number': invoice_id.l10n_latam_document_number,
-            'xml_key': invoice_id.l10n_ec_authorization_number,
-        }
+        return super(InvoiceUpdate, self).vals(vals)
