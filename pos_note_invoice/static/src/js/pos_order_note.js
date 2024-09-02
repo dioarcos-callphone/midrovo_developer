7f2f516e48ca2c6744b273619fbac82d54276565
midrovom <midrovo@callphoneecuador.com>
2024-09-02 18:01:38 -0500

    fix-update: se actualiza javascript eventBus

--- pos_note_invoice/static/src/js/pos_order_note.js
+++ pos_note_invoice/static/src/js/pos_order_note.js
@@ -3,21 +3,17 @@ odoo.define('pos_note_invoice.pos_order_note', function (require) {
 
     const models = require('point_of_sale.models');
 
-    // Extender el modelo Order
     const _super_Order = models.Order.prototype;
     models.Order = models.Order.extend({
-        // Definir el método set_note_context
         set_note_context: function(note) {
             this.note_context = note;
-            this.trigger('change', this); // Dispara un evento de cambio para actualizar la vista si es necesario.
+            this.trigger('change', this);
         },
 
-        // Método para obtener el contexto de la nota
         get_note_context: function() {
-            return this.note_context || ''; // Retorna la nota almacenada o una cadena vacía si no hay ninguna.
+            return this.note_context || '';
         },
 
-        // Sobrescribir el método export_as_JSON para incluir el contexto de la nota
         export_as_JSON: function() {
             const json = _super_Order.export_as_JSON.apply(this, arguments);
             json.note_context = this.get_note_context();
