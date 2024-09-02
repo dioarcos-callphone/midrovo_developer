72fed36dfef2ac922387c58372a4ea7dca89d2bc
midrovom <midrovo@callphoneecuador.com>
2024-09-02 15:06:04 -0500

    fix-update: se actualiza javascript useBus

--- pos_note_invoice/models/invoice_update.py
+++ pos_note_invoice/models/invoice_update.py
@@ -3,7 +3,7 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class InvoiceUpdate(models.Model):
-    _inherit = 'account.move'
+    _inherit = 'pos.order'
     
     @api.model
     def get_invoice_field(self, id):
