52a27d35ad89b618cd5b7c59b85d7c161b672da6
midrovom <midrovo@callphoneecuador.com>
2024-09-03 09:33:44 -0500

    fix-update: se actualiza javascript eventBus

--- pos_note_invoice/static/src/js/order_line_note_button.js
+++ pos_note_invoice/static/src/js/order_line_note_button.js
@@ -28,7 +28,7 @@ odoo.define('pos_note_invoice.order_line_note_button', (require) => {
         }
 
         noteInput(event) {
-            console.log(`MOSTRANDO EVENTO >>> ${ event }`)
+            console.log(`MOSTRANDO EVENTO >>> ${ event.note }`)
         }
 
     }
