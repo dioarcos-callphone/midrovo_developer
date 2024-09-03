bd3c9a120476978e1a87455d1710c37b5e3a2336
midrovom <midrovo@callphoneecuador.com>
2024-09-03 12:03:09 -0500

    fix-update: se actualiza payment field rpc

--- pos_note_invoice/models/invoice_update.py
+++ pos_note_invoice/models/invoice_update.py
@@ -5,28 +5,7 @@ _logger = logging.getLogger(__name__)
 class InvoiceUpdate(models.Model):
     _inherit = 'account.move'
     
-    def get_note(self, note):
+    @api.model
+    def get_note(self, nota):
+        _logger.info(f'OTENIENDO NOTA EN EL BACK >>> { nota }')
         return note
-    
-    # @api.model
-    # def _l10n_ec_get_payment_data(self):
-    #     note_context = self.env.context.get('note_context')
-        
-    #     _logger.info(f'Nota recibida del contexto: {note_context}')
-    #     pay_term_line_ids = self.line_ids.filtered(
-    #         lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')
-    #     )
-        
-    #     move_id = pay_term_line_ids.move_id.id
-        
-    #     account_move = self.search([('id', '=', move_id)])
-        
-    #     # account_move.write({ 'narration': 'ACTUALIZANDO NOTA' })
-        
-    #     _logger.info(f'OBTENIENDO EL PAY TERM >>> { pay_term_line_ids }')
-
-        
-    #     # _logger.info(f'SE OBTIENE EL PAYMENT DATA >>> { payment_data }')        
-    #     # _logger.info(f'SE OBTIENE EL PAYMENT CONTABLE >>> { payment_contable }')
-        
-    #     return super(InvoiceUpdate, self)._l10n_ec_get_payment_data()
