95df2ff31551c8ffb4eff627422c82ff71b6cf40
midrovom <midrovo@callphoneecuador.com>
2024-09-03 14:24:23 -0500

    fix-update: se actualiza payment field rpc

--- pos_note_invoice/static/src/js/pos_order_receipt.js
+++ pos_note_invoice/static/src/js/pos_order_receipt.js
@@ -8,9 +8,9 @@ odoo.define("pos_note_invoice.pos_order_receipt", function (require) {
 
     const PosGlobalStateExtend =  (PosGlobalState) => 
         class PosGlobalStateExtend extends PosGlobalState {
-            /* setup() {
+            setup() {
                 super.setup();
-            } */
+            }
 
             async _processData(loadedData) {
                 await super._processData(...arguments);
@@ -19,36 +19,6 @@ odoo.define("pos_note_invoice.pos_order_receipt", function (require) {
             
             async _flush_orders(orders, options) {
                 const nota = NoteService.getNote();
-
-                var self = this;
-                var result, data;
-                let session_orders = self.session_orders;
-                var length = session_orders.length-1
-                var order_session = session_orders[length]
-                /* Si el en el config esta activado */
-                result = data = super._flush_orders(...arguments);
-                //if ( order_session.l10n_ec_sri_payment_ids == true ) {
-                    _.each(orders,function(order){
-                        data.then(function(){
-                            let receipt_number = self.env.pos.selectedOrder.name;
-                            let sri_lines = self.env.pos.get_order().export_as_JSON().l10n_ec_sri_payment_ids;
-                            if(sri_lines) {
-                                rpc.query({ 
-                                    model: 'pos.order',
-                                    method: 'get_invoice',
-                                    args: [receipt_number]
-                                    }).then(function(result){
-                                        let invocice_name = result.pos_name;
-                                        rpc.query({
-                                            model: 'account.move',
-                                            method: 'update_account_move_sri_lines',
-                                            args: [invocice_name, sri_lines]
-                                        })
-                                    });
-                            }
-                        });
-                    });
-                //}
 
                 rpc.query({
                     model: 'account.move',
