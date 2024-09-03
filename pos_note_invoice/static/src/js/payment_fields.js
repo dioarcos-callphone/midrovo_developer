bd04c49dbc0ad3389cb9abf60d7782af0a992af6
midrovom <midrovo@callphoneecuador.com>
2024-09-03 16:38:32 -0500

    fix-update: se actualiza payment field rpc

--- pos_note_invoice/static/src/js/payment_fields.js
+++ pos_note_invoice/static/src/js/payment_fields.js
@@ -18,7 +18,7 @@ odoo.define('pos_note_invoice.payment_fields', function (require) {
             const POS = orders.orderlines[0]
             const { invoice } = POS.pos
 
-            console.log(`MOSTRANDO POS >>> ${ POS }`)
+            console.log(POS)
 
             console.log(invoice);
 
