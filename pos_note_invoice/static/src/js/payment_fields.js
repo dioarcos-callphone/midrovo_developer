38f4cbca2fdee632b45ce560b6fc77275246437e
midrovom <midrovo@callphoneecuador.com>
2024-09-03 15:00:38 -0500

    fix-update: se actualiza payment field rpc

--- pos_note_invoice/static/src/js/payment_fields.js
+++ pos_note_invoice/static/src/js/payment_fields.js
@@ -11,6 +11,24 @@ odoo.define('pos_note_invoice.payment_fields', function (require) {
             // this.mostrandoNote();
         }
 
+        async validateOrder(isForceValidate) {
+            let receipt_number = this.env.pos.selectedOrder.name;
+            var orders = this.env.pos.selectedOrder
+            const receipt_order = await super.validateOrder(...arguments);
+            var self = this;
+            rpc.query({
+               model: 'pos.order',
+               method: 'get_invoice_field',
+               args: [receipt_number]
+            }).then(function(result){
+                if (result.invoice_name) {
+                    self.env.pos.invoice  = result.invoice_name
+                    self.env.pos.invoice_xml_key  = result.xml_key
+                }
+            });
+               return receipt_order
+        }
+
         async _finalizeValidation() {
             const receipt_number = this.env.pos.selectedOrder.name;
             const argumentos = {
