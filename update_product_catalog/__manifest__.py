88025fb43416852152d150b919c24ad2b4c824ec
midrovom <midrovo@callphoneecuador.com>
2024-09-04 15:44:07 -0500

    fix-add: se agrega modulo update product catalog

---
+++ update_product_catalog/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Update Product Catalog",
+    
+    "summary": "Actualiación en el diseño del catalogo de productos.",
+    "description": """
+        Este modulo permite actualizar el diseño del catalogo de productos.
+    """,
+    
+    "category": "Sales",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone sa',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'amf_product_catalog', 'sale', ],
+    'data': [
+        'report/product_catalog_template.xml',    
+    ],
+    
+    # 'assets': {
+    #     'web.report_assets_common': [
+    #         'update_product_catalog/static/src/css/catalog_style.css',
+    #     ],
+    #     'web.report_assets_pdf': [
+    #         'update_product_catalog/static/src/css/catalog_style_pdf.css',
+    #     ],
+    # },
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
