71151011c24ce75f0e22b3e4ff9899b532013574
midrovom <midrovo@callphoneecuador.com>
2024-09-04 09:01:34 -0500

    fix-update: se actualiza pos global state

--- pos_note_invoice/models/invoice_update.py
+++ pos_note_invoice/models/invoice_update.py
@@ -5,48 +5,57 @@ _logger = logging.getLogger(__name__)
 nota_actual = ''
 
 class InvoiceUpdate(models.Model):
-    _inherit = 'account.move'
+    _inherit = 'pos.order'
     
     @api.model
-    def get_note(self, argumentos):
-        pay_term_line_ids = self.line_ids.filtered(
-            lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')
-        )
-        
-        _logger.info(f'PAY TERM >>> { pay_term_line_ids }')
-        
-        receipt_number = argumentos['receipt_number']
-        nota = argumentos['note']
-        
-        _logger.info(f'NUMBER RECEIPT >>> { receipt_number } || NOTA >>> { nota }')
-        
-        pos_id = self.env['pos.order'].search([('pos_reference', '=', receipt_number)], limit=1)
-        
-        invoice = self.search([('ref', '=', pos_id.name)], limit=1)
-        
-        _logger.info(f'MOSTRANDO INVOICE >>>> { invoice }')
-        
-        _logger.info(f'MOSTRANDO NARRATION >>>> { invoice.narration }')
-        
-        # invoice.write({ 'narration': nota })        
-        
-        _logger.info(f'MOSTRANDO ARGUMENTOS >>> { argumentos }')
-        # pay_term_line_ids = self.line_ids.filtered(
-        #     lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')
-        # )
-        
-        # move_id = pay_term_line_ids.move_id.id
-        
-        # _logger.info(f'OBTENIENDO MOVE ID >>> { move_id }')
-        
-        # invoice = self.search([('id', '=', move_id)])
-        
-        # _logger.info(f'OBTENIENDO INVOICE >>> { invoice }')
-        
-        # if invoice:
-        #     invoice.write({ 'narration': nota })
+    def get_note(self, args):
+        # invoice = super(InvoiceUpdate, self).create_from_ui(orders)
 
-        return argumentos
+        _logger.info(f'OBTENIENDO INVOICE >>> { args }')
+        # _logger.info(f'OBTENIENDO NOTA >>> { nota }')
+
+        return args
+    
+    # @api.model
+    # def get_note(self, argumentos):
+    #     pay_term_line_ids = self.line_ids.filtered(
+    #         lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')
+    #     )
+        
+    #     _logger.info(f'PAY TERM >>> { pay_term_line_ids }')
+        
+    #     receipt_number = argumentos['receipt_number']
+    #     nota = argumentos['note']
+        
+    #     _logger.info(f'NUMBER RECEIPT >>> { receipt_number } || NOTA >>> { nota }')
+        
+    #     pos_id = self.env['pos.order'].search([('pos_reference', '=', receipt_number)], limit=1)
+        
+    #     invoice = self.search([('ref', '=', pos_id.name)], limit=1)
+        
+    #     _logger.info(f'MOSTRANDO INVOICE >>>> { invoice }')
+        
+    #     _logger.info(f'MOSTRANDO NARRATION >>>> { invoice.narration }')
+        
+    #     # invoice.write({ 'narration': nota })        
+        
+    #     _logger.info(f'MOSTRANDO ARGUMENTOS >>> { argumentos }')
+    #     # pay_term_line_ids = self.line_ids.filtered(
+    #     #     lambda line: line.account_id.account_type in ('asset_receivable', 'liability_payable')
+    #     # )
+        
+    #     # move_id = pay_term_line_ids.move_id.id
+        
+    #     # _logger.info(f'OBTENIENDO MOVE ID >>> { move_id }')
+        
+    #     # invoice = self.search([('id', '=', move_id)])
+        
+    #     # _logger.info(f'OBTENIENDO INVOICE >>> { invoice }')
+        
+    #     # if invoice:
+    #     #     invoice.write({ 'narration': nota })
+
+    #     return argumentos
     
     # @api.model
     # def _l10n_ec_get_payment_data(self):
---
+++ pos_note_invoice/static/src/js/pos_global_state.js
@@ -0,0 +1,45 @@
+odoo.define("pos_note_invoice.pos_global_state", (require) => {
+    const PosGlobalState = require("point_of_sale.models");
+    const rpc = require('web.rpc');
+    const NoteService = require('pos_note_invoice.note_service');
+
+    const PosGlobalStateExtend = PosGlobalState => class extends PosGlobalState {
+        setup() {
+            super.setup()
+        }
+
+        _save_to_server(orders, options) {
+            if (!orders || !orders.length) {
+                return Promise.resolve([]);
+            }
+            this.set_synch("connecting", orders.length);
+            options = options || {};
+    
+            var args = [
+                _.map(orders, function (order) {
+                    order.to_invoice = options.to_invoice || false;
+                    return order;
+                }),
+            ];
+
+            const nota = NoteService.getNote();
+
+            args.push(options.draft || false);
+            args.push(nota);
+
+            rpc.query({
+                model: 'pos.order',
+                method: 'get_note',
+                args: args
+            }).then(function(result) {
+                console.log(`MOSTRANDO RESULT >>> ${ result }`)
+            });
+            
+
+            return super._save_to_server(orders, options);
+        }
+    }
+
+    Registries.Model.extend(PosGlobalState, PosGlobalStateExtend);
+
+});
