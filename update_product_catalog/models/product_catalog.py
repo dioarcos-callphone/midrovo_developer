926e5e370c1cd257c0c30353d9cd2d8722eec22f
midrovom <midrovo@callphoneecuador.com>
2024-09-05 15:28:19 -0500

    fix-add: se agrega modulo update product catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -1,16 +1,21 @@
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+
+
 
 class ProductCategory(models.Model):
     _inherit = "product.template"
     
     @api.model
     def _get_data_product_variants(self, product_template_id):
-        product_variants = self.env['product.product'].search([('id', '=', product_template_id)])
+        product_variants = self.env['product.product'].search([('product_tmpl_id', '=', product_template_id)])
         
         data = []
         
         for product_variant in product_variants:
             data.append(product_variant)
             
+        _logger.info(f'MOSTRANDO LA DATA >>> { data }')
             
         return data
