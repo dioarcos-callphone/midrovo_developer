fcfbb6bcbd2f08a1e2ffad10820ce0c862871a74
midrovom <midrovo@callphoneecuador.com>
2024-09-06 17:39:15 -0500

    fix-update: actualizando modulo product catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -19,7 +19,7 @@ class ProductCategory(models.Model):
             # for variant_value in variant_values:
             #     _logger.info(f'MOSTRANDO VARIANT VALUE >>> { variant_value.product_attribute_value_id.name }')
             
-            _logger.info(f'MOSTRANDO LA DATA >>> { product_variant.product_template_variant_value_ids }')
+            _logger.info(f'MOSTRANDO LA DATA >>> { product_variant.product_template_variant_value_ids.attribute_id }')
             
             data.append(product_variant)
                         
