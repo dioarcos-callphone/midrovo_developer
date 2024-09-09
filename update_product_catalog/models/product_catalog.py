a2a4b700e3bdb84d00e7ea5becf70b8ff0a8a8fd
midrovom <midrovo@callphoneecuador.com>
2024-09-09 12:56:59 -0500

    fix-update: actualizando modulo producto catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -12,9 +12,9 @@ class ProductCategory(models.Model):
         data = []
         product_variants = []
         colores = []
-        # product_variants = self.search([('atrribute_id', '=', product_template.attribute_id)],)
+
         product_product = self.env['product.product'].search([('product_tmpl_id', '=', product_template.id)])
-        
+                
         product_attributte_lines = self.env['product.template.attribute.line'].search([(
             'product_tmpl_id', '=', product_template.id
         )])
@@ -45,34 +45,5 @@ class ProductCategory(models.Model):
             }
             
             data.append(product_data)
-            
-        
-        
-        _logger.info(f'MOSTRANDO PRODUCT PRODUCT >>> { product_product }')
-        _logger.info(f'MOSTRANDO PRODUCT DATA >>> { data }')
-        
-        # product_attributte_lines = self.env['product.template.attribute.line'].search([(
-        #     'product_tmpl_id', '=', product_template.id
-        # )])
-        
-        # _logger.info(f'PRODUCTO TEMPLATE >>> { product_attributte_lines.value_ids }')
-        
-        # for product_line in product_attributte_lines:
-        #     color = product_line.attribute_id.name
-        #     if(color.lower() == 'color'):
-        #         for value in product_line.value_ids:
-        #             _logger.info(f'MOSTRANDO COLORES >>> { value.name }')
-        
-        # variantes = []
-        
-        # for product_variant in product_variants:
-        #     # variant_values = product_variant.product_template_variant_value_ids
-            
-        #     # for variant_value in variant_values:
-        #     #     _logger.info(f'MOSTRANDO VARIANT VALUE >>> { variant_value.product_attribute_value_id.name }')
-            
-        #     _logger.info(f'MOSTRANDO LA DATA >>> { product_variant.product_template_variant_value_ids.attribute_id }')
-            
-        #     variantes.append(product_variant.product_template_variant_value_ids.attribute_id)
                         
-        return 'variantes'
+        return data
