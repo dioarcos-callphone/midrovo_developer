4fdbcbe8855bb41a8267d6e68d8864936194e035
midrovom <midrovo@callphoneecuador.com>
2024-09-09 09:13:09 -0500

    fix-update: actualizando modulo producto catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -8,19 +8,21 @@ class ProductCategory(models.Model):
     _inherit = "product.template"
     
     @api.model
-    def _get_data_product_variants(self, product_template_id):
-        product_variants = self.env['product.product'].search([('product_tmpl_id', '=', product_template_id)], limit=1)
+    def _get_data_product_variants(self, product_template):
+        # product_variants = self.search([('atrribute_id', '=', product_template.attribute_id)],)
         
-        variantes = []
+        _logger.info(f'PRODUCTO TEMPLATE >>> { product_template.attribute_id }')
         
-        for product_variant in product_variants:
-            # variant_values = product_variant.product_template_variant_value_ids
+        # variantes = []
+        
+        # for product_variant in product_variants:
+        #     # variant_values = product_variant.product_template_variant_value_ids
             
-            # for variant_value in variant_values:
-            #     _logger.info(f'MOSTRANDO VARIANT VALUE >>> { variant_value.product_attribute_value_id.name }')
+        #     # for variant_value in variant_values:
+        #     #     _logger.info(f'MOSTRANDO VARIANT VALUE >>> { variant_value.product_attribute_value_id.name }')
             
-            _logger.info(f'MOSTRANDO LA DATA >>> { product_variant.product_template_variant_value_ids.attribute_id }')
+        #     _logger.info(f'MOSTRANDO LA DATA >>> { product_variant.product_template_variant_value_ids.attribute_id }')
             
-            variantes.append(product_variant.product_template_variant_value_ids.attribute_id)
+        #     variantes.append(product_variant.product_template_variant_value_ids.attribute_id)
                         
-        return variantes
+        return 'variantes'
