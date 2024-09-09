fc387c14a2b159a071c1e0f2829c64ea547806e6
midrovom <midrovo@callphoneecuador.com>
2024-09-09 09:16:09 -0500

    fix-update: actualizando modulo producto catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -11,7 +11,7 @@ class ProductCategory(models.Model):
     def _get_data_product_variants(self, product_template):
         # product_variants = self.search([('atrribute_id', '=', product_template.attribute_id)],)
         
-        _logger.info(f'PRODUCTO TEMPLATE >>> { product_template.attribute_id }')
+        _logger.info(f'PRODUCTO TEMPLATE >>> { product_template }')
         
         # variantes = []
         
