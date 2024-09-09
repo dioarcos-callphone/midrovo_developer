88db33d28592f90b541088e3835f4b14299ad1b7
midrovom <midrovo@callphoneecuador.com>
2024-09-09 10:50:35 -0500

    fix-update: actualizando manifest

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -19,7 +19,8 @@ class ProductCategory(models.Model):
         for product_line in product_attributte_lines:
             color = product_line.attribute_id.name
             if(color.lower() == 'color'):
-                _logger.info(f'MOSTRANDO COLORES >>> { product_line.value_ids }')
+                for value in product_line.value_ids.name:
+                    _logger.info(f'MOSTRANDO COLORES >>> { value }')
         
         # variantes = []
         
