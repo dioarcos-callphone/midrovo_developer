2beeababe9a85f60245d083d67798e504a3da2fa
midrovom <midrovo@callphoneecuador.com>
2024-09-09 17:45:43 -0500

    fix-update: actualizando modulo producto catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -26,6 +26,7 @@ class ProductCategory(models.Model):
                     colores.append(value.name)
                     
         for color in colores:
+            product_variants.clear()
             for product in product_product:
                 values = product.product_template_variant_value_ids
                 for value in values:
@@ -42,8 +43,6 @@ class ProductCategory(models.Model):
             }
 
             data.append(product_data)
-            
-            product_variants.clear()
             
         # for r in data[0]:
         #     for talla in r['tallas']:
