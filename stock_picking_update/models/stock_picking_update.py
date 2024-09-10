ba189cd1fb9dc21b45b05536ab5ec981a28e3230
midrovom <midrovo@callphoneecuador.com>
2024-09-10 17:06:17 -0500

    fix-update: actualizando modelo

--- stock_picking_update/models/stock_picking_update.py
+++ stock_picking_update/models/stock_picking_update.py
@@ -5,7 +5,16 @@ _logger = logging.getLogger(__name__)
 class StockPickingUpdate(models.Model):
     _inherit = "stock.move"
     
+    select_validate = []
+    
     @api.onchange('product_id')
     def _onchange_(self):
         product_id = self.product_id
-        _logger.info(f'MOSTRANDO PRODUCT >>> { product_id }')
+        
+        self.select_validate.append(product_id)
+        
+        for select in self.select_validate:
+            if select.id == product_id.id:
+                _logger.info(f'YA FUE SELECCIONADO EL PRODUCTO >>> { product_id }')
+                
+        
