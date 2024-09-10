914c494b2724cddd46c2f9b41cb1982b454a2fc4
midrovom <midrovo@callphoneecuador.com>
2024-09-10 16:38:37 -0500

    fix-update: actualizando manifest

--- stock_picking_update/__manifest__.py
+++ stock_picking_update/__manifest__.py
@@ -10,7 +10,7 @@
     'author': 'Mauricio Idrovo',
     'company': 'Callphone sa',
     'website': "https://www.callphoneecuador.com",
-    'depends': ['stock_picking'],
+    'depends': ['base'],
     'data': [],
 
     'license': 'LGPL-3',
