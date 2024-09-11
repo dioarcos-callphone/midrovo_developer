c91a9d17dc33476e07e1bbe9706eb72161d43044
midrovom <midrovo@callphoneecuador.com>
2024-09-11 17:02:05 -0500

    fix-update: actualizando modulo

--- update_product_catalog/__manifest__.py
+++ update_product_catalog/__manifest__.py
@@ -16,7 +16,8 @@
     'website': "https://www.callphoneecuador.com",
     'depends': [ 'amf_product_catalog', 'sale', ],
     'data': [
-        'report/product_catalog_template.xml',    
+        'report/product_catalog_template.xml',
+        'report/product_catalog_report.xml'   
     ],
     
     'assets': {
