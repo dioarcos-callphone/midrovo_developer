3384bc2382ce7783bcdb53752d9f03b5ad57f368
midrovom <midrovo@callphoneecuador.com>
2024-09-12 14:26:42 -0500

    fix-add: creando modulo product_template_catalog

---
+++ product_template_catalog/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    "name": "Product Template Catalog",
+    
+    "summary": "Optimizacion y Personalizacion Nueva del Catalogo de Productos.",
+    "description": """
+        
+    """,
+    
+    "category": "Product Template",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone sa',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'base' ],
+    'data': [],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
