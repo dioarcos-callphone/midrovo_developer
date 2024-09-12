e11df9ac28820af55d4fa74f065bf2008785c4f5
midrovom <midrovo@callphoneecuador.com>
2024-09-12 16:26:25 -0500

    fix-update: actualizando vista de reporte

--- product_template_catalog/models/product_catalog.py
+++ product_template_catalog/models/product_catalog.py
@@ -5,3 +5,12 @@ _logger = logging.getLogger(__name__)
 
 class ProductTemplateCatalog(models.Model):
     _inherit = "product.template"
+    
+    @api.model
+    def product_catalog_group_by_dinamic(self):
+        product_tmpl_attribute_lines = self.env['product.template.attribute_line'].search([('product_tmpl_id','=',self.id)])
+        
+        _logger.info('YYYY MOSTRANDO VALOR YYYY')
+        _logger.info(product_tmpl_attribute_lines)
+        
+        return self.name
