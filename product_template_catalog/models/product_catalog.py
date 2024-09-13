f802a66197094997bdf51fc539de8adb83f9f124
midrovom <midrovo@callphoneecuador.com>
2024-09-13 12:50:05 -0500

    fix-update: actualizando product_attribute_lines

--- product_template_catalog/models/product_catalog.py
+++ product_template_catalog/models/product_catalog.py
@@ -25,14 +25,14 @@ class ProductTemplateCatalog(models.Model):
     def product_variant_group(self):
         product_id = self.id
         
-        attributes = self.env['product.template.attribute.line'].search([
-            ('product_tmpl_id', '=', product_id)
-        ])
+        # attributes = self.env['product.template.attribute.line'].search([
+        #     ('product_tmpl_id', '=', product_id)
+        # ])
         
         product_variants = self.env['product.product'].read_group(
             domain=[
                 ('product_tmpl_id', '=', product_id),
-                ('product_template_variant_value_ids', 'in', attributes )
+                ('product_template_variant_value_ids', 'in', [ 11, 12, ] )
             ],
             fields=['product_template_variant_value_ids'],
             groupby=[
@@ -43,24 +43,24 @@ class ProductTemplateCatalog(models.Model):
         
         _logger.info(f'Mostrando variantes de producto >>> { product_variants }')
         
-        formatted_variants = []
-        for variant in product_variants:
-            variant_values = self.env['product.template.attribute.value'].browse(variant['product_template_variant_value_ids'][0])
+        # formatted_variants = []
+        # for variant in product_variants:
+        #     variant_values = self.env['product.template.attribute.value'].browse(variant['product_template_variant_value_ids'][0])
             
-            # attribute = variant_values.attribute_id.name
+        #     # attribute = variant_values.attribute_id.name
             
-            # if attribute in [ 'tallas', 'color' ]:
-            #     formatted_variants.append({
-            #         'variante': variant_values.attribute_id.name,
-            #         'count': variant['__count'],
-            #     })
+        #     # if attribute in [ 'tallas', 'color' ]:
+        #     #     formatted_variants.append({
+        #     #         'variante': variant_values.attribute_id.name,
+        #     #         'count': variant['__count'],
+        #     #     })
             
-            formatted_variants.append({
-                'variante': variant_values.attribute_id.name,
-                'count': variant['__count'],
-            })
+        #     formatted_variants.append({
+        #         'variante': variant_values.attribute_id.name,
+        #         'count': variant['__count'],
+        #     })
         
-        _logger.info(formatted_variants)
+        # _logger.info(formatted_variants)
         
         return 'prueba'
 
