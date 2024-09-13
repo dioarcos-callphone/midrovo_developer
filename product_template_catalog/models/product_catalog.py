515a592fe0f7b3411622cf0c70576e28ca7b2caa
midrovom <midrovo@callphoneecuador.com>
2024-09-13 11:12:57 -0500

    fix-update: actualizando product_attribute_lines

--- product_template_catalog/models/product_catalog.py
+++ product_template_catalog/models/product_catalog.py
@@ -29,7 +29,7 @@ class ProductTemplateCatalog(models.Model):
             domain=[ ('product_tmpl_id', '=', product_id) ],
             fields=['product_template_variant_value_ids'],
             groupby=[
-                'product_template_variant_value_ids.name',
+                'product_template_variant_value_ids',
             ],
             lazy=False
         )
