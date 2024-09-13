0c3c72f29fa7f079fc38836bdc009a2fc890c724
midrovom <midrovo@callphoneecuador.com>
2024-09-13 10:27:03 -0500

    fix-update: actualizando product_attribute_lines

--- product_template_catalog/models/product_catalog.py
+++ product_template_catalog/models/product_catalog.py
@@ -7,29 +7,28 @@ class ProductTemplateCatalog(models.Model):
     _inherit = "product.template"
     
     @api.model
-    def product_catalog_group_by_dinamic(self):
-        product_variants = self.env['product.product'].read_group(
-            [( 'product_tmpl_id', '=', self.id )],
-            [ 'product_tmpl_id', 'name', 'product_template_variant_value_ids', 'qty_available', 'immediately_usable_qty' ],
-            [ 'product_tmpl_id' ]
-        )
+    def product_attribute_lines(self):      
+        product_template_id = self.id
         
-        for variant in product_variants:
-            _logger.info('YYYY MOSTRANDO VALOR YYYY')
-            
-            name = variant.get('name')
-            variant_values = variant.get('product_template_variant_value_ids')
-            qty_available = variant.get('qty_available')
-            immediately_usable = variant.get('immediately_usable_qty')
-            
-            _logger.info(f'Nombre del producto: { name }')
-            _logger.info(f'Variantes: { variant_values }')
-            _logger.info(f'Cantidad en mano: { qty_available }')
-            _logger.info(f'Cantidad inmediata: { immediately_usable }')
+        attribute_lines = self.env['product.template.attribute.line'].search([
+            ('product_tmpl_id', '=', product_template_id)
+        ])
         
-        # _logger.info('YYYY MOSTRANDO VALOR YYYY')
-        # _logger.info(product_variants)
+        if attribute_lines:
+            _logger.info(len(attribute_lines))
+        
+        # product_variants = self.env['product.product'].search([
+        #     ('product_tmpl_id', '=', product_template_id)
+        # ])
+        
+        
+        
+        # if product_variants:
+        #     variantes = product_variants.product_template
+            
+        #     _logger.info('')
         
         name = self.name
         
         return name
+
