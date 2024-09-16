1ba30f46b6c82ace40ead9c9a200df99bcb90866
midrovom <midrovo@callphoneecuador.com>
2024-09-16 16:46:32 -0500

    fix-update: actualizando directorio

--- product_template_catalog/reports/product_catalog.py
+++ product_template_catalog/reports/product_catalog.py
@@ -135,7 +135,7 @@ class ProductTemplateCatalog(models.Model):
                                 if product_data:
                                     products_data.append(product_data)
                                     
-            product_data = set(product_data)
+            products_data = set(products_data)
             _logger.info(products_data)
                                 
             product_color = set(product_color)
