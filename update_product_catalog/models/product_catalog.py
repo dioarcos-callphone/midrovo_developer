c3dee0ba9dfe90dbe3cad31060a0dcf02b31f091
midrovom <midrovo@callphoneecuador.com>
2024-09-17 17:10:15 -0500

    fix-update: actualizando directorio

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -78,48 +78,80 @@ class ProductCategory(models.Model):
                 data.append(product_data)
                         
             # return data
-        if data:
-            for d in data:
-                product_catalogo = {}
-                col = d['color']
-                _logger.info(f'color  >>>>  { col }')
-                tallas = d['tallas']
-                suma = 0
-                sizes = []
-                product_catalogo['color'] = d['color']
-                product_catalogo['img'] = d['img']
-                product_catalogo['disponible'] = d['disponible']
-                product_catalogo['talla_unica'] = d['talla_unica']
-                for ta in talla:
-                    contador = 0
-                    total = 0
-                    for t in tallas:
+            
+            if data:
+                for d in data:
+                    product_catalogo = {}
+                    col = d['color']
+                    _logger.info(f'color  >>>>  { col }')
+                    tallas = d['tallas']
+                    suma = 0
+                    sizes = {}
+                    product_catalogo['color'] = d['color']
+                    product_catalogo['img'] = d['img']
+                    product_catalogo['disponible'] = d['disponible']
+                    product_catalogo['talla_unica'] = d['talla_unica']
+
+                    for ta in talla:
+                        for t in tallas:
+                            for v in t.product_template_variant_value_ids:
+                                if v.attribute_id.name.lower() in ['talla', 'tallas']:
+                                    if v.name == ta:
+                                        # Si la talla ya está en sizes, suma los totales
+                                        if v.name in sizes:
+                                            sizes[v.name] += t.qty_available
+                                        else:
+                                            sizes[v.name] = t.qty_available
+
+                    # Convertir el diccionario sizes a una lista de diccionarios
+                    product_catalogo['tallas'] = [{'talla': key, 'total': value} for key, value in sizes.items()]
+                    data_catalog.append(product_catalogo)
+
+                _logger.info(f'{ data_catalog }')
+
+        # if data:
+        #     for d in data:
+        #         product_catalogo = {}
+        #         col = d['color']
+        #         _logger.info(f'color  >>>>  { col }')
+        #         tallas = d['tallas']
+        #         suma = 0
+        #         sizes = []
+        #         product_catalogo['color'] = d['color']
+        #         product_catalogo['img'] = d['img']
+        #         product_catalogo['disponible'] = d['disponible']
+        #         product_catalogo['talla_unica'] = d['talla_unica']
+        #         for ta in talla:
+        #             contador = 0
+        #             total = 0
+        #             for t in tallas:
                         
-                        for v in t.product_template_variant_value_ids:
-                            if v.attribute_id.name.lower() in ['talla', 'tallas']:
-                                if v.name == ta:
+        #                 for v in t.product_template_variant_value_ids:
+        #                     if v.attribute_id.name.lower() in ['talla', 'tallas']:
+        #                         if v.name == ta:
                                     
-                                    contador = contador + 1
-                                    suma = suma + t.qty_available                                 
-                                    if contador > 1:
-                                        total = suma
-                                        sizes.append({
-                                            "talla": v.name,
-                                            "total": total
-                                        })
+        #                             contador = contador + 1
+        #                             suma = suma + t.qty_available                                 
+        #                             if contador > 1:
+        #                                 total = suma
+        #                                 sizes.append({
+        #                                     "talla": v.name,
+        #                                     "total": total
+        #                                 })
                                         
-                                else:
-                                    for size in sizes:
-                                        if size['talla'] != ta:
-                                            sizes.append({
-                                                "talla": v.name,
-                                                "total": t.qty_available
-                                            })
+        #                         else:
+                                    
+        #                             for size in sizes:
+        #                                 if size['talla'] != ta:
+        #                                     sizes.append({
+        #                                         "talla": v.name,
+        #                                         "total": t.qty_available
+        #                                     })
                                         
-                                    product_catalogo['tallas'] = sizes                                    
+        #                             product_catalogo['tallas'] = sizes                                    
   
-                                    #_logger.info(f'talla >>> { v.name } - precio >>> { t.immediately_usable_qty } - total >>> { total }')
-                data_catalog.append(product_catalogo)
-            _logger.info(f'{ data_catalog }')
+        #                             #_logger.info(f'talla >>> { v.name } - precio >>> { t.immediately_usable_qty } - total >>> { total }')
+        #         data_catalog.append(product_catalogo)
+        #     _logger.info(f'{ data_catalog }')
                 # _logger.info(" ")
         return data if data else None
