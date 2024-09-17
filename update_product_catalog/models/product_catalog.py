6b89fd3f15a6a252d925cebe4c925698ea917c61
midrovom <midrovo@callphoneecuador.com>
2024-09-17 16:55:42 -0500

    fix-update: actualizando directorio

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -103,13 +103,18 @@ class ProductCategory(models.Model):
                                     suma = suma + t.qty_available                                 
                                     if contador > 1:
                                         total = suma
-                                    
-                                sizes.append({
-                                    "talla": v.name,
-                                    "total": total if contador > 1 else t.qty_available
-                                })
-                                    
-                                product_catalogo['tallas'] = sizes                                    
+                                        sizes.append({
+                                            "talla": v.name,
+                                            "total": total
+                                        })
+                                        
+                                else:
+                                    sizes.append({
+                                        "talla": v.name,
+                                        "total": t.qty_available
+                                    })
+                                        
+                                    product_catalogo['tallas'] = sizes                                    
   
                                     #_logger.info(f'talla >>> { v.name } - precio >>> { t.immediately_usable_qty } - total >>> { total }')
                 data_catalog.append(product_catalogo)
