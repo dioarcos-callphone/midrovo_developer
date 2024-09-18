c26f36cf6f328cccdfc29e78118863c02aa7da47
midrovom <midrovo@callphoneecuador.com>
2024-09-18 14:27:04 -0500

    fix-update: actualizando product catalog

--- update_product_catalog/models/product_catalog.py
+++ update_product_catalog/models/product_catalog.py
@@ -114,3 +114,14 @@ class ProductCategory(models.Model):
             raise UserError(f'Solo se permite crear catálogo con cantidad disponible')
         
         return True
+    
+class CustomReplenishmentTemplateReport(models.AbstractModel):
+    _inherit = 'report.stock.report_product_template_replenishment'
+
+    @api.model
+    def get_report_values(self, docids, data=None, serialize=False):
+        res = super(CustomReplenishmentTemplateReport, self).get_report_values(docids, data, serialize)
+        
+        _logger.info(f'>>>>> FUNCIONA <<<<<')
+        
+        return res
