2ca22933a2a9ff02c3c26529e17d9b7532811b10
midrovom <midrovo@callphoneecuador.com>
2024-09-23 12:32:59 -0500

    fix-update: actualizando nombre de la vista wizard

--- inventory_report_location/wizards/stock_quantity_history.py
+++ inventory_report_location/wizards/stock_quantity_history.py
@@ -8,14 +8,19 @@ _logger = logging.getLogger(__name__)
 class StockQuantityHistoryInherit(models.TransientModel):
     _inherit = 'stock.quantity.history'
     
+    location_ids = fields.Many2many(
+        "stock.location", string="Ubicaciones",
+        domain=[('usage','=','internal')],
+        help="Seleccione las ubicaciones para generar el informe")
+    
     def action_pdf(self):
         _logger.info(f'MOSTRANDO DESDE EL WIZARD >>> { self.id }')
         
         """This function is for printing pdf report"""
         data = {
-            'model_id': 'self.id',
+            'model_id': self.id,
             'product_ids': 'self.product_ids.ids',
-            'location_ids': 'self.location_ids.ids',
+            'location_ids': self.location_ids.ids,
             'category_ids': 'self.category_ids.ids',
             'company_ids': 'self.company_ids.ids',
             'age_breakdown_days': 'self.age_breakdown_days',
