81a93043e6eda5cb147806224f1cbc11f10722bb
midrovom <midrovo@callphoneecuador.com>
2024-09-23 10:37:28 -0500

    fix-add: agregando modulo inventory report location

---
+++ inventory_report_location/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Inventory Report Location",
+    
+    "summary": "Reporte de inventario por Ubicación.",
+    "description": """
+        Muestra el inventario PDF de acuerdo a la categoria de producto y la localidad.
+        "
+    """,
+    
+    "category": "Stock",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone sa',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'base', 'stock' ],
+    'data': [
+        'reports/inventory_template.xml',
+        'wizards/inventory_wizard.xml',
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
