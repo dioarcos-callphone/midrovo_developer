c0d32dedecf7c1eda262bfa4ea30b0b9ce922730
midrovom <midrovo@callphoneecuador.com>
2024-09-23 12:54:35 -0500

    fix-update: actualizando nombre de la vista wizard

--- inventory_report_location/reports/stock_quantity_history.py
+++ inventory_report_location/reports/stock_quantity_history.py
@@ -17,33 +17,29 @@ class StockQuantityHistory(models.AbstractModel):
         ]
         
         # Agrupamos por el nombre del producto y el precio estándar
-        # quant_records = self.env['stock.quant'].read_group(
-        #     domain,
-        #     ['product_id', 'quantity'],
-        #     ['product_id:product_tmpl_id', 'product_id:standard_price'],
-        # )
+        quant_records = self.env['stock.quant'].read_group(
+            domain,
+            ['product_id', 'quantity'],
+            ['product_id:product_tmpl_id.id', 'product_id:standard_price'],
+        )
 
         # Procesamos los resultados
         result = []
-        # for record in quant_records:
-        #     result.append({
-        #         'name': record['product_id'][1],  # Nombre del producto
-        #         'standard_price': record['product_id'][2],  # Precio estándar
-        #         'quantity': record['quantity'],  # Suma de cantidades
-        #     })
+        for record in quant_records:
+            result.append({
+                'name': record['product_id'][1],  # Nombre del producto
+                'standard_price': record['product_id'][2],  # Precio estándar
+                'quantity': record['quantity'],  # Suma de cantidades
+            })
         
-        _logger.info(f'MOSTRANDO RESULTADO >>> ') 
+        _logger.info(f'MOSTRANDO RESULTADO >>> { result }') 
         
-        if result:
-            _logger.info(f'MOSTRANDO RESULTADO >>>')           
+        if result:          
             return {
                 'doc_ids': docids,
                 'doc_model': 'report.stock.quantity.history',
                 'options': result,
             }
             
-        return {
-            'doc_ids': docids,
-            'doc_model': 'report.stock.quantity.history',
-            'options': result,
-        }
+        else:
+            raise ValidationError("No records found for the given criteria!")
