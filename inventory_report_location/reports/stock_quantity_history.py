4ef5b8d1fa30c099d113318cd172c7e81528fe89
midrovom <midrovo@callphoneecuador.com>
2024-09-23 11:11:40 -0500

    fix-update: actualizando nombre de la vista wizard

---
+++ inventory_report_location/reports/stock_quantity_history.py
@@ -0,0 +1,13 @@
+from odoo import api, models
+from odoo.exceptions import ValidationError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class StockQuantityHistory(models.AbstractModel):
+    _name = 'stock.quantity.history'
+    _description = 'Stock Quantity History'
+    
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        pass
