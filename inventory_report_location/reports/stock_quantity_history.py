62dbe982c6bc3c7a3465569debdb91b074804d6b
midrovom <midrovo@callphoneecuador.com>
2024-09-23 15:47:37 -0500

    fix-update: actualizando nombre de la vista wizard

--- inventory_report_location/reports/stock_quantity_history.py
+++ inventory_report_location/reports/stock_quantity_history.py
@@ -25,7 +25,8 @@ class StockQuantityHistory(models.AbstractModel):
         
         domain = [
             ('location_id', 'in', location_id),
-            ('inventory_date', '!=', False)
+            ('inventory_date', '!=', False),
+            ('invnetory_date', '=', date_format)
         ]
         
         # Agrupamos por el nombre del producto y el precio estándar
