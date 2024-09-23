3ee8804ec24f1c296d3130e42f14a1b9386b5527
midrovom <midrovo@callphoneecuador.com>
2024-09-23 12:25:18 -0500

    fix-update: actualizando nombre de la vista wizard

--- inventory_report_location/wizards/stock_quantity_history.py
+++ inventory_report_location/wizards/stock_quantity_history.py
@@ -1,9 +1,16 @@
-from odoo import _, fields, models
+from odoo import fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
 
 class StockQuantityHistoryInherit(models.TransientModel):
     _inherit = 'stock.quantity.history'
     
     def action_pdf(self):
+        _logger.info(f'MOSTRANDO DESDE EL WIZARD >>> { self.id }')
+        
         """This function is for printing pdf report"""
         data = {
             'model_id': 'self.id',
@@ -14,7 +21,5 @@ class StockQuantityHistoryInherit(models.TransientModel):
             'age_breakdown_days': 'self.age_breakdown_days',
         }
         return (
-            self.env.ref(
-                'inventory_report_location.'
-                'report_stock_quantity_history')
+            self.env.ref('inventory_report_location.report_stock_quantity_history')
             .report_action(None, data=data))
