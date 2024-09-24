7a5af3a6c6c977edf2d1d8e16fd2f5bfcad274e7
midrovom <midrovo@callphoneecuador.com>
2024-09-24 16:28:22 -0500

    fix-update: actualizando open_at_date

--- inventory_report_location/models/product_product.py
+++ inventory_report_location/models/product_product.py
@@ -9,26 +9,25 @@ class ProductProductInherit(models.Model):
         for producto in self:
             variantes = []
             
-            if producto.qty_available > 0 and producto.standard_price > 0 and producto.total_value > 0:
-                costo = producto.standard_price
-                valor_stock = producto.total_value
+            costo = producto.standard_price
+            valor_stock = producto.total_value
 
-                data = {
-                    "id": producto.id,
-                    "nombre": producto.name,
-                    "cantidad": producto.qty_available,
-                    "costo": round(costo, 3),
-                    "valor_stock": round(valor_stock, 3),
-                }
-            
-                if producto.product_template_variant_value_ids:
-                    for v in producto.product_template_variant_value_ids:
-                        variantes.append({
-                            f'{ v.attribute_id.name }' : f'{ v.name }',
-                        })
-                    
-                data['atributos'] = variantes
-                data_productos.append(data)
+            data = {
+                "id": producto.id,
+                "nombre": producto.name,
+                "cantidad": producto.qty_available,
+                "costo": round(costo, 3),
+                "valor_stock": round(valor_stock, 3),
+            }
+        
+            if producto.product_template_variant_value_ids:
+                for v in producto.product_template_variant_value_ids:
+                    variantes.append({
+                        f'{ v.attribute_id.name }' : f'{ v.name }',
+                    })
+                
+            data['atributos'] = variantes
+            data_productos.append(data)
             
         data = {
             "productos" : data_productos
