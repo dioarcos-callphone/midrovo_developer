8facce5467172a7de9d9cebdb838fd54c7698681
midrovom <midrovo@callphoneecuador.com>
2024-09-24 09:48:16 -0500

    fix-update: actualizando open_at_date

--- inventory_report_location/wizards/stock_quantity_history.py
+++ inventory_report_location/wizards/stock_quantity_history.py
@@ -26,6 +26,38 @@ class StockQuantityHistoryInherit(models.TransientModel):
             .report_action(None, data=data))
         
         
+    # def open_at_date(self):
+    #     action = super(StockQuantityHistoryInherit, self).open_at_date()
+        
+    #     domain = action.get('domain', [])
+        
+    #     # Agregar filtros por categoría
+    #     if self.category_ids:
+    #         _logger.info(f'MOSTRANDO CATEGORIES IDS >>> {self.category_ids}')
+    #         category_ids = self.category_ids.ids
+    #         domain = expression.AND([domain, [('categ_id', 'in', category_ids)]])  # Cambio aquí: categ_id en lugar de categ_id.id
+        
+    #     # Agregar filtros por ubicación
+    #     if self.location_ids:
+    #         _logger.info(f'MOSTRANDO LOCATION IDS >>> {self.location_ids}')
+    #         location_ids = self.location_ids.ids
+    #         _logger.info(f'LOCATION IDS >>> {location_ids}')
+            
+    #         # Buscar los productos en las ubicaciones
+    #         quants = self.env['stock.quant'].search([('location_id', 'in', location_ids)])
+    #         product_ids = quants.mapped('product_id')  # Obtenemos los IDs de los productos
+    #         _logger.info(f'PRODUCT IDS >>> {product_ids.ids}')
+            
+    #         # Filtrar por productos en las ubicaciones seleccionadas
+    #         domain = expression.AND([domain, [('id', 'in', product_ids.ids)]])
+        
+    #     # Actualizar el dominio en la acción
+    #     action['domain'] = domain
+        
+    #     _logger.info(f"Final domain: {action['domain']}")
+        
+    #     return action
+    
     def open_at_date(self):
         action = super(StockQuantityHistoryInherit, self).open_at_date()
         
@@ -33,23 +65,34 @@ class StockQuantityHistoryInherit(models.TransientModel):
         
         # Agregar filtros por categoría
         if self.category_ids:
-            _logger.info(f'MOSTRANDO CATEGORIES IDS >>> {self.category_ids}')
             category_ids = self.category_ids.ids
-            domain = expression.AND([domain, [('categ_id', 'in', category_ids)]])  # Cambio aquí: categ_id en lugar de categ_id.id
+            domain = expression.AND([domain, [('categ_id', 'in', category_ids)]])
         
         # Agregar filtros por ubicación
         if self.location_ids:
-            _logger.info(f'MOSTRANDO LOCATION IDS >>> {self.location_ids}')
             location_ids = self.location_ids.ids
-            _logger.info(f'LOCATION IDS >>> {location_ids}')
             
-            # Buscar los productos en las ubicaciones
+            # Buscar los productos y sus cantidades por ubicación
             quants = self.env['stock.quant'].search([('location_id', 'in', location_ids)])
-            product_ids = quants.mapped('product_id')  # Obtenemos los IDs de los productos
-            _logger.info(f'PRODUCT IDS >>> {product_ids.ids}')
             
-            # Filtrar por productos en las ubicaciones seleccionadas
-            domain = expression.AND([domain, [('id', 'in', product_ids.ids)]])
+            # Crear un diccionario para almacenar la cantidad por producto por ubicación
+            product_quantities = {}
+            
+            for quant in quants:
+                product_id = quant.product_id.id
+                if product_id not in product_quantities:
+                    product_quantities[product_id] = 0.0
+                product_quantities[product_id] += quant.quantity
+            
+            # Obtener los IDs de productos y las cantidades correspondientes
+            product_ids = list(product_quantities.keys())
+            quantities = list(product_quantities.values())
+            
+            _logger.info(f'PRODUCT IDS >>> {product_ids}')
+            _logger.info(f'QUANTITIES >>> {quantities}')
+            
+            # Filtrar por productos y sus cantidades específicas en las ubicaciones seleccionadas
+            domain = expression.AND([domain, [('id', 'in', product_ids), ('qty_available', 'in', quantities)]])
         
         # Actualizar el dominio en la acción
         action['domain'] = domain
@@ -57,3 +100,4 @@ class StockQuantityHistoryInherit(models.TransientModel):
         _logger.info(f"Final domain: {action['domain']}")
         
         return action
+
