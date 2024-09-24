1048d6ef0e7d0183793fa2fecb3a000cc14b7770
midrovom <midrovo@callphoneecuador.com>
2024-09-24 09:44:52 -0500

    fix-update: actualizando open_at_date

--- inventory_report_location/wizards/stock_quantity_history.py
+++ inventory_report_location/wizards/stock_quantity_history.py
@@ -33,20 +33,24 @@ class StockQuantityHistoryInherit(models.TransientModel):
         
         # Agregar filtros por categoría
         if self.category_ids:
-            _logger.info(f'MOSTRANDO CATEGORIES IDS >>> { self.category_ids }')
+            _logger.info(f'MOSTRANDO CATEGORIES IDS >>> {self.category_ids}')
             category_ids = self.category_ids.ids
-            domain = expression.AND([domain, [('categ_id.id', 'in', category_ids)]])
-
+            domain = expression.AND([domain, [('categ_id', 'in', category_ids)]])  # Cambio aquí: categ_id en lugar de categ_id.id
+        
         # Agregar filtros por ubicación
         if self.location_ids:
-            _logger.info(f'MOSTRANDO LOCATION IDS >>> { self.location_ids }')
+            _logger.info(f'MOSTRANDO LOCATION IDS >>> {self.location_ids}')
             location_ids = self.location_ids.ids
-            _logger.info(f'LOCATION IDS >>> { location_ids }')
-            products = self.env['stock.quant'].search([('location_id', 'in', location_ids)])
-            ids = [ p.inventory_quantity_auto_apply for p in products ]
-            _logger.info(f'QTY AVAILABLE >>> { ids }')
-            domain = expression.AND([domain, [('qty_available', 'in', ids)]])
-
+            _logger.info(f'LOCATION IDS >>> {location_ids}')
+            
+            # Buscar los productos en las ubicaciones
+            quants = self.env['stock.quant'].search([('location_id', 'in', location_ids)])
+            product_ids = quants.mapped('product_id')  # Obtenemos los IDs de los productos
+            _logger.info(f'PRODUCT IDS >>> {product_ids.ids}')
+            
+            # Filtrar por productos en las ubicaciones seleccionadas
+            domain = expression.AND([domain, [('id', 'in', product_ids.ids)]])
+        
         # Actualizar el dominio en la acción
         action['domain'] = domain
         
