cba9847eb35d041b3bb07d0dafa391c6a05b5b3c
midrovom <midrovo@callphoneecuador.com>
2024-09-25 11:31:59 -0500

    fix-update: actualizando manifest

--- inventory_report_location/static/src/js/button_tree_extends.js
+++ inventory_report_location/static/src/js/button_tree_extends.js
@@ -2,13 +2,14 @@
 import { ListController } from "@web/views/list/list_controller";
 import { registry } from '@web/core/registry';
 import { listView } from '@web/views/list/list_view';
+import { rpc } from '@web';
 export class SaleListController extends ListController {
    setup() {
        super.setup();
    }
    async actionPDF() {
        // Hacer una llamada RPC al método 'action_pdf' en el modelo 'product.product'
-       const { data } = await this._rpc({
+       const { data } = await rpc({
            model: 'product.product',
            method: 'action_pdf',
        });
