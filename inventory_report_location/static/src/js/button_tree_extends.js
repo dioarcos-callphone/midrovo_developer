9e5db6d358bf7a3a6d1d6a6a01150fcd70154248
midrovom <midrovo@callphoneecuador.com>
2024-09-25 12:21:52 -0500

    fix-update: actualizando manifest

--- inventory_report_location/static/src/js/button_tree_extends.js
+++ inventory_report_location/static/src/js/button_tree_extends.js
@@ -3,35 +3,31 @@ import { ListController } from "@web/views/list/list_controller";
 import { registry } from '@web/core/registry';
 import { listView } from '@web/views/list/list_view';
 export class SaleListController extends ListController {
-   setup() {
+    setup() {
        super.setup();
-   }
-   async actionPDF() {
-    try {
-        // Llamar al método action_pdf sin pasar ningún argumento
-        const { data } = await this._rpc({
-            model: 'product.product',
-            method: 'action_pdf',
-            args: [],  // No se pasan argumentos ya que no hay selección
-        });
-
-        // Manejar la respuesta para abrir el PDF
-        if (data && data.pdf_url) {
-            window.open(data.pdf_url, '_blank');
-        } else {
-            this._notify("Error al generar el PDF", { type: "warning" });
-        }
     }
     
-    catch (error) {
-        console.error("Error al ejecutar actionPDF:", error);
-        this._notify("Ocurrió un error al generar el PDF", { type: "danger" });
-    }
+    async actionPDF() {
+        const result = await this._rpc({
+            model: 'product.product',
+            method: 'action_pdf',
+        });
 
-   }
+        if (result) {
+            const blob = new Blob([result], { type: 'application/pdf' });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'Informe_Inventario.pdf';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    }
 }
+
 registry.category("views").add("button_in_tree", {
-   ...listView,
-   Controller: SaleListController,
-   buttonTemplate: "button_sale.ListView.Buttons",
+    ...listView,
+    Controller: SaleListController,
+    buttonTemplate: "button_sale.ListView.Buttons",
 });
