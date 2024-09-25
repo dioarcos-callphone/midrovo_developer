a8d7d521440f8f7a231293091061b767533efb14
midrovom <midrovo@callphoneecuador.com>
2024-09-25 13:00:03 -0500

    fix-update: actualizando manifest

--- inventory_report_location/controllers/controller.py
+++ inventory_report_location/controllers/controller.py
@@ -3,7 +3,7 @@ from odoo.http import request
 
 class ProductReportController(http.Controller):
 
-    @http.route('/product/pdf_report', type='http', auth='user')
+    @http.route('/product/pdf_report', type='http', auth='public' csrf=False)
     def pdf_report(self, **kwargs):
         products = request.env['product.product'].search([])  # O puedes aplicar un dominio
         return products.action_pdf()
