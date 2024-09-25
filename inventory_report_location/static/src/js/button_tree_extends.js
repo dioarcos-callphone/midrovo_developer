49c5fe8f76051618c8d260b9c26cc48230d4679b
midrovom <midrovo@callphoneecuador.com>
2024-09-25 11:47:25 -0500

    fix-update: actualizando manifest

--- inventory_report_location/static/src/js/button_tree_extends.js
+++ inventory_report_location/static/src/js/button_tree_extends.js
@@ -9,12 +9,10 @@ export class SaleListController extends ListController {
    }
    async actionPDF() {
        // Hacer una llamada RPC al método 'action_pdf' en el modelo 'product.product'
-       const { data } = await rpc.query({
+       return await rpc.query({
            model: 'product.product',
            method: 'action_pdf',
        });
-
-       return
 
    }
 }
