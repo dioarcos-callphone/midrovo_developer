86e56edcbaacb152b92bbf9e5ec7c5ce3de66211
midrovom <midrovo@callphoneecuador.com>
2024-09-25 11:28:52 -0500

    fix-update: actualizando manifest

--- inventory_report_location/static/src/js/button_tree_extends.js
+++ inventory_report_location/static/src/js/button_tree_extends.js
@@ -7,19 +7,10 @@ export class SaleListController extends ListController {
        super.setup();
    }
    async actionPDF() {
-       // Obtener los IDs de los productos seleccionados
-       const selectedIds = this.selectedRecordIds;
-
-       if (selectedIds.length === 0) {
-           this._notify("No se han seleccionado productos", { type: "warning" });
-           return;
-       }
-
        // Hacer una llamada RPC al método 'action_pdf' en el modelo 'product.product'
        const { data } = await this._rpc({
            model: 'product.product',
            method: 'action_pdf',
-           args: [selectedIds],
        });
 
    }
