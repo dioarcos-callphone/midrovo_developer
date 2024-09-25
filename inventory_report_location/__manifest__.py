d683f5aabd4f8473bd8ecce2d69f38af9e9260b8
midrovom <midrovo@callphoneecuador.com>
2024-09-25 09:03:34 -0500

    fix-update: actualizando manifest

--- inventory_report_location/__manifest__.py
+++ inventory_report_location/__manifest__.py
@@ -12,13 +12,19 @@
     'author': 'Mauricio Idrovo',
     'company': 'Callphone sa',
     'website': "https://www.callphoneecuador.com",
-    'depends': [ 'stock', ],
+    'depends': [ 'stock', 'web' ],
     'data': [
         'wizards/stock_quantity_history_wizard.xml',
         'reports/stock_quantity_history_report.xml',
         'reports/stock_quantity_history_template.xml',
         'views/stock_quantity_view.xml'
     ],
+    
+    'assets': {
+        'web.assets_backend': [
+            'inventory_report_location/static/src/xml/button_add_template.xml',
+        ],
+    },
     
     'license': 'LGPL-3',
     'installable': True,
