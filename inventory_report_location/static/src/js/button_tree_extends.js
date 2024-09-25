d875e7158e3f4e00a3ba16a5b56f5bb01ea4d025
midrovom <midrovo@callphoneecuador.com>
2024-09-25 12:55:05 -0500

    fix-update: actualizando manifest

--- inventory_report_location/static/src/js/button_tree_extends.js
+++ inventory_report_location/static/src/js/button_tree_extends.js
@@ -8,9 +8,23 @@ export class SaleListController extends ListController {
        super.setup();
     }
     
-    // async actionPDF() {
-        
-    // }
+    async actionPDF() {
+        try {
+            // Hacer una llamada al controlador
+            const result = await rpc({
+                route: '/product/pdf_report',
+                params: {}, // Aquí puedes enviar parámetros si lo necesitas
+            });
+
+            // La respuesta de la acción PDF puede redirigir a la URL de descarga
+            if (result) {
+                // Aquí puedes redirigir a la URL de descarga del PDF si es necesario
+                window.open(result, '_blank'); // Abre en una nueva pestaña
+            }
+        } catch (error) {
+            console.error("Error al generar el PDF:", error);
+        }
+    }
 }
 
 registry.category("views").add("button_in_tree", {
