7ca4836c7c1a5d94c2c8fd2b6b5d90e4d177bc99
midrovom <midrovo@callphoneecuador.com>
2024-09-25 09:05:34 -0500

    fix-update: actualizando manifest

--- inventory_report_location/__manifest__.py
+++ inventory_report_location/__manifest__.py
@@ -20,11 +20,11 @@
         'views/stock_quantity_view.xml'
     ],
     
-    'assets': {
-        'web.assets_backend': [
-            'inventory_report_location/static/src/xml/button_add_template.xml',
-        ],
-    },
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'inventory_report_location/static/src/xml/button_add_template.xml',
+    #     ],
+    # },
     
     'license': 'LGPL-3',
     'installable': True,
