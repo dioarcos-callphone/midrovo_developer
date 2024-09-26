9e620153058195588d0bfd33400115a23c7bc578
midrovom <midrovo@callphoneecuador.com>
2024-09-26 10:27:30 -0500

    fix-add: añadiendo modulo invoice details view

---
+++ invoice_details_view/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Invoice Details View',
+    'summary': 'Modulo que filtra desde un wizard y genera informe de los detalles de facturas',
+    'description': """
+        Este modulo realiza los siguientes detalles:
+        - El wizard filtra por rango de fecha, diario contable, comercial, sales person
+        - La vista tree del account move line muestra los detalles de la factura
+        - Cuando genere el informe seleccione los registros y de click en PDF
+    """,
+    
+    "category": "Account Move",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'account' ],
+    'data': [
+        
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
