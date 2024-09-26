4682630cc377ad7ca5165749d16540764a28a9ef
midrovom <midrovo@callphoneecuador.com>
2024-09-26 16:24:53 -0500

    fix-update: actualizando _get_report_values

--- invoice_details_view/report/invoice_details.py
+++ invoice_details_view/report/invoice_details.py
@@ -10,6 +10,7 @@ class InvoiceDetails(models.AbstractModel):
     
     @api.model
     def _get_report_values(self, docids, data=None):
+        data_invoice_details = []
         fecha_inicio = data['fecha_inicio']
         fecha_fin = data['fecha_fin']
         diario = data['diario']
@@ -25,11 +26,22 @@ class InvoiceDetails(models.AbstractModel):
         if invoice_details:
             _logger.info(f'MOSTRANDO INVOICE DETAILS >>> { invoice_details }')
             
+            for detail in invoice_details:
+                data_detail = {
+                    "numero": detail.move_name,
+                    "producto": detail.product_id.name,
+                    "cantidad": detail.quantity,
+                    "precio": detail.price_unit,
+                    "costo": detail.price_subtotal,
+                }
+                
+                data_invoice_details.append(data_detail)
+            
             return {
                 'doc_ids': docids,
                 'doc_model': 'report.invoice_details_view.report_invoice_details',
                 'data': data,
-                'options': invoice_details,
+                'options': data_invoice_details,
             }
         else:
             raise ValidationError("No records found for the given criteria!")
