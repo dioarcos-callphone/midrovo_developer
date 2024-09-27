571e34cb7f9ffd996454582e66ded6c88709ddc1
midrovom <midrovo@callphoneecuador.com>
2024-09-27 10:57:55 -0500

    fix-update: actualizando vista wizard

--- invoice_details_view/static/src/js/action_report.js
+++ invoice_details_view/static/src/js/action_report.js
@@ -4,16 +4,20 @@ import framework from 'web.framework';
 import session from 'web.session';
 
 registry.category("ir.actions.report handlers").add("xlsx", async (action) => {
-   if (action.report_type === 'xlsx') {
-       framework.blockUI();
-       var def = $.Deferred();
-       session.get_file({
-           url: '/xlsx_reports',
-           data: action.data,
-           success: def.resolve.bind(def),
-           error: (error) => this.call('crash_manager', 'rpc_error', error),
-           complete: framework.unblockUI,
-       });
-       return def;
-   }
+    try {
+        if (action.report_type === 'xlsx') {
+            framework.blockUI();
+            var def = $.Deferred();
+            session.get_file({
+                url: '/xlsx_reports',
+                data: action.data,
+                success: def.resolve.bind(def),
+                error: (error) => this.call('crash_manager', 'rpc_error', error),
+                complete: framework.unblockUI,
+            });
+            return def;
+        }   
+    } catch (error) {
+        console.log(error)
+    }
 });
