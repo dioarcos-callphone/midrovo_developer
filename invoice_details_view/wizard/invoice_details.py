0c03fd75ca00fba2fa64819d3ac74491905909fc
midrovom <midrovo@callphoneecuador.com>
2024-09-30 15:16:33 -0500

    fix-update: actualizando get report data

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -85,7 +85,7 @@ class InvoiceDetails(models.TransientModel):
                 # category = producto.categ_id
                 # account = category.property_account_expense_categ_id
                 
-                _logger.info(f'MOSTRANDO ACCOUNT >>> { detail.debit }')
+                _logger.info(f'MOSTRANDO ACCOUNT >>> { detail.move_name }')
                 
                 date_formated = datetime.strftime(detail.move_id.invoice_date, "%d/%m/%Y")
                 
