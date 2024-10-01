e1eb00b5612ea790e5f12d82a2475ad6ad3e1ad1
midrovom <midrovo@callphoneecuador.com>
2024-10-01 10:37:56 -0500

    fix-update: actualizando get report data

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -56,7 +56,7 @@ class InvoiceDetails(models.TransientModel):
         cashier = self.cashier_ids.ids
         
         domain = [
-            ('move_id', '!=', False)
+            ('move_id', '!=', False),
             ('product_id', '!=', False),
             ('date', '>=', fecha_inicio),
             ('date', '<=', fecha_fin),
