a3b9e23e6ff145b7bbdc871822a8076c8de513ea
midrovom <midrovo@callphoneecuador.com>
2024-10-09 14:47:23 -0500

    fix-add: agregando modulo custom security rules

---
+++ custom_security_rules/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Custom Sale Order and Partner Security',
+    'version': '1.0',
+    'category': 'Sales',
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    'depends': ['sale', 'contacts'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/sale_order_rules.xml',
+        'security/res_partner_rules.xml',
+        'data/groups.xml',
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
