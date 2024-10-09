32a57d407b163a7fe5b65ec482bc9810f30722a2
midrovom <midrovo@callphoneecuador.com>
2024-10-09 16:29:48 -0500

    fix-update: actualizar record

--- custom_security_rules/__manifest__.py
+++ custom_security_rules/__manifest__.py
@@ -1,19 +1,14 @@
 {
-    'name': 'Custom Sale Order and Partner Security',
+    'name': 'Custom Security',
     'version': '1.0',
-    'category': 'Sales',
     'author': 'Mauricio Idrovo',
     'company': 'Callphone S.A.',
     'website': "https://www.callphoneecuador.com",
-    'depends': ['sale', 'contacts'],
+    'depends': ['base', 'sale', 'contacts'],
     'data': [
-        'data/groups.xml',
-        # 'security/ir.model.access.csv',
-        'security/rules.xml',        
+        'security/ir.model.access.csv',
+        'security/custom_security_access.xml',
     ],
-    
-    'license': 'LGPL-3',
     'installable': True,
     'application': True,
-    'auto_install': True,
 }
