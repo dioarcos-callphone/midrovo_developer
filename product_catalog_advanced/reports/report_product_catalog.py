c00a42a8abf898012723eeed9fa71c80b55b0ad4
midrovom <midrovo@callphoneecuador.com>
2024-10-09 10:31:15 -0500

    fix-update: actualizando get_report_values

--- product_catalog_advanced/reports/report_product_catalog.py
+++ product_catalog_advanced/reports/report_product_catalog.py
@@ -8,16 +8,13 @@ class ReportProductCatalog(models.AbstractModel):
     
     @api.model
     def _get_report_values(self, docids, data=None):
-        productos = data['productos']  
-        
-        default_image = self.env['product.template'].default_get(['image_1920'])['image_1920']      
+        productos = data['productos']     
             
         if productos:
             return {
                 'doc_ids': docids,
                 'doc_model': 'report.product_catalog_advanced.product_catalog_template',
                 'options': productos,
-                'default': default_image
             }
             
         else:
