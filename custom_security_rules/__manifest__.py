baa26c6132fe0a6e0b983cae1beb68f30e19821f
midrovom <midrovo@callphoneecuador.com>
2024-10-14 15:38:44 -0500

    fix-update: actualizacion del views

--- custom_security_rules/__manifest__.py
+++ custom_security_rules/__manifest__.py
@@ -9,6 +9,7 @@
         'security/custom_security_access.xml',
         'security/ir.model.access.csv',
         'data/rules.xml',
+        'views/res_partner_form.xml',
     ],
     
     'license': 'LGPL-3',
