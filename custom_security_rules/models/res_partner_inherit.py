fe3907797a81dd1c0841ae489bfe00f9922f44d9
midrovom <midrovo@callphoneecuador.com>
2024-10-14 15:11:33 -0500

    fix-update: actualizacion del views

---
+++ custom_security_rules/models/res_partner_inherit.py
@@ -0,0 +1,14 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+
+class ResPartnerInherit(models.Model):
+    _inherit = 'res.partner'
+    
+    @api.model
+    def write(self, vals):
+        # Verificar si el usuario pertenece al grupo 'group_no_edit_delete'
+        if self.env.user.has_group('custom_security_rules.group_no_edit_delete'):
+            raise UserError("No tienes permiso para actualizar este registro.")
+        
+        # Si no pertenece al grupo, permite la escritura
+        return super(ResPartnerInherit, self).write(vals)
