67a56f378c0b4d17011f526657ddc2a89767357e
midrovom <midrovo@callphoneecuador.com>
2024-10-15 16:55:06 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -4,8 +4,8 @@ from odoo.exceptions import UserError
 class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.constrains('price_unit')
-    def _check_price_unit(self):
-        # Verifica si el usuario pertenece al grupo restringido
-        if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
-            raise UserError("No tiene permisos para modificar los precios.")
+    # @api.constrains('price_unit')
+    # def _check_price_unit(self):
+    #     # Verifica si el usuario pertenece al grupo restringido
+    #     if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
+    #         raise UserError("No tiene permisos para modificar los precios.")
