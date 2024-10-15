9870b27a230b6bfdde63e8ed73a25bec3b2b4cc9
midrovom <midrovo@callphoneecuador.com>
2024-10-15 18:06:54 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -6,9 +6,6 @@ class SaleOrderLineInherit(models.Model):
 
     @api.constrains('price_unit')
     def _check_price_unit(self):
-        for record in self:
-            # Compara el valor original con el actual
-            if record.price_unit != record._origin.price_unit:
-                # Verifica si el usuario pertenece al grupo restringido
-                if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
-                    raise UserError("No tiene permisos para modificar los precios.")
+        # Verifica si el usuario pertenece al grupo restringido
+        if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
+            raise UserError("No tiene permisos para modificar los precios.")
