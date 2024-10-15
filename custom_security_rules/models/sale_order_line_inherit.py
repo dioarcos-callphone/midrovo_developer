a3518237fbe3b09e2fe580569e707a7021ffe64b
midrovom <midrovo@callphoneecuador.com>
2024-10-15 12:35:33 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -4,7 +4,7 @@ from odoo.exceptions import UserError
 class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.constrains('price_unit')
+    @api.onchange('price_unit')
     def _check_price_unit(self):
         # Verifica si el usuario pertenece al grupo restringido
         if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
