c5c23933afd3fe24f37084601e5e6bed77998d9c
midrovom <midrovo@callphoneecuador.com>
2024-10-15 14:46:14 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -1,13 +1,19 @@
 from odoo import models, fields, api
-from odoo.exceptions import UserError
 
-class SaleOrderLineInherit(models.Model):
-    _inherit = 'sale.order.line'
-    
-    @api.depends('price_unit')  # Añade otros campos relevantes si es necesario
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.depends('order_line')
     def _compute_price_unit_readonly(self):
         group_id = self.env.ref('custom_security_rules.group_custom_security_role_user').id
         for record in self:
             record.price_unit_readonly = not self.env.user.has_group(group_id)
 
     price_unit_readonly = fields.Boolean(compute='_compute_price_unit_readonly', store=False)
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    price_unit = fields.Float(
+        readonly=lambda self: self.price_unit_readonly
+    )
