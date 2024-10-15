175c9a420c066fc3c2dfeaba065e3523c6aaad13
midrovom <midrovo@callphoneecuador.com>
2024-10-15 14:55:26 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -1,9 +1,8 @@
 from odoo import models, fields, api
 
 class SaleOrderLineInherit(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = 'sale.order'
 
-    @api.depends('order_id')  # Asegúrate de que este campo sea relevante para tu lógica
     def _compute_price_unit_readonly(self):
         group_id = self.env.ref('custom_security_rules.group_custom_security_role_user').id
         for record in self:
