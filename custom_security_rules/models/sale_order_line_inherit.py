23bc24e9972811f64bcab413306541ec2a935671
midrovom <midrovo@callphoneecuador.com>
2024-10-15 12:52:11 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -5,13 +5,16 @@ class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
     
     price_unit = fields.Float(
-        readonly=lambda self: self._check_readonly_price_unit()
+        readonly=lambda self: self.is_active()
     )
 
     def _check_readonly_price_unit(self):
         # Comprobar si el usuario pertenece a un grupo específico
         group_id = self.env.ref('custom_security_rules.group_custom_security_role_user').id
         return not self.env.user.has_group(group_id)
+    
+    def is_active(self):
+        return True
 
     # @api.depends('price_unit')
     # def _check_price_unit(self):
