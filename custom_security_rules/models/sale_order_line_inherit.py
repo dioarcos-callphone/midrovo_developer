f2f5ae26cb4f394bb3b436920124276c49f40c1b
midrovom <midrovo@callphoneecuador.com>
2024-10-15 16:16:12 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/sale_order_line_inherit.py
+++ custom_security_rules/models/sale_order_line_inherit.py
@@ -1,12 +1,12 @@
 from odoo import models, fields, api
 
-class SaleOrderLineInherit(models.Model):
-    _inherit = 'sale.order.line'
+class SaleOrderInherit(models.Model):
+    _inherit = 'sale.order'  # Cambiar a 'sale.order.line' si es necesario
 
     can_edit_price = fields.Boolean(compute='_compute_can_edit_price', store=True)
 
-    @api.depends('order_id.user_id')
+    @api.depends('user_id')  # Cambia esto a 'order_id.user_id' si estás en 'sale.order.line'
     def _compute_can_edit_price(self):
-        for line in self:
-            # Por ejemplo, puedes basarlo en el usuario que crea la orden o en otras reglas de negocio
-            line.can_edit_price = line.order_id.user_has_groups('custom_security_rules.group_custom_security_role_user')
+        for order in self:
+            # Aquí utilizas el método para verificar si el usuario pertenece al grupo
+            order.can_edit_price = order.user_has_groups('custom_security_rules.group_custom_security_role_user')
