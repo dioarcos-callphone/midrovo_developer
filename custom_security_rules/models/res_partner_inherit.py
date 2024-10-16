e2a6a515cb14216006bee57823d01afeb00b3f68
midrovom <midrovo@callphoneecuador.com>
2024-10-16 16:54:23 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/res_partner_inherit.py
+++ custom_security_rules/models/res_partner_inherit.py
@@ -1,4 +1,4 @@
-from odoo import models, api
+from odoo import models, api, _
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -10,24 +10,33 @@ class ResPartner(models.Model):
     @api.model
     def create(self, vals):
         # Obtener el grupo
-        group_user = self.env.ref('custom_security_rules.group_custom_security_role_user')
-        _logger.info('ENTRA')
-        # Obtener el permiso actual del grupo para res.partner
+        group_user = self.env.ref('your_module.group_custom_security_role_user')
+
+        # Verificar si el grupo se obtiene correctamente
+        if not group_user:
+            raise ValueError(_("El grupo no se encontró: 'group_custom_security_role_user'"))
+
+        # Buscar el permiso de acceso en ir.model.access
         write_permission = self.env['ir.model.access'].search([
             ('group_id', '=', group_user.id),
             ('model_id.model', '=', 'res.partner')
         ], limit=1)
-        
+
+        # Registrar en el log para verificar
         if write_permission:
-            _logger.info('ENTRA A LA CONDICION')
+            _logger.info(f"Permiso de escritura encontrado para el grupo {group_user.name}")
+        else:
+            _logger.warning(f"No se encontró el permiso de escritura para el grupo {group_user.name}")
+
+        if write_permission:
             # Habilitar el permiso de escritura temporalmente
             write_permission.perm_write = True
 
-        # Crear el registro
+        # Crear el registro de partner
         partner = super(ResPartner, self).create(vals)
 
-        # if write_permission:
-        #     # Deshabilitar el permiso de escritura nuevamente
-        #     write_permission.perm_write = False
+        if write_permission:
+            # Deshabilitar el permiso de escritura nuevamente
+            write_permission.perm_write = False
 
         return partner
