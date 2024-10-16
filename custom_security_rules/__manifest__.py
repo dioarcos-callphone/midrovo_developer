480afb18bc8f5aab488998def54ed9f6edafc1a9
midrovom <midrovo@callphoneecuador.com>
2024-10-16 10:13:54 -0500

    fix-update: actualizacion del views

--- custom_security_rules/__manifest__.py
+++ custom_security_rules/__manifest__.py
@@ -12,6 +12,13 @@
         'views/sale_order_form.xml'
     ],
     
+    'assets': {
+        'web.assets_backend': [
+            'custom_security_rules/static/js/custom_security_res_partner',
+            'custom_security_rules/static/xml/show_save_buttons_partner',
+        ]
+    },
+    
     'license': 'LGPL-3',
     'installable': True,
     'application': True,
---
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -0,0 +1,32 @@
+odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
+    "use strict";
+
+    var FormController = require('web.FormController');
+    var rpc = require('web.rpc');
+
+    FormController.include({
+        _onButtonNew: function (event) {
+            // Llamamos al método original para la creación de un nuevo registro
+            this._super.apply(this, arguments);
+
+            var self = this;
+
+            // Verificar permisos del usuario con una llamada RPC
+            rpc.query({
+                model: 'res.users',
+                method: 'has_group',
+                args: ['custom_security_rules.group_custom_security_role_user'],  // Verificar si pertenece al grupo
+            }).then(function (hasPermission) {
+                if (hasPermission) {
+                    // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
+                    self.$buttons.find('.o_form_button_save').show();
+                    self.$buttons.find('.o_form_button_cancel').show();
+                } else {
+                    // Ocultar los botones si el usuario no tiene permiso
+                    self.$buttons.find('.o_form_button_save').hide();
+                    self.$buttons.find('.o_form_button_cancel').hide();
+                }
+            });
+        },
+    });
+});
