49a9c8b55418769ff7b81d06abd1065fd802b73b
midrovom <midrovo@callphoneecuador.com>
2024-10-17 16:03:03 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/stock_picking_inherit.py
+++ custom_security_rules/models/stock_picking_inherit.py
@@ -4,7 +4,6 @@ from odoo.exceptions import AccessError
 class StockPickingInherit(models.Model):
     _inherit = 'stock.picking'
 
-    @api.model
     def unlink(self):
         # Verificar si el usuario pertenece al grupo restringido
         if self.env.user.has_group('custom_security_rules.group_custom_security_role_user_2'):
