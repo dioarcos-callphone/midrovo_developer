95bb01843e5a3ee32abbb4d76da67c751b1f121f
midrovom <midrovo@callphoneecuador.com>
2024-10-17 12:56:29 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -3,19 +3,11 @@ odoo.define('custom_security_rules.custom_security_res_partner', function (requi
 
     var FormController = require('web.FormController');
     var core = require('web.core');
-    var qweb = core.qweb;
-
-    var FormController = require('web.FormController');
 
     FormController.include({
-        constructor: function (parent, action) {
-            this._super(parent, action);
-            console.log('FormController extendido'); // Log para verificar que se extiende
-        },
         _onButtonNew: function (ev) {
-            this._super(ev);
-            console.log('¡Se hizo clic en el botón Nuevo!'); // Log al hacer clic en el botón Nuevo
+            console.log('¡Se hizo clic en el botón Nuevo!');
+            this._super(ev); // Asegúrate de que esto se llame después de tu código
         },
     });
-
 });
