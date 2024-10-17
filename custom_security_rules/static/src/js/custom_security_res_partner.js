7003f90fe69600506f24df3ce858aa2a22b32bca
midrovom <midrovo@callphoneecuador.com>
2024-10-17 12:48:21 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,38 +1,17 @@
 odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
-    "use strict";
+    'use strict';
 
-    // Importaciones necesarias
-    const KanbanController = require('@web/views/kanban/kanban_controller').KanbanController;
-    const { patch } = require('@web/core/utils/patch');
-    const rpc = require('web.rpc');
+    var FormController = require('web.FormController');
+    var core = require('web.core');
+    var qweb = core.qweb;
 
-    // Parchear el KanbanController para extender su funcionalidad
-    patch(KanbanController.prototype, 'custom_security_rules.custom_security_res_partner', {
-        createRecord() {
-            const self = this;
+    FormController.include({
+        // Método para inicializar el controlador
+        _onButtonNew: function (ev) {
+            this._super(ev); // Llama al método original para mantener la funcionalidad
 
-            // Llamar al método RPC para verificar el grupo
-            rpc.query({
-                model: 'res.users',
-                method: 'has_group',
-                args: ['custom_security_rules.group_custom_security_role_user'],  // Verificar si pertenece al grupo
-            }).then(function (hasPermission) {
-                if (hasPermission) {
-                    // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
-                    console.log('ENTRA TIENE PERMISOS')
-                    
-                    document.querySelectorAll('.o_form_button_save').forEach(button => {
-                        button.style.display = 'none';  // Oculta el botón
-                    });
-                    
-                    document.querySelectorAll('.o_form_button_cancel').forEach(button => {
-                        button.style.display = 'none';  // Oculta el botón
-                    });
-                }
-            });
-
-            // Llama al método original de createRecord
-            return this._super(...arguments);
+            // Agrega un console.log cuando se hace clic en el botón "Nuevo"
+            console.log('¡Se hizo clic en el botón Nuevo!');
         },
     });
 });
