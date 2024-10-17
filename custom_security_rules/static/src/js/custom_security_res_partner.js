f64dc1c4539c8fa2e6aff0196503a176c77f802f
midrovom <midrovo@callphoneecuador.com>
2024-10-17 11:17:19 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,17 +1,15 @@
 odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
     "use strict";
 
-    var FormController = require('web.KanbanController');
+    var FormController = require('web.FormController');
     var rpc = require('web.rpc');
 
     FormController.include({
-        _onButtonNew: function (event) {
-            // Llamamos al método original para la creación de un nuevo registro
-            this._super.apply(this, arguments);
-
+        _renderButtons: function () {
+            this._super.apply(this, arguments);  // Llamar al método original
             var self = this;
 
-            console.log('ENTRAMOS EN EL JAVASCRIPT ON BUTTON NEW')
+            console.log('Renderizando botones de formulario');
 
             // Verificar permisos del usuario con una llamada RPC
             rpc.query({
@@ -23,10 +21,6 @@ odoo.define('custom_security_rules.custom_security_res_partner', function (requi
                     // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
                     self.$buttons.find('.o_form_button_save').show();
                     self.$buttons.find('.o_form_button_cancel').show();
-                } else {
-                    // Ocultar los botones si el usuario no tiene permiso
-                    self.$buttons.find('.o_form_button_save').hide();
-                    self.$buttons.find('.o_form_button_cancel').hide();
                 }
             });
         },
