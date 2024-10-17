a44bde2c767a21e6c7c53280c262fa5c2e0b1624
midrovom <midrovo@callphoneecuador.com>
2024-10-17 12:27:48 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -9,17 +9,22 @@ odoo.define('custom_security_rules.custom_security_res_partner', function (requi
     // Parchear el KanbanController para extender su funcionalidad
     patch(KanbanController.prototype, 'custom_security_rules.custom_security_res_partner', {
         createRecord() {
-            // No es necesario usar self, ya que utilizamos una función flecha
+            // Realiza la consulta RPC para verificar el grupo
             rpc.query({
                 model: 'res.users',
                 method: 'has_group',
                 args: ['custom_security_rules.group_custom_security_role_user'],  // Verificar si pertenece al grupo
             }).then((hasPermission) => {  // Función flecha aquí
-                console.log(this);  // Ahora `this` se refiere a la instancia de KanbanController
+                console.log(this);  // Verifica el valor de `this`, que debe ser la instancia de KanbanController
                 if (hasPermission) {
                     console.log('ENTRA TIENE PERMISOS');
-                    this.$buttons.find('.o_form_button_save').show();
-                    this.$buttons.find('.o_form_button_cancel').show();
+                    // Usa this.$buttons para acceder a la propiedad de la instancia
+                    if (this.$buttons) {
+                        this.$buttons.find('.o_form_button_save').show();
+                        this.$buttons.find('.o_form_button_cancel').show();
+                    } else {
+                        console.error('this.$buttons is undefined');
+                    }
                 }
             });
 
