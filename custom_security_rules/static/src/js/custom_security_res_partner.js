0c75a50037f9bb3b5b02f5917f50b8b0ff667cbb
midrovom <midrovo@callphoneecuador.com>
2024-10-17 11:45:12 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,12 +1,16 @@
-/** @odoo-module **/
-import { KanbanController } from "@web/views/kanban/kanban_controller";  // Si es necesario manipular el controlador
-import { registry } from "@web/core/registry";  // Para manejar registros si lo necesitas
+odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
+    "use strict";
 
-const KanbanControllerExtended = KanbanController.extend({
-    createRecord: function () {
-        console.log("Botón Nuevo presionado!");
-        this._super();
-    }
+    // Importaciones necesarias
+    const KanbanController = require('@web/views/kanban/kanban_controller').KanbanController;
+    const { patch } = require('@web/core/utils/patch');
+
+    // Parchear el KanbanController para extender su funcionalidad
+    patch(KanbanController.prototype, 'custom_security_rules.custom_security_res_partner', {
+        createRecord() {
+            console.log("Botón Nuevo presionado!");
+            return this._super(...arguments);  // Llama al método original
+        },
+    });
+
 });
-
-registry.category("views").add("kanban", KanbanControllerExtended);
