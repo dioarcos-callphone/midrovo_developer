3be1f9168ad1113eb53eb01424cfd8d147696cde
midrovom <midrovo@callphoneecuador.com>
2024-10-17 16:52:31 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,42 +1,20 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
-import { FormStatusIndicator } from "web.FormStatusIndicator";
+import { Component } from 'owl';
+import { useSession } from '@web/core/session';
+import { registry } from '@web/core/registry';
 
-export class CustomFormStatusIndicator extends FormStatusIndicator {
-    get displayButtons() {
-        // Condición para mostrar los botones solo para un grupo específico
-        const userHasGroup = this.env.services['auth_service'].isUserInGroup('your_specific_group_xml_id');
-        return userHasGroup && super.displayButtons;
+class MyComponent extends Component {
+    setup() {
+        super.setup(...arguments);
+        this.session = useSession();
+        this.userHasEditAccess = this.hasEditPermissions();
     }
 
-    async discard() {
-        // Lógica adicional para descartar
-        if (this.shouldDiscard()) {
-            await super.discard();
-        } else {
-            // Opcional: Mostrar un mensaje o manejar la lógica si no se puede descartar
-        }
-    }
-
-    async save() {
-        // Lógica adicional para guardar
-        if (this.shouldSave()) {
-            await super.save();
-        } else {
-            // Opcional: Mostrar un mensaje o manejar la lógica si no se puede guardar
-        }
-    }
-
-    shouldDiscard() {
-        // Tu lógica de condición para descartar cambios
-        return true; // Cambia esto según tu lógica
-    }
-
-    shouldSave() {
-        // Tu lógica de condición para guardar
-        return true; // Cambia esto según tu lógica
+    hasEditPermissions() {
+        // Verificar si el usuario pertenece al grupo
+        return this.session.user_groups.includes('custom_security_rules.group_custom_security_role_user_2');
     }
 }
 
-CustomFormStatusIndicator.template = "custom_security_rules.CustomFormStatusIndicator";
+registry.category('actions').add('my_component', MyComponent);
