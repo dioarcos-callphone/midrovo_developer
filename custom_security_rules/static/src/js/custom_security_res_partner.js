9bd5e975f39afc8114d21d6281a6cf879a7cbb21
midrovom <midrovo@callphoneecuador.com>
2024-10-17 12:32:57 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -9,22 +9,19 @@ odoo.define('custom_security_rules.custom_security_res_partner', function (requi
     // Parchear el KanbanController para extender su funcionalidad
     patch(KanbanController.prototype, 'custom_security_rules.custom_security_res_partner', {
         createRecord() {
-            // Realiza la consulta RPC para verificar el grupo
+            const self = this;
+
+            // Llamar al método RPC para verificar el grupo
             rpc.query({
                 model: 'res.users',
                 method: 'has_group',
                 args: ['custom_security_rules.group_custom_security_role_user'],  // Verificar si pertenece al grupo
-            }).then((hasPermission) => {  // Función flecha aquí
-                console.log(this);  // Verifica el valor de `this`, que debe ser la instancia de KanbanController
+            }).then(function (hasPermission) {
                 if (hasPermission) {
-                    console.log('ENTRA TIENE PERMISOS');
-                    // Usa this.$buttons para acceder a la propiedad de la instancia
-                    if (this.$buttons) {
-                        this.$buttons.find('.o_form_button_save').show();
-                        this.$buttons.find('.o_form_button_cancel').show();
-                    } else {
-                        console.error('this.$buttons is undefined');
-                    }
+                    // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
+                    console.log('ENTRA TIENE PERMISOS')
+                    $('.o_form_button_save').show();
+                    $('.o_form_button_cancel').show();
                 }
             });
 
