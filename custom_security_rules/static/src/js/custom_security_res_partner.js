bf7cf21b26bff204fd67198c62e05e3d1a8d8fba
midrovom <midrovo@callphoneecuador.com>
2024-10-17 16:38:13 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,13 +1,42 @@
-odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
-    'use strict';
+/** @odoo-module **/
 
-    var FormController = require('web.FormController');
-    var core = require('web.core');
+import { Component } from "@odoo/owl";
+import { FormStatusIndicator } from "web.FormStatusIndicator";
 
-    FormController.include({
-        _onButtonNew: function (ev) {
-            console.log('¡Se hizo clic en el botón Nuevo!');
-            this._super(ev); // Asegúrate de que esto se llame después de tu código
-        },
-    });
-});
+export class CustomFormStatusIndicator extends FormStatusIndicator {
+    get displayButtons() {
+        // Condición para mostrar los botones solo para un grupo específico
+        const userHasGroup = this.env.services['auth_service'].isUserInGroup('your_specific_group_xml_id');
+        return userHasGroup && super.displayButtons;
+    }
+
+    async discard() {
+        // Lógica adicional para descartar
+        if (this.shouldDiscard()) {
+            await super.discard();
+        } else {
+            // Opcional: Mostrar un mensaje o manejar la lógica si no se puede descartar
+        }
+    }
+
+    async save() {
+        // Lógica adicional para guardar
+        if (this.shouldSave()) {
+            await super.save();
+        } else {
+            // Opcional: Mostrar un mensaje o manejar la lógica si no se puede guardar
+        }
+    }
+
+    shouldDiscard() {
+        // Tu lógica de condición para descartar cambios
+        return true; // Cambia esto según tu lógica
+    }
+
+    shouldSave() {
+        // Tu lógica de condición para guardar
+        return true; // Cambia esto según tu lógica
+    }
+}
+
+CustomFormStatusIndicator.template = "custom_security_rules.CustomFormStatusIndicator";
