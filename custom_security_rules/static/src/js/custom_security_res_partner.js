ac229adcdfacf837273e6d4398981f7542420a85
midrovom <midrovo@callphoneecuador.com>
2024-10-17 12:09:09 -0500

    fix-update: actualizacion del views

--- custom_security_rules/static/src/js/custom_security_res_partner.js
+++ custom_security_rules/static/src/js/custom_security_res_partner.js
@@ -1,32 +1,38 @@
-odoo.define('custom_security_rules.custom_security_res_partner', function (require) {
+odoo.define('custom_security_rules.custom_form_controller', function (require) {
     "use strict";
 
-    // Importaciones necesarias
-    const KanbanController = require('@web/views/kanban/kanban_controller').KanbanController;
-    const { patch } = require('@web/core/utils/patch');
+    const FormController = require('web.FormController');
     const rpc = require('web.rpc');
 
-    // Parchear el KanbanController para extender su funcionalidad
-    patch(KanbanController.prototype, 'custom_security_rules.custom_security_res_partner', {
-        createRecord() {
+    // Extender el FormController
+    FormController.include({
+        async render() {
+            // Llama al método original
+            await this._super(...arguments);
+            
+            // Llama a tu función para verificar permisos
+            this.checkUserPermissions();
+        },
+
+        async checkUserPermissions() {
             const self = this;
 
             // Llamar al método RPC para verificar el grupo
-            rpc.query({
+            const hasPermission = await rpc.query({
                 model: 'res.users',
                 method: 'has_group',
                 args: ['custom_security_rules.group_custom_security_role_user'],  // Verificar si pertenece al grupo
-            }).then(function (hasPermission) {
-                if (hasPermission) {
-                    // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
-                    console.log('ENTRA TIENE PERMISOS')
-                    self.$buttons.find('.o_form_button_save').show();
-                    self.$buttons.find('.o_form_button_cancel').show();
-                }
             });
 
-            // Llama al método original de createRecord
-            return this._super(...arguments);
+            if (hasPermission) {
+                // Mostrar los botones de Guardar y Descartar si el usuario tiene permiso
+                self.$buttons.find('.o_form_button_save').show();
+                self.$buttons.find('.o_form_button_cancel').show();
+            } else {
+                // Ocultar los botones si el usuario no tiene permiso
+                self.$buttons.find('.o_form_button_save').hide();
+                self.$buttons.find('.o_form_button_cancel').hide();
+            }
         },
     });
 });
