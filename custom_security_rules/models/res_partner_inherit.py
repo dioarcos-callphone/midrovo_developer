4e053f0b4da3c33b4c74ca0b84bacbdaca056f72
midrovom <midrovo@callphoneecuador.com>
2024-10-18 17:27:32 -0500

    fix-update: actualizacion del views

--- custom_security_rules/models/res_partner_inherit.py
+++ custom_security_rules/models/res_partner_inherit.py
@@ -8,38 +8,43 @@ _logger = logging.getLogger(__name__)
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    # @api.model
+    # def create(self, vals):
+    #     # Obtener el grupo
+    #     group_user = self.env.ref('custom_security_rules.group_custom_security_role_user')
+    #     _logger.info('ENTRA EN EL CREATE')
+    #     # Verificar si el grupo se obtiene correctamente
+    #     if not group_user:
+    #         raise ValueError(_("El grupo no se encontró: 'group_custom_security_role_user'"))
+
+    #     # Buscar el permiso de acceso en ir.model.access
+    #     write_permission = self.env['ir.model.access'].sudo().search([
+    #         ('group_id', '=', group_user.id),
+    #         ('model_id.model', '=', 'res.partner')
+    #     ], limit=1)
+
+    #     if write_permission:
+    #         _logger.info('ENTRA AQUI')
+    #         # Habilitar el permiso de escritura temporalmente
+    #         write_permission.perm_write = True
+
+    #     # Crear el registro de partner
+    #     partner = super(ResPartner, self).create(vals)
+
+    #     if write_permission:
+    #         # Deshabilitar el permiso de escritura nuevamente
+    #         write_permission.perm_write = False
+
+    #     return partner
+
     @api.model
     def create(self, vals):
-        # Obtener el grupo
-        group_user = self.env.ref('custom_security_rules.group_custom_security_role_user')
-        _logger.info('ENTRA EN EL CREATE')
-        # Verificar si el grupo se obtiene correctamente
-        if not group_user:
-            raise ValueError(_("El grupo no se encontró: 'group_custom_security_role_user'"))
+        # Permitir creación sin restricciones
+        return super(ResPartner, self).create(vals)
 
-        # Buscar el permiso de acceso en ir.model.access
-        write_permission = self.env['ir.model.access'].sudo().search([
-            ('group_id', '=', group_user.id),
-            ('model_id.model', '=', 'res.partner')
-        ], limit=1)
-
-        if write_permission:
-            _logger.info('ENTRA AQUI')
-            # Habilitar el permiso de escritura temporalmente
-            write_permission.perm_write = True
-
-        # Crear el registro de partner
-        partner = super(ResPartner, self).create(vals)
-
-        if write_permission:
-            # Deshabilitar el permiso de escritura nuevamente
-            write_permission.perm_write = False
-
-        return partner
-
-    # def write(self, vals):
-    #     # Solo bloquear si el registro ya existe
-    #     if self and self.ids:  # Si el registro ya tiene un ID, es una actualización
-    #         if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
-    #             raise UserError("You are not allowed to update records.")
-    #     return super(ResPartner, self).write(vals)
+    def write(self, vals):
+        # Verificar si el usuario pertenece al grupo que solo debe crear contactos
+        if self.env.user.has_group('custom_security_rules.group_custom_security_role_user'):
+            raise UserError(_('No tiene permisos para actualizar contactos.'))
+        # Si no pertenece al grupo, proceder con la actualización
+        return super(ResPartner, self).write(vals)
