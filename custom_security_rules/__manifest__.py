089f5e6f2563c9a7a674045ec0f302cf6b148f71
midrovom <midrovo@callphoneecuador.com>
2024-10-18 15:50:15 -0500

    fix-update: actualizacion del views

--- custom_security_rules/__manifest__.py
+++ custom_security_rules/__manifest__.py
@@ -17,7 +17,8 @@
         'security/rules/security_rules_data.xml',
         'security/ir.model.access.csv',
         'views/sale_order_form.xml',
-        'views/stock_picking_form.xml'
+        'views/stock_picking_form.xml',
+        'views/res_partner_form.xml'
         
     ],
     
