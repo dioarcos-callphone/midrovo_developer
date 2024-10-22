bc3d29cff13cb3b0f94bd86ed36e3c7ccfaa568f
midrovom <midrovo@callphoneecuador.com>
2024-10-22 15:46:54 -0500

    fix-update: haciendo log en el make report xls

--- ec_kardex/reports/report_stock_utils.py
+++ ec_kardex/reports/report_stock_utils.py
@@ -476,7 +476,7 @@ class ReportStockUtils(models.AbstractModel):
 					"partner": move.picking_id and move.picking_id.name or '',
 					"qty_in": qty_in,
 					"qty_out": qty_out,
-					"balance": 'total_qty_in-total_qty_out',
+					"balance": 0,
 				})
 		else:
 			lines.append({
@@ -493,7 +493,7 @@ class ReportStockUtils(models.AbstractModel):
 				"partner": "",
 				"qty_in": total_qty_in,
 				"qty_out": total_qty_out,
-				"balance": 'total_qty_in - total_qty_out',
+				"balance": 0,
 			})
 		return lines
 
