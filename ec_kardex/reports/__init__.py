65ae3b9fa3218b7c81275f7136bd85f172245d51
midrovom <midrovo@callphoneecuador.com>
2024-10-23 17:58:26 -0500

    fix-update: actualizando modulo

--- ec_kardex/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import report_stock_utils
-from . import report_kardex_all
-from . import report_kardex_individual
-from . import report_kardex_all_stock 
--- ec_kardex/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-from . import wizard_kardex_individual_report
-from . import wizard_kardex_all_report
-from . import wizard_kardex_all_stock_report
-
-
-
