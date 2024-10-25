d9e8763422b2477b5faf3c81014e2f04c27d0249
midrovom <midrovo@callphoneecuador.com>
2024-10-25 11:48:53 -0500

    fix-update: se realizan logs de prueba para ver metodos de pago

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -241,8 +241,6 @@ class InvoiceDetails(models.TransientModel):
                 'is_cost_or_debit': self.cost_options
             }
             
-            _logger.info(f'MOSTRANDO DATA >>> { data }')
-            
             return data
         
         else:
@@ -348,6 +346,7 @@ class InvoiceDetails(models.TransientModel):
             'Precio',
             'Descuento',
             'Subtotal',
+            'Métodos de Pago',
         ]
         
         if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
@@ -377,11 +376,12 @@ class InvoiceDetails(models.TransientModel):
         sheet.set_column('P:P', 9)  # Precio
         sheet.set_column('Q:Q', 11)  # Descuento
         sheet.set_column('R:R', 8)  # Subtotal
+        sheet.set_column('S:S', 20)  # Metodos de Pago
         
         if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
-            sheet.set_column('S:S', 9)  # Costo o Debito
-            sheet.set_column('T:T', 12)  # Total Costo
-            sheet.set_column('U:U', 13)  # Rentabilidad
+            sheet.set_column('T:T', 9)  # Costo o Debito
+            sheet.set_column('U:U', 12)  # Total Costo
+            sheet.set_column('V:V', 13)  # Rentabilidad
 
         # Escribir datos
         row = 3  # Comenzar desde la fila 3 después de los encabezados
@@ -405,14 +405,18 @@ class InvoiceDetails(models.TransientModel):
             sheet.write(row, 16, val['descuento'], text_format)
             sheet.write(row, 17, val['subtotal'], text_format)
             
+            metodo_pago = val['metodo_pago']
+            metodo_pago_str = "\n".join(metodo_pago)  # Unir elementos con salto de línea
+            sheet.write(row, 18, metodo_pago_str, text_format)
+            
             if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
                 if is_cost_or_debit == 'master':
-                    sheet.write(row, 18, val['costo'], text_format)
+                    sheet.write(row, 19, val['costo'], text_format)
                 elif is_cost_or_debit == 'movement':
-                    sheet.write(row, 18, val['debito'], text_format)
+                    sheet.write(row, 19, val['debito'], text_format)
                 
-                sheet.write(row, 19, val['total_costo'], text_format)
-                sheet.write(row, 20, val['rentabilidad'], text_format)
+                sheet.write(row, 20, val['total_costo'], text_format)
+                sheet.write(row, 21, val['rentabilidad'], text_format)
             
             row += 1
 
