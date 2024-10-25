38dcce7674a06458a7a7bde648b9637fe375fa9f
midrovom <midrovo@callphoneecuador.com>
2024-10-25 12:48:25 -0500

    fix-update: se realizan logs de prueba para ver metodos de pago

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -327,9 +327,9 @@ class InvoiceDetails(models.TransientModel):
 
         # Título del informe
         if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
-            sheet.merge_range('A1:U1', 'Informe de Detalles de Facturas y Notas de Crédito', title_format)
+            sheet.merge_range('A1:V1', 'Informe de Detalles de Facturas y Notas de Crédito', title_format)
         else:
-            sheet.merge_range('A1:T1', 'Informe de Detalles de Facturas y Notas de Crédito', title_format)
+            sheet.merge_range('A1:S1', 'Informe de Detalles de Facturas y Notas de Crédito', title_format)
 
         # Encabezados
         headers = [
