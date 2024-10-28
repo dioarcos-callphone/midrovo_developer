cf49abd5026dfe7b349e4d9dc0323b5d5f1637d4
midrovom <midrovo@callphoneecuador.com>
2024-10-28 11:49:54 -0500

    fix-update: actualizacion de get_xlsx_report

--- invoice_details_view/report/invoice_details.py
+++ invoice_details_view/report/invoice_details.py
@@ -137,8 +137,12 @@ class InvoiceDetails(models.AbstractModel):
                         for payment in pos_order.payment_ids:
                             if method['name'] == payment.payment_method_id.name:
                                 data_detail[method['name']] = payment.amount
-                                
-                _logger.info(f'MOSTRANDO CONTENT >>> {detail.move_id.invoice_payments_widget}')
+                    else:
+                        journal_name = detail.move_id.invoice_payments_widget.journal_name
+                        amount = detail.move_id.invoice_payments_widget.amount
+                        if method['name'] == journal_name:
+                            data_detail[method['name']] = amount
+
                     
                 data_invoice_details.append(data_detail)
             
