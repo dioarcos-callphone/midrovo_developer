7f14bbaa68663a6986e6c9cf43356d8f28d26952
midrovom <midrovo@callphoneecuador.com>
2024-10-28 16:09:44 -0500

    fix-update: actualizacion de get_xlsx_report

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -473,6 +473,15 @@ class InvoiceDetails(models.TransientModel):
             sheet.write(row, 4, val['comercial'], text_format)
             sheet.write(row, 5, val['pos'], text_format)
             sheet.write(row, 6, val['cliente'], text_format)
+            
+            if is_resumen == 'r':
+                sheet.write(row, 7, val['subtotal'], text_format)
+                sheet.write(row, 8, val['iva'], text_format)
+                sheet.write(row, 9, val['total'], text_format)
+                sheet.write(row, 10, val['Efectivo'], text_format)
+                sheet.write(row, 11, val['Banco'], text_format)
+                sheet.write(row, 12, val['Cuenta de cliente'], text_format)
+            
             if is_resumen == 'd':
                 sheet.write(row, 7, val['producto'], text_format)
                 sheet.write(row, 8, val['marca'], text_format)
@@ -485,19 +494,19 @@ class InvoiceDetails(models.TransientModel):
                 sheet.write(row, 15, val['cantidad'], text_format)
                 sheet.write(row, 16, val['precio'], text_format)
                 sheet.write(row, 17, val['descuento'], text_format)
-            sheet.write(row, 18, val['subtotal'], text_format)
-            sheet.write(row, 19, val['Efectivo'], text_format)
-            sheet.write(row, 20, val['Banco'], text_format)
-            sheet.write(row, 21, val['Cuenta de cliente'], text_format)
+                sheet.write(row, 18, val['subtotal'], text_format)
+                sheet.write(row, 19, val['Efectivo'], text_format)
+                sheet.write(row, 20, val['Banco'], text_format)
+                sheet.write(row, 21, val['Cuenta de cliente'], text_format)
             
-            if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
-                if is_cost_or_debit == 'master':
-                    sheet.write(row, 22, val['costo'], text_format)
-                elif is_cost_or_debit == 'movement':
-                    sheet.write(row, 22, val['debito'], text_format)
-                
-                sheet.write(row, 23, val['total_costo'], text_format)
-                sheet.write(row, 24, val['rentabilidad'], text_format)
+                if not self.env.user.has_group('invoice_details_view.group_invoice_details_view_user'):
+                    if is_cost_or_debit == 'master':
+                        sheet.write(row, 22, val['costo'], text_format)
+                    elif is_cost_or_debit == 'movement':
+                        sheet.write(row, 22, val['debito'], text_format)
+                    
+                    sheet.write(row, 23, val['total_costo'], text_format)
+                    sheet.write(row, 24, val['rentabilidad'], text_format)
             
             row += 1
 
