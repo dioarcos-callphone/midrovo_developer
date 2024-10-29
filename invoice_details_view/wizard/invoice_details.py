7e98baf28bbab3c5bba00bf03f4dfafefee326c6
midrovom <midrovo@callphoneecuador.com>
2024-10-29 12:40:07 -0500

    fix-update: actualizacion de get_xlsx_report

--- invoice_details_view/wizard/invoice_details.py
+++ invoice_details_view/wizard/invoice_details.py
@@ -130,7 +130,7 @@ class InvoiceDetails(models.TransientModel):
                             
                             for c in content:
                                 content_journal_type = self.env['account.journal'].search_read([],['type'])
-                                if method.name == c['journal_name'] and journal_type == content_journal_type:
+                                if method.name == c['journal_name'] and journal_type == content_journal_type['type']:
                                     _logger.info(f'MOSTRANDO JOURNAL TYPE { journal_type }')
                                     _logger.info(f"MOSTRANDO PAYMENT METHOD { c['journal_name'] }")
                                     
