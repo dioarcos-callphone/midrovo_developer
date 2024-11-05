372970dd932b03e361d73dcb0d7f65334a459ac3
midrovom <midrovo@callphoneecuador.com>
2024-11-05 17:30:39 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_line_inherit.py
+++ account_analytics/models/account_move_line_inherit.py
@@ -11,9 +11,13 @@ class AccountMoveLineInherit(models.Model):
     def default_get(self, fields_list):
         res = super(AccountMoveLineInherit, self).default_get(fields_list)
         # Definir la cuenta analítica por defecto (ID de la cuenta analítica)
-        default_analytic_account_id = self.env['account.analytic.account'].search([('name', '=', 'Proyecto XYZ')], limit=1)
+        # default_analytic_account_id = self.env['account.analytic.account'].search([('name', '=', 'Proyecto XYZ')], limit=1)
+        
+        analytic_account = self.move_id.journal_id.analytic_id
+        
+        if analytic_account:
         
         # Establecer la distribución analítica por defecto si existe la cuenta
-        if default_analytic_account_id:
-            res['analytic_distribution'] = {str(default_analytic_account_id.id): 100}  # Distribuir 100% a esa cuenta
+        # if default_analytic_account_id:
+            res['analytic_distribution'] = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
         return res
