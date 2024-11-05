f877aa51c051e099c21b816eb9a5bf4c24ae4036
midrovom <midrovo@callphoneecuador.com>
2024-11-05 12:39:05 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/__manifest__.py
+++ account_analytics/__manifest__.py
@@ -12,6 +12,7 @@
     'website': "https://www.callphoneecuador.com",
     'depends': [ 'account', ],
     'data': [
+        '/views/journal_form_inherit.xml',
     ],
     
     'license': 'LGPL-3',
