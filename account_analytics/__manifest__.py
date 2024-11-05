b3a9a2ede9131409364ae2a0dbf6269ce3fa22f3
midrovom <midrovo@callphoneecuador.com>
2024-11-05 12:12:29 -0500

    fix-add: agregando modulo account analytics

---
+++ account_analytics/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Personalización de Cuentas Analíticas',
+    'summary': '',
+    'description': """
+        
+    """,
+    
+    "category": "Account",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'account', ],
+    'data': [
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
