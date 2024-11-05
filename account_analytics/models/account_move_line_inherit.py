23557a0383b03a52ce0f6c1f553e74b13fc6f943
midrovom <midrovo@callphoneecuador.com>
2024-11-05 16:15:06 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_line_inherit.py
+++ account_analytics/models/account_move_line_inherit.py
@@ -16,7 +16,7 @@ class AccountMoveLineInherit(models.AbstractModel):
         if default_analytic_account_id:
             res['analytic_distribution'] = {str(default_analytic_account_id.id): 100}  # Distribuir 100% a esa cuenta
         
-        if self:
+        if self.journal_id and self.account_id:
             _logger.info(f'MOSTRANDO ACCOUNT MOVE LINE >>> { self.journal_id }')
             _logger.info(f'MOSTRANDO ACCOUNT MOVE LINE >>> { self.account_id }')
         return res
