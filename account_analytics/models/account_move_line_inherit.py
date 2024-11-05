8a5288a8329ffcefa914369cb3ee1fe692d3678a
midrovom <midrovo@callphoneecuador.com>
2024-11-05 17:09:13 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_line_inherit.py
+++ account_analytics/models/account_move_line_inherit.py
@@ -12,15 +12,12 @@ class AccountMoveLineInherit(models.AbstractModel):
         res = super(AccountMoveLineInherit, self).default_get(fields_list)
         
         if self.move_id.journal_id and self.account_id:
-            _logger.info(f'CUENTA ANALITICA >>> { self.move_id.journal_id.analytic_id }')
             analytic_account = self.move_id.journal_id.analytic_id
             
             # Establecer la distribución analítica por defecto si existe la cuenta
-            if analytic_account:
-                code_account = self.account_id.code
-                
-                # Si el codigo de la cuenta contiene 4 o 5 se establece la cuenta analitica
-                # if code_account.startswith(('4', '5')):
+            if analytic_account:                
+                _logger.info(f'CUENTA ANALITICA >>> { analytic_account }')
+
                 res['analytic_distribution'] = {str(analytic_account.id): 100}
                 
         return res
