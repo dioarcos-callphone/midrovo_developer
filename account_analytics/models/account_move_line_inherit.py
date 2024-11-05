4c356364a85f438b0808a1dcec9dae5c81dbdf55
midrovom <midrovo@callphoneecuador.com>
2024-11-05 15:11:24 -0500

    fix-update: actualizando manifest y vistas

---
+++ account_analytics/models/account_move_line_inherit.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+class AccountMoveLineInherit(models.AbstractModel):
+    _inherit = 'account.move.line'
+    
+    @api.model
+    def default_get(self, fields_list):
+        res = super(AccountMoveLineInherit, self).default_get(fields_list)
+        # Definir la cuenta analítica por defecto (ID de la cuenta analítica)
+        default_analytic_account_id = self.env['account.analytic.account'].search([('name', '=', 'Proyecto XYZ')], limit=1)
+        
+        # Establecer la distribución analítica por defecto si existe la cuenta
+        if default_analytic_account_id:
+            res['analytic_distribution'] = {str(default_analytic_account_id.id): 100}  # Distribuir 100% a esa cuenta
+        return res
