21c8d67e8ec597f494566650f8271b4de62a7b34
midrovom <midrovo@callphoneecuador.com>
2024-11-06 11:09:56 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_inherit.py
+++ account_analytics/models/account_move_inherit.py
@@ -13,23 +13,12 @@ class AccountMoveInherit(models.Model):
         Se puede usar para actualizar las cuentas analíticas en las líneas del asiento.
         """
         if self:
-            _logger.info(f"Journal changed to: {self.journal_id.name}")
             for line in self.line_ids:
                 if self.journal_id.analytic_id:
-                    _logger.info(f"Setting analytic distribution for line {line.id}")
+                    _logger.info(f'MOSTRANDO ACCOUNT >>> { line.account_id.type }')
                     line.analytic_distribution = { str(self.journal_id.analytic_id.id): 100 }
-                else:
-                    _logger.warning("No analytic account found for the selected journal.")
 
 class AccountMoveLineInherit(models.Model):
     _inherit = "account.move.line"
     
-    @api.depends('move_id.journal_id')    
-    def _compute_journal_id(self):
-        if self.move_id:
-            analytic_account = self.move_id.journal_id.analytic_id
-            if analytic_account:
-                _logger.info(f"Computing analytic distribution for line {self.id}")
-                self.analytic_distribution = { str(analytic_account.id): 100 }  # Distribuir 100% a esa cuenta
-            else:
-                _logger.warning(f"No analytic account found for journal in move {self.move_id.id}")
+    
