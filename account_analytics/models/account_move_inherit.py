26eb7d102c60cabf65d23ec47cc08e7a05188a41
midrovom <midrovo@callphoneecuador.com>
2024-11-06 10:25:54 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_inherit.py
+++ account_analytics/models/account_move_inherit.py
@@ -7,26 +7,6 @@ _logger = logging.getLogger(__name__)
 class AccountMoveInherit(models.Model):
     _inherit = 'account.move'
     
-    # analytic_distribution = fields.Json(
-    #     default_get = lambda self: {
-    #         str(self.env['account.analytic.account'].search([('name', '=', self.journal_id.analytic_id)], limit=1).id): 100
-    #     },
-    #     inverse="_inverse_analytic_distribution",
-    # )
-    
-    # @api.model
-    # def default_get(self, fields_list):
-    #     if self.move_id:
-    #         analytic_account = self.move_id.journal_id.analytic_id
-    #         self.analytic_distribution = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
-        
-    #     return super(AccountMoveLineInherit, self).default_get(fields_list)
-    
-    # @api.onchange('move_id.journal_id')
-    # def onchange_move_id(self):
-    #     analytic_account = self.move_id.journal_id.analytic_id
-    #     self.move_analytic_distributionid = {str(analytic_account.id): 100}
-    
     @api.onchange('journal_id')
     def onchange_journal(self):
         """
@@ -46,9 +26,7 @@ class AccountMoveLineInherit(models.Model):
     def default_get(self, fields_list):
         if self.move_id:
             analytic_account = self.move_id.journal_id.analytic_id
-            self.analytic_distribution = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
+            if analytic_account:
+                self.analytic_distribution = { str(analytic_account.id): 100 }  # Distribuir 100% a esa cuenta
         
         return super(AccountMoveLineInherit, self).default_get(fields_list)
-    
-    
-    
