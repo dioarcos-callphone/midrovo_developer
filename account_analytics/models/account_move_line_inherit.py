974928ef2ec53699e7450a8dd976aee48edcda6b
midrovom <midrovo@callphoneecuador.com>
2024-11-06 09:30:09 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_line_inherit.py
+++ account_analytics/models/account_move_line_inherit.py
@@ -17,7 +17,13 @@ class AccountMoveLineInherit(models.Model):
     @api.model
     def default_get(self, fields_list):
         if self.move_id:
-            analytic_account = self.journal_id.analytic_id
+            analytic_account = self.move_id.journal_id.analytic_id
             self.analytic_distribution = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
         
         return super(AccountMoveLineInherit, self).default_get(fields_list)
+    
+    # @api.onchange('move_id')
+    # def onchange_move_id(self):
+    #     analytic_account = self.journal_id.analytic_id
+    #     self.move_analytic_distributionid = 
+    
