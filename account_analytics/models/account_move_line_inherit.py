a1e019d0b24966f33290c4f2416d551a1dcf6909
midrovom <midrovo@callphoneecuador.com>
2024-11-06 09:44:35 -0500

    fix-update: actualizando manifest y vistas

--- account_analytics/models/account_move_line_inherit.py
+++ account_analytics/models/account_move_line_inherit.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountMoveLineInherit(models.Model):
-    _inherit = 'account.move.line'
+    _inherit = 'account.move'
     
     # analytic_distribution = fields.Json(
     #     default_get = lambda self: {
@@ -14,16 +14,28 @@ class AccountMoveLineInherit(models.Model):
     #     inverse="_inverse_analytic_distribution",
     # )
     
-    @api.model
-    def default_get(self, fields_list):
-        if self.move_id:
-            analytic_account = self.move_id.journal_id.analytic_id
-            self.analytic_distribution = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
+    # @api.model
+    # def default_get(self, fields_list):
+    #     if self.move_id:
+    #         analytic_account = self.move_id.journal_id.analytic_id
+    #         self.analytic_distribution = {str(analytic_account.id): 100}  # Distribuir 100% a esa cuenta
         
-        return super(AccountMoveLineInherit, self).default_get(fields_list)
+    #     return super(AccountMoveLineInherit, self).default_get(fields_list)
     
-    @api.onchange('move_id.journal_id')
-    def onchange_move_id(self):
-        analytic_account = self.move_id.journal_id.analytic_id
-        self.move_analytic_distributionid = {str(analytic_account.id): 100}
+    # @api.onchange('move_id.journal_id')
+    # def onchange_move_id(self):
+    #     analytic_account = self.move_id.journal_id.analytic_id
+    #     self.move_analytic_distributionid = {str(analytic_account.id): 100}
+    
+    @api.onchange('journal_id')
+    def onchange_journal_Id(self):
+        """
+        Cuando el usuario cambia el diario (journal_id), este método se ejecuta.
+        Se puede usar para actualizar las cuentas analíticas en las líneas del asiento.
+        
+        """
+        for line in self.line_ids:
+            if self.journal_id.analytic_id:
+                line.analytic_distribution = { str(self.journal_id.analytic_id.id): 100 }
+    
     
