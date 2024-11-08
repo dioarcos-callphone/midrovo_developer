edb5d9f97988bcf64bc7e61901e3315d3702250e
midrovom <midrovo@callphoneecuador.com>
2024-11-08 17:19:08 -0500

    fix-update: actualizando archivos views y models

--- credit_card_pos/models/credit_card.py
+++ credit_card_pos/models/credit_card.py
@@ -6,11 +6,6 @@ _logger = logging.getLogger(__name__)
 class CreditCard(models.Model):
     _name = 'credit.card'
     _description = 'CreditCard'
-
-    metodo_pago = fields.Many2one(
-        string="Método de Pago",
-        comodel_name='pos.payment.method',
-    )
     
     name = fields.Char(
         string='Nombre',
