343c6def9d61d3977e25f11c6863bcbb63280e52
midrovom <midrovo@callphoneecuador.com>
2024-11-08 10:49:02 -0500

    fix-add: implementando modulo credit card pos

---
+++ credit_card_pos/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name": "POS Credit Card",
+    
+    "summary": "",
+    "description": """
+    
+    """,
+    
+    "category": "POS",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    
+    'depends': [],
+    'data': [],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
