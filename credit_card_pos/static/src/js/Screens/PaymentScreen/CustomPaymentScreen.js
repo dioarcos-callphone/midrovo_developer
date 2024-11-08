4949baf60b260070e223033f21e403d7d6d62fc7
midrovom <midrovo@callphoneecuador.com>
2024-11-08 15:55:19 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
@@ -1,20 +1,19 @@
-/** @odoo-module **/
+odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
+    const PaymentScreen = require("point_of_sale.PaymentScreen");
+    const Registries = require("point_of_sale.Registries");
 
-console.log("CustomPaymentScreen.js loaded");
+    // Heredamos la clase PaymentScreen
+    const CustomPaymentScreen = (PaymentScreen) =>
+        class extends PaymentScreen {
+            // Extiende la función setup si quieres añadir lógica adicional
+            setup() {
+                super.setup();  // Llamar al método padre
+                console.log("CustomPaymentScreen Setup");
+            }
 
-import PaymentScreen from "@point_of_sale/js/Screens/PaymentScreen/PaymentScreen";
-import Registries from "@point_of_sale/js/Registries";
+        };
 
-// Heredamos la clase PaymentScreen
-const CustomPaymentScreen = (PaymentScreen) =>
-    class extends PaymentScreen {
-        // Extiende la función setup si quieres añadir lógica adicional
-        setup() {
-            super.setup();  // Llamar al método padre
-            console.log("CustomPaymentScreen Setup");
-        }
+    // Registramos la nueva clase heredada en los registros de Odoo
+    Registries.Component.extend(PaymentScreen, CustomPaymentScreen);
 
-    };
-
-// Registramos la nueva clase heredada en los registros de Odoo
-Registries.Component.extend(PaymentScreen, CustomPaymentScreen);
+})
