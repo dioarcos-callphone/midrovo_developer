00a18131b097306365e9c6458dbef74f093d186f
midrovom <midrovo@callphoneecuador.com>
2024-11-11 10:02:24 -0500

    fix-update: actualizando archivos views y models

--- credit_card_pos/static/src/js/Popups/CreditCardModal.js
+++ credit_card_pos/static/src/js/Popups/CreditCardModal.js
@@ -1,6 +1,6 @@
 odoo.define("credit_card_pos.CreditCardModal", (require) => {
     const PosComponent = require("point_of_sale.PosComponent");
-    const Registries = rquire("point_of_sale.Registries");
+    const Registries = require("point_of_sale.Registries");
     
     class CreditCardModal extends PosComponent {
         // Selecciona una tarjeta de crédito y cierra el modal
