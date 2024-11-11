b945c9a15ff241725f4c9531ecabe00a81730299
midrovom <midrovo@callphoneecuador.com>
2024-11-11 10:00:54 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Popups/CreditCardModal.js
+++ credit_card_pos/static/src/js/Popups/CreditCardModal.js
@@ -1,13 +1,14 @@
-import PosComponent from "@point_of_sale/js/PosComponent";
-import Registries from "@point_of_sale/js/Registries";
-
-class CreditCardModal extends PosComponent {
-    // Selecciona una tarjeta de crédito y cierra el modal
-    selectCard(card) {
-        this.trigger('close-popup', { card });
+odoo.define("credit_card_pos.CreditCardModal", (require) => {
+    const PosComponent = require("point_of_sale.PosComponent");
+    const Registries = rquire("point_of_sale.Registries");
+    
+    class CreditCardModal extends PosComponent {
+        // Selecciona una tarjeta de crédito y cierra el modal
+        selectCard(card) {
+            this.trigger('close-popup', { card });
+        }
     }
-}
-
-CreditCardModal.template = "CreditCardModal";
-Registries.Component.add(CreditCardModal);
-export default CreditCardModal;
+    
+    CreditCardModal.template = "CreditCardModal";
+    Registries.Component.add(CreditCardModal);
+})
--- credit_card_pos/static/src/js/Screens/PaymentScreen/PSNumpadInputButton.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/PSNumpadInputButton.js
@@ -1,39 +1,42 @@
-import PosComponent from "@point_of_sale/js/PosComponent";
-import Registries from "@point_of_sale/js/Registries";
-import CreditCardModal from "./CreditCardModal"; // Modal que crearemos
+odoo.define("credit_card_pos.PSNumpadInputButton", (require) => {
+    const PosComponent = require("point_of_sale.PosComponent");
+    const Registries = require("point_of_sale.Registries");
+    
+    // Asegúrate de que el modal esté registrado en el `Registries.Component`
+    const CreditCardModal = require("credit_card_pos.CreditCardModal"); 
 
-class PSNumpadInputButton extends PosComponent {
-    get _class() {
-        return this.props.changeClassTo || "input-button number-char";
-    }
+    class PSNumpadInputButton extends PosComponent {
+        get _class() {
+            return this.props.changeClassTo || "input-button number-char";
+        }
 
-    // Sobrescribir el método de clic
-    onClick() {
-        if (this.props.value === 'credit_card') {  // Verifica si el valor es "tarjeta de crédito"
-            // Usar datos quemados
-            const creditCards = [
-                { id: 1, name: "Visa" },
-                { id: 2, name: "MasterCard" },
-                { id: 3, name: "American Express" },
-            ];
+        // Sobrescribir el método de clic
+        onClick() {
+            if (this.props.value === 'credit_card') {  // Verifica si el valor es "tarjeta de crédito"
+                // Usar datos quemados
+                const creditCards = [
+                    { id: 1, name: "Visa" },
+                    { id: 2, name: "MasterCard" },
+                    { id: 3, name: "American Express" },
+                ];
 
-            // Abre el modal de selección de tarjetas de crédito
-            this.showPopup(CreditCardModal, {
-                title: "Selecciona una tarjeta de crédito",
-                creditCards: creditCards, // Pasar lista de tarjetas
-            }).then((selectedCard) => {
-                if (selectedCard) {
-                    // Procesa la tarjeta seleccionada y continúa el flujo
-                    this.trigger('input-from-numpad', { key: selectedCard });
-                }
-            });
-        } else {
-            // Si no es "tarjeta de crédito", continúa el flujo normal
-            this.trigger('input-from-numpad', { key: this.props.value });
+                // Abre el modal de selección de tarjetas de crédito
+                this.showPopup('CreditCardModal', {  // Nombre del modal debe ser un string
+                    title: "Selecciona una tarjeta de crédito",
+                    creditCards: creditCards, // Pasar lista de tarjetas
+                }).then((selectedCard) => {
+                    if (selectedCard) {
+                        // Procesa la tarjeta seleccionada y continúa el flujo
+                        this.trigger('input-from-numpad', { key: selectedCard.name });
+                    }
+                });
+            } else {
+                // Si no es "tarjeta de crédito", continúa el flujo normal
+                this.trigger('input-from-numpad', { key: this.props.value });
+            }
         }
     }
-}
 
-PSNumpadInputButton.template = "PSNumpadInputButton";
-Registries.Component.add(PSNumpadInputButton);
-export default PSNumpadInputButton;
+    PSNumpadInputButton.template = "PSNumpadInputButton";
+    Registries.Component.add(PSNumpadInputButton);
+});
