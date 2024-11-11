dab75fe4f2a8c3c40472738a5bc9027473873de3
midrovom <midrovo@callphoneecuador.com>
2024-11-11 11:47:32 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
@@ -1,6 +1,26 @@
 odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
     const PaymentScreen = require("point_of_sale.PaymentScreen");
     const Registries = require("point_of_sale.Registries");
+    const { useState } = require("react"); // Usamos React para manejar el estado del modal
+
+    // Componente del Modal para mostrar las tarjetas
+    const CreditCardModal = (props) => {
+        const { isVisible, closeModal } = props;
+        return isVisible ? (
+            <div className="credit-card-modal">
+                <div className="modal-content">
+                    <h3>Selecciona una tarjeta de crédito</h3>
+                    {/* Aquí puedes poner la lista de tarjetas de crédito */}
+                    <ul>
+                        <li>Tarjeta Visa</li>
+                        <li>Tarjeta MasterCard</li>
+                        <li>Tarjeta American Express</li>
+                    </ul>
+                    <button onClick={closeModal}>Cerrar</button>
+                </div>
+            </div>
+        ) : null;
+    };
 
     // Heredamos la clase PaymentScreen
     const CustomPaymentScreen = (PaymentScreen) =>
@@ -8,19 +28,31 @@ odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
             // Extiende la función setup si quieres añadir lógica adicional
             setup() {
                 super.setup();  // Llamar al método padre
+                this.state = useState({ isModalVisible: false });
+            }
+
+             // Método para abrir el modal
+             openModal() {
+                this.state.isModalVisible = true;
+            }
+
+            // Método para cerrar el modal
+            closeModal() {
+                this.state.isModalVisible = false;
             }
 
             // Sobrescribimos el método addNewPaymentLine
             async addNewPaymentLine({ detail: paymentMethod }) {
                 const method_name = paymentMethod.name
-                console.log(`MOSTRANDO METHO NAME >>> ${ method_name }`)
                 const result_rpc = await this.rpc({
                     model: "pos.payment.method",
                     method: "is_card",
                     args: [ method_name ]
                 })
-                console.log("MOSTRANDO RESULTADO DEL RPC")
-                console.log(result_rpc)
+
+                if(result_rpc) {
+                    this.openModal();
+                }
 
                 // Retornamos el método original de PaymentScreen utilizando super
                 return super.addNewPaymentLine({ detail: paymentMethod });
