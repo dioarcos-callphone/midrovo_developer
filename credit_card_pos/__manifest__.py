772aa9d39b3e330ac21c7b0699d8d8346956af71
midrovom <midrovo@callphoneecuador.com>
2024-11-11 15:47:01 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/__manifest__.py
+++ credit_card_pos/__manifest__.py
@@ -23,6 +23,7 @@
         "point_of_sale.assets": [
             "credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js",
             "credit_card_pos/static/src/js/Popup/RecapAuthPopup.js",
+            "credit_card_pos/static/src/css/style.css",
             "credit_card_pos/static/src/xml/Popup/RecapAuthPopup.xml",
         ],
     },
