6abf01bcb615755490a0a0603ba4f82658eeb483
midrovom <midrovo@callphoneecuador.com>
2024-11-12 09:39:16 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/PosGlobalStateExtend.js
+++ credit_card_pos/static/src/js/PosGlobalStateExtend.js
@@ -11,7 +11,7 @@ odoo.define("credit_card_pos.PosGlobalStateExtend", (require) => {
 
             // SE OBTIENE DICCIONARIO EJ. {id: 865, pos_reference: 'Pedido 00142-356-0001', account_move: 1951}
             const result = await super._save_to_server(orders, options);
-            console.log(this)
+            console.log(this.payment_methods)
             //console.log(result)
 
             await rpc.query({
