16734d4d07510d50a7bdce8305a932bd3effa851
midrovom <midrovo@callphoneecuador.com>
2024-11-13 10:47:16 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/pos_payment_update.py
+++ credit_card_pos/models/pos_payment_update.py
@@ -13,21 +13,23 @@ class PosPaymentUpdate(models.Model):
     def update_invoice_payments_widget(self, credit_cards, results):
         for result in results:
             pos_order_id = result['id']
-            pos_payment = self.search([('pos_order_id', '=', pos_order_id)])
+            pos_payments = self.search([('pos_order_id', '=', pos_order_id)])
             
-            if pos_payment:
-                if pos_payment.payment_method_id:
-                    payment_methods = pos_payment.payment_method_id
-                    for payment_method in payment_methods:
-                        if payment_method.apply_card:                        
-                            for card in credit_cards:
-                                credit_card = self.env['credit.card'].search([('name', '=', card.get('card'))], limit=1)
-                                self.env['credit.card.info'].create({
-                                    'pos_payment_id': pos_payment.id,
-                                    'credit_card_id': credit_card.id,
-                                    'recap': card.get('recap'),
-                                    'authorization': card.get('auth'),
-                                    'reference': card.get('ref'),
-                                })
+            # Filtrar los pagos cuyo método de pago tiene 'apply_card' en True
+            card_payments = pos_payments.filtered(lambda payment: payment.payment_method_id.apply_card)
+            
+            if card_payments:
+                for payment in card_payments:   
+                    _logger.info(f'MOSTRANDO RESULTADO >>> { payment }')
+                    _logger.info(f'MOSTRANDO RESULTADO >>> { payment.payment_method_id }')     
+                    # for card in credit_cards:
+                    #     credit_card = self.env['credit.card'].search([('name', '=', card.get('card'))], limit=1)
+                    #     self.env['credit.card.info'].create({
+                    #         'pos_payment_id': pos_payment.id,
+                    #         'credit_card_id': credit_card.id,
+                    #         'recap': card.get('recap'),
+                    #         'authorization': card.get('auth'),
+                    #         'reference': card.get('ref'),
+                    #     })
 
                
