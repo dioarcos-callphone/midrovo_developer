74a0f289a56725d5e533a4799350c18d12584b13
midrovom <midrovo@callphoneecuador.com>
2024-11-13 09:37:42 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/invoice_update.py
+++ credit_card_pos/models/invoice_update.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 
 class InvoiceUpdate(models.Model):
-    _inherit = "account.move"
+    _inherit = "pos.payment"
     
     credit_card_info_ids = fields.One2many(
         'credit.card.info',
@@ -16,18 +16,19 @@ class InvoiceUpdate(models.Model):
     @api.model
     def update_invoice_payments_widget(self, credit_cards, results):
         for result in results:
-            account_move = result['account_move']
-            invoice = self.search([('id', '=', account_move)])
+            pos_order_id = result['id']
+            pos_payment = self.search([('pos_order_id', '=', pos_order_id)])
             
-            if invoice:
-                for card in credit_cards:
-                    credit_card = self.env['credit.card'].search([('name', '=', card.get('card'))], limit=1)
-                    self.env['credit.card.info'].create({
-                        'account_move_id': invoice.id,
-                        'credit_card_id': credit_card.id,
-                        'recap': card.get('recap'),
-                        'authorization': card.get('auth'),
-                        'reference': card.get('ref'),
-                    })
+            if pos_payment:
+                _logger.info(f'POS PAYMENT >>> { pos_payment.pos_order_id }')
+                # for card in credit_cards:
+                #     credit_card = self.env['credit.card'].search([('name', '=', card.get('card'))], limit=1)
+                #     self.env['credit.card.info'].create({
+                #         'account_move_id': invoice.id,
+                #         'credit_card_id': credit_card.id,
+                #         'recap': card.get('recap'),
+                #         'authorization': card.get('auth'),
+                #         'reference': card.get('ref'),
+                #     })
 
                 
