e04c270f81643d671accc8dce8fa3fd702af2e03
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-13 16:06:03 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
@@ -74,7 +74,7 @@ odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
                             this.env.pos.creditCards.push(credit_card)
 
                             // console.log(`Esta variable es de tipo >>> ${ typeof(this.paymentLines) }`)
-                            console.log(this.paymentLines.PosCollection)
+                            console.log(this.paymentLines)
 
                             return super.addNewPaymentLine({ detail: paymentMethod });
                         }
