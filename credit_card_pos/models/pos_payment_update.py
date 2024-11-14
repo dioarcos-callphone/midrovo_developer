0f24015a9ff095a6e35e13d5a91ba7043bff711c
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-14 17:38:38 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/pos_payment_update.py
+++ credit_card_pos/models/pos_payment_update.py
@@ -10,9 +10,7 @@ class PosPaymentUpdate(models.Model):
     credit_card_info_ids = fields.One2many('credit.card.info', 'pos_payment_id', string="Tarjetas de Crédito")
     
     @api.model
-    def update_invoice_payments_widget(self, statementFlated, results):
-        _logger.info(f'MOSTRANDO STATEMENT >>>> { statementFlated }')
-        
+    def update_invoice_payments_widget(self, statementFlated, results):       
         for result in results:
             pos_order_id = result['id']
             pos_payments = self.search([('pos_order_id', '=', pos_order_id)])
@@ -25,14 +23,13 @@ class PosPaymentUpdate(models.Model):
                     for payment in card_payments:      
                         credit_card = self.env['credit.card'].search([('name', '=', statement.get('card'))], limit=1)
                         
-                        if statement.get('amount') == payment.amount and\
-                            statement.get('payment_method_id') == payment.payment_method_id:
-                                self.env['credit.card.info'].create({
-                                    'pos_payment_id': payment.id,
-                                    'credit_card_id': credit_card.id,
-                                    'recap': statement.get('recap'),
-                                    'authorization': statement.get('auth'),
-                                    'reference': statement.get('ref'),
-                                })
+                        if statement.get('amount') == payment.amount and statement.get('payment_method_id') == payment.payment_method_id.id:
+                            self.env['credit.card.info'].create({
+                                'pos_payment_id': payment.id,
+                                'credit_card_id': credit_card.id,
+                                'recap': statement.get('recap'),
+                                'authorization': statement.get('auth'),
+                                'reference': statement.get('ref'),
+                            })
 
                
