24a7eacfaa9a594d546b93d4f4bd1325d2322aed
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 14:30:23 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -3,16 +3,8 @@ odoo.define("credit_card_pos.NumberBufferExtend", (require) => {
 
     const NumberBuffer = require("point_of_sale.NumberBuffer");
 
-    // Guardar los métodos originales
-    const originalDeactivate = NumberBuffer.deactivate;
-
     // Sobrescribir deactivate
     NumberBuffer.deactivate = function () {
-        // Llamar al método original
-        if (originalDeactivate) {
-            originalDeactivate.call(this);
-        }
-
         // Eliminar el listener
         window.removeEventListener("keyup", this._onKeyboardInput.bind(this));
         this.listenerAttached = false;
