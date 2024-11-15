7a08571d440f46abb796218b404a78c901aa52d5
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 12:05:58 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -4,14 +4,10 @@ odoo.define("credit_card_pos.NumberBufferExtend", (require) => {
     const NumberBuffer = require("point_of_sale.NumberBuffer");
     const { removeExternalListener } = owl;
 
-    // Extiende la clase NumberBuffer
-    class NumberBufferExtend extends NumberBuffer {
-        deactivate() {
-            // Eliminar el listener de 'keyup'
-            removeExternalListener(window, "keyup", this._onKeyboardInput.bind(this));
-        }
+    NumberBuffer.deactivate = async () => {
+        // Eliminar el listener de 'keyup'
+        removeExternalListener(window, "keyup", this._onKeyboardInput.bind(this));
     }
 
-    // Exportar la clase extendida como default
-    return new NumberBufferExtend();
+    return NumberBuffer;
 });
