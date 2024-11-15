0411cd4914cb0cb757cc9c2e2db991ab0a3d3e8c
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 14:25:52 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -2,28 +2,38 @@ odoo.define("credit_card_pos.NumberBufferExtend", (require) => {
     "use strict";
 
     const NumberBuffer = require("point_of_sale.NumberBuffer");
-    const { useExternalListener } = owl;
 
-    // Usar una función tradicional para preservar el contexto de `this`
+    // Guardar los métodos originales
+    const originalActivate = NumberBuffer.activate;
+    const originalDeactivate = NumberBuffer.deactivate;
+
+    // Sobrescribir activate
+    NumberBuffer.activate = function () {
+        // Llamar al método original
+        if (originalActivate) {
+            originalActivate.call(this);
+        }
+
+        // Agregar lógica adicional
+        if (!this.listenerAttached) {
+            window.addEventListener("keyup", this._onKeyboardInput.bind(this));
+            this.listenerAttached = true;
+        }
+    };
+
+    // Sobrescribir deactivate
     NumberBuffer.deactivate = function () {
-        useExternalListener(window, "keyup", this._onKeyboardInput.bind(this), true);
-        this.listenerAttached = false;
-        // if (this.listenerAttached) {
-        //     // Eliminar el listener de teclado si está presente
-        //     useExternalListener(window, "keyup", this._onKeyboardInput.bind(this), true);
-        //     this.listenerAttached = false;
-        // }
-    }
+        // Llamar al método original
+        if (originalDeactivate) {
+            originalDeactivate.call(this);
+        }
 
-    // NumberBuffer.activate = function () {
-    //     // Llamar al método original de NumberBuffer.activate()
-    //     if (!this.listenerAttached) {
-    //         // Llamamos a la implementación original de activate de NumberBuffer
-    //         this.activate.call(this); // Esto llama al método original
-
-    //         this.listenerAttached = true;
-    //     }
-    // };
+        // Eliminar el listener
+        if (this.listenerAttached) {
+            window.removeEventListener("keyup", this._onKeyboardInput.bind(this));
+            this.listenerAttached = false;
+        }
+    };
 
     return NumberBuffer;
 });
