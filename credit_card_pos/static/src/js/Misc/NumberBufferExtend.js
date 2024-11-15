5421cea986e5c049bfc3857b050dd940ba670beb
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 12:58:40 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -4,22 +4,23 @@ odoo.define("credit_card_pos.NumberBufferExtend", (require) => {
     const NumberBuffer = require("point_of_sale.NumberBuffer");
     const { useExternalListener } = owl;
 
-    // Usar una función tradicional para preservar el contexto de `this`
-    NumberBuffer.deactivate = function () {
-        if (this.listenerAttached) {
-            // Eliminar el listener de teclado si está presente
-            useExternalListener(window, "keyup", this._onKeyboardInput.bind(this), true);
-            this.listenerAttached = false;
-        }
-    }
-
-    NumberBuffer.activate = function () {
+    // Sobrescribir el método activate para agregar listenerAttached
+    NumberBuffer.prototype.activate = function () {
         // Llamar al método original de NumberBuffer.activate()
         if (!this.listenerAttached) {
             // Llamamos a la implementación original de activate de NumberBuffer
-            this.activate.call(this); // Esto llama al método original
+            NumberBuffer.prototype.activate.call(this); // Esto llama al método original
 
             this.listenerAttached = true;
+        }
+    };
+
+    // Añadir el método deactivate para eliminar el listener
+    NumberBuffer.prototype.deactivate = function () {
+        // Verificar si el listener está adjunto antes de intentar eliminarlo
+        if (this.listenerAttached) {
+            useExternalListener(window, "keyup", this._onKeyboardInput.bind(this), true);
+            this.listenerAttached = false;
         }
     };
 
