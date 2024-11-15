cf7726d9fc33f8093f0a07005d5af2aa6509d8e7
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 17:38:56 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -1,26 +1,13 @@
 odoo.define('credit_card_pos.NumberBufferExtend', function (require) {
     "use strict";
 
-    // Importa el módulo o clase original que deseas extender
     const NumberBuffer = require('point_of_sale.NumberBuffer');  // Ruta correcta para importar NumberBuffer
 
-    // Crea una nueva clase que contenga una instancia de NumberBuffer
-    const NumberBufferExtend = function () {
-        this.numberBufferInstance = new NumberBuffer();
-    };
-
     // Agrega métodos o propiedades adicionales a esta clase
-    NumberBufferExtend.prototype.newMethod = function () {
+    NumberBuffer.newMethod = function () {
         console.log('Método extendido');
         // Implementación adicional
     };
 
-    // Sobrescribe un método existente si lo necesitas
-    NumberBufferExtend.prototype.reset = function () {
-        this.numberBufferInstance.reset();  // Llama al método original de NumberBuffer
-        console.log('Buffer ha sido reseteado');
-        // Puedes agregar lógica extra aquí
-    };
-
-    return NumberBufferExtend;
+    return NumberBuffer;
 });
