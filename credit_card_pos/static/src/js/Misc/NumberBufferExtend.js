9281a99cab28912b784b32876b404bb23fb1a0c8
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-15 14:53:49 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
+++ credit_card_pos/static/src/js/Misc/NumberBufferExtend.js
@@ -3,12 +3,47 @@ odoo.define("credit_card_pos.NumberBufferExtend", (require) => {
 
     const NumberBuffer = require("point_of_sale.NumberBuffer");
 
-    // Sobrescribir deactivate
-    NumberBuffer.deactivate = function (event) {
-        // Eliminar el listener
-        event.preventDefault();
-        event.stopPropagation();
-    };
+    const NumberBufferExtend = NumberBuffer.extend({
+        init() {
+            this._super(...arguments);
+            this.keyboardEnabled = true; // Nueva propiedad para manejar el estado del teclado
+        },
 
-    return NumberBuffer;
+        /**
+         * Deshabilita el manejo del teclado.
+         */
+        disableKeyboard() {
+            this.keyboardEnabled = false;
+        },
+
+        /**
+         * Habilita el manejo del teclado.
+         */
+        enableKeyboard() {
+            this.keyboardEnabled = true;
+        },
+
+        /**
+         * Sobrescribe el método _onKeyboardInput para verificar si el teclado está habilitado.
+         */
+        _onKeyboardInput(event) {
+            if (!this.keyboardEnabled) {
+                return; // Ignorar eventos del teclado si está deshabilitado
+            }
+            return this._super(event);
+        },
+
+        /**
+         * Sobrescribe el método _onNonKeyboardInput para verificar si el teclado está habilitado.
+         */
+        _onNonKeyboardInput(event) {
+            if (!this.keyboardEnabled) {
+                return; // Ignorar eventos no provenientes del teclado si está deshabilitado
+            }
+            return this._super(event);
+        },
+    });
+
+    // Sobrescribe el `NumberBuffer` global con la versión extendida
+    return new NumberBufferExtend();
 });
