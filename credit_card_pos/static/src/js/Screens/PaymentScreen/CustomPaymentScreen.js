7413cf79eb2e342fbf6a5297df1a5220c395b69a
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-18 12:43:03 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
@@ -5,7 +5,8 @@ odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
     const Registries = require("point_of_sale.Registries");
     const NumberBuffer = require("point_of_sale.NumberBuffer");
 
-    const { useListener, removeEventListener } = owl;
+    const { removeEventListener } = owl;
+    const { useListener } = require("@web/core/utils/hooks")
 
     // Se añade la función deactivate para eliminar el listener
     NumberBuffer.deactivate = function () {
