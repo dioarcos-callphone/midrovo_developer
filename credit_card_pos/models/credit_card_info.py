9e39016b069aea4f511169ecb0597270675ecf73
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-18 17:33:14 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/credit_card_info.py
+++ credit_card_pos/models/credit_card_info.py
@@ -3,7 +3,6 @@ from odoo import models, fields, api
 class CreditCardInfo(models.Model):
     _name = "credit.card.info"
     
-    pos_payment_id = fields.Many2one('pos.payment', string="Factura")
     credit_card_id = fields.Many2one('credit.card', string="Número de Tarjeta")
     recap = fields.Char(string="Recap")
     authorization = fields.Char(string="Autorización")
