430cadadd6e642872189b0732655be242ebc0333
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-18 10:23:57 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/__manifest__.py
+++ credit_card_pos/__manifest__.py
@@ -27,7 +27,6 @@
             "credit_card_pos/static/src/js/Misc/NumberBufferExtend.js",
             "credit_card_pos/static/src/xml/Popup/RecapAuthPopup.xml",
             "credit_card_pos/static/src/js/PosGlobalStateExtend.js",
-            "credit_card_pos/static/src/js/ChromeExtend.js",
             "credit_card_pos/static/src/xml/PaymentScreen/PaymentScreenPaymentLinesInherit.xml",
         ],
     },
