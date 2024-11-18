1f2a4cb5b9fc029a9b019d3596502434178b388c
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-18 15:32:52 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/__init__.py
+++ credit_card_pos/models/__init__.py
@@ -2,3 +2,4 @@ from . import credit_card
 from . import pos_payment_method_inherit
 from . import pos_payment_update
 from . import credit_card_info
+from . import account_move_inherit
---
+++ credit_card_pos/models/account_move_inherit.py
@@ -0,0 +1,22 @@
+from odoo import models, fields, api
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class AccountMoveInherit(models.Model):
+    _inherit = 'account.move'
+    
+    @api.depends('move_type', 'line_ids.amount_residual')
+    def _compute_payments_widget_reconciled_info(self):
+        # Llamar al método original si quieres conservar la lógica
+        super(AccountMoveInherit, self)._compute_payments_widget_reconciled_info()
+        
+        # Agregar lógica personalizada
+        for move in self:
+            _logger.info(f'MOSTRANDO MOVE >>> { move }')
+            # if move.state == 'posted' and move.is_invoice(include_receipts=True):
+            #     # Personaliza o añade campos adicionales a payments_widget_vals
+            #     if move.invoice_payments_widget:
+            #         for payment in move.invoice_payments_widget.get('content', []):
+            #             payment['custom_field'] = 'Valor personalizado'  # Ejemplo
