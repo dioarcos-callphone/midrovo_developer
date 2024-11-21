148146a080995c82cfd314613c50579b8e79bf1f
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-21 16:26:51 -0500

    fix-update: actualizando archivos statics

--- credit_card_pos/models/pos_session_inherit.py
+++ credit_card_pos/models/pos_session_inherit.py
@@ -3,6 +3,17 @@ from odoo import models, fields, api
 class PosSessionInherit(models.Model):
     _inherit = 'pos.session'
     
+    def _pos_ui_models_to_load(self):
+        result = super()._pos_ui_models_to_load()
+        if self.config_id.module_pos_hr:
+            credit_card = 'credit.card'
+            credit_card_info = 'credit.card.info'
+            if credit_card not in result:
+                result.append(credit_card)
+            if credit_card_info not in result:
+                result.append(credit_card_info)
+        return result
+    
     def _loader_params_credit_card():
         return {
             'search_params': { 'fields': ['name', 'tipo', 'banco'] },
