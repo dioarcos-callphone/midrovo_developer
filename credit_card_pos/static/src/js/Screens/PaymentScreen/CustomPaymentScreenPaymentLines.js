72ca211df2ed420f36f53ff4be619a474befac4a
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-22 15:45:40 -0500

    fix-update: actualizacion de componente js

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreenPaymentLines.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreenPaymentLines.js
@@ -5,7 +5,7 @@ odoo.define("credit_card_pos.CustomPaymentScreenPaymentLines", (require) => {
     const CustomPaymentScreenPaymentLines = (PaymentScreenPaymentLines) =>
         class extends PaymentScreenPaymentLines {
             selectedLineClass(line) {
-                console.log(line.get_payment_status())
+                console.log(line)
                 
                 return super.selectedLineClass(line);
             }
