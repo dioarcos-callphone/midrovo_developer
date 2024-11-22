2ce56998b9a8d8d62e0ad44f21b6f0871df49dec
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-22 10:33:44 -0500

    fix-update: actualizacion de componente js

--- credit_card_pos/static/src/js/PosGlobalStateExtend.js
+++ credit_card_pos/static/src/js/PosGlobalStateExtend.js
@@ -14,6 +14,7 @@ odoo.define("credit_card_pos.PosGlobalStateExtend", (require) => {
         }
         
         async _save_to_server(orders, options) {
+            console.log(this)
             // SE OBTIENE DICCIONARIO EJ. {id: 865, pos_reference: 'Pedido 00142-356-0001', account_move: 1951}
             const result = await super._save_to_server(orders, options);
             
