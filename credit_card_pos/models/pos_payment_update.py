a4eb32c5d10f2fff943be692e031d4ea3729c5bc
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-25 11:04:49 -0500

    fix-update: actualizacion de componente js

--- credit_card_pos/models/pos_payment_update.py
+++ credit_card_pos/models/pos_payment_update.py
@@ -22,23 +22,18 @@ class PosPaymentUpdate(models.Model):
             if card_payments:
                 for statement in statementFlated:
                     creditCard = statement.get('creditCard')
-                    for payment in card_payments:      
-                        credit_card = self.env['credit.card'].search([('name', '=', creditCard.get('card'))], limit=1)
-                        
-                        if statement.get('amount') == payment.amount and statement.get('payment_method_id') == payment.payment_method_id.id:
+                    for payment in card_payments:
+                        if (statement.get('amount') == payment.amount and 
+                            statement.get('payment_method_id') == payment.payment_method_id.id and 
+                            not payment.credit_card_info_id):
+                            
+                            credit_card = self.env['credit.card'].search([('name', '=', creditCard.get('card'))], limit=1)
                             credit_card_new = self.env['credit.card.info'].create({
                                 'credit_card_id': credit_card.id,
                                 'recap': creditCard.get('recap'),
                                 'authorization': creditCard.get('auth'),
                                 'reference': creditCard.get('ref'),
                             })
-                            
-                            payment.write({ 'credit_card_info_id': credit_card_new.id })
-                            
-                # for payment in card_payments:
-                #     for statement in statementFlated:
-                #         creditCard = statement.get("creditCard")
-                #         credit_card = self.env['credit.card'].search([('name', '=', creditCard.get('card'))], limit=1)
-                        
-                #         if statement.get("amount") == payment.amount 
+                            payment.write({'credit_card_info_id': credit_card_new.id})
+
                                                    
