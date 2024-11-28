742eb28558a33c94a35d5bf52e6e52398ebc0ef0
fundacioncj <caminandojuntoproy@gmail.com>
2024-11-28 12:31:20 -0500

    fix-update: actualizando pos global

--- credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
+++ credit_card_pos/static/src/js/Screens/PaymentScreen/CustomPaymentScreen.js
@@ -136,7 +136,17 @@ odoo.define("credit_card_pos.CustomPaymentScreen", (require) => {
             }
 
             async validateOrder(isForceValidate) {
-                console.log(this)
+                for(const line of this.paymentLines) {
+                    if(line.payment_method.apply_card === True && !line.creditCard) {
+                        await this.showPopup("ErrorPopup", {
+                            title: this.env._t("Error"),
+                            body: this.env._t(
+                                "El método de pago contiene tarjetas de crédito pero no se han establecido."
+                            ),
+                        });
+                        return
+                    }
+                }
 
                 await super.validateOrder(isForceValidate)
             }
