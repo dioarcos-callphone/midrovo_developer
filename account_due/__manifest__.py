72729d3c94a8942ef7f4941990b455d8da3659fd
fundacioncj <caminandojuntoproy@gmail.com>
2024-12-02 17:07:50 -0500

    fix-add: implementando modulo account due

---
+++ account_due/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': '',
+    'summary': '',
+    'description': """
+
+    """,
+    
+    "category": "Account Move",
+    "version": "16.0.1.0.0",
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'account', 'point_of_sale', 'invoice_details_view' ],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/account_due_wizard.xml',
+        # 'report/invoice_details_report.xml',
+        # 'report/invoice_details_template.xml',
+    ],
+    
+    'assets': {
+        'web.assets_backend': [
+            # 'invoice_details_view/static/src/js/action_manager.js',
+            'invoice_details_view/static/src/css/style.css'
+        ]
+    },
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+}
---
+++ account_due/wizard/account_due_wizard.py
@@ -0,0 +1,31 @@
+import io
+import json
+from datetime import datetime
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+try:
+    from odoo.tools.misc import xlsxwriter
+except ImportError:
+    import xlsxwriter
+
+class AccountDueWizard(models.TransientModel):
+    _name = "account.due.wizard"
+    _description = "Cuentas por Cobrar Vencidas"
+    
+    court_date = fields.Date(
+        string = 'Fecha de corte',
+        help = 'Fecha de corte para analizar el informe',
+        required = True
+    )
+    
+    client_id = fields.Many2one(
+        string='Cliente',
+        comodel_name='res.partner',
+        
+        domain=[
+            ('type','!=','private'),
+            ('company_id','=',False),
+        ] 
+    )
+    
