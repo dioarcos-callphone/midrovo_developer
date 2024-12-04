64dbc0972bf4669401acd0161626bdbc9d8e9860
fundacioncj <caminandojuntoproy@gmail.com>
2024-12-04 18:00:09 -0500

    fix-update: actualizando wizard

--- account_due/report/account_due.py
+++ account_due/report/account_due.py
@@ -151,7 +151,7 @@ class InvoiceDetails(models.AbstractModel):
         # Formatear el resultado
         formatted_results = [
             {
-                'partner_name': res['partner_id'][1] if res['partner_id'] else 'Unknown',
+                'partner_name': res['partner_id'][1] if isinstance(res['partner_id'], list) else 'Unknown',
                 'total_amount_residual': res['amount_residual']
             }
             for res in results
