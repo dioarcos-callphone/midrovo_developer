1df3228a14331f1392900f680e8339fb247e4efb
fundacioncj <caminandojuntoproy@gmail.com>
2024-12-05 12:51:42 -0500

    fix-update: actualizando get residual totals

--- account_due/wizard/account_due_wizard.py
+++ account_due/wizard/account_due_wizard.py
@@ -380,7 +380,7 @@ class AccountDueWizard(models.TransientModel):
         
         if is_summary == 'd':
             row = 4
-            sheet.write(row, 0, val.get('cliente'), text_format)
+            sheet.write(row, 0, val.get('client'), text_format)
             sheet.write(row, 1, '', text_format)
             sheet.write(row, 2, '', text_format)
             sheet.write(row, 3, val.get('actual') if val.get('actual') else '', text_format)
