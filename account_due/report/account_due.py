807829934f670b0f9f8ee3381c894683e747e972
fundacioncj <caminandojuntoproy@gmail.com>
2024-12-06 17:26:56 -0500

    fix-update: actualizando get residual totals

--- account_due/report/account_due.py
+++ account_due/report/account_due.py
@@ -150,6 +150,7 @@ class InvoiceDetails(models.AbstractModel):
                 )
             elif journal_id:
                 _logger.info(f'ENTRY >>>> { entry } ')
+                _logger.info(f'ENTRY >>>> { total } ')
                 total = total - entry
                 # Filtrar solo por journal_id
                 account_move_lines_filtered = list(
