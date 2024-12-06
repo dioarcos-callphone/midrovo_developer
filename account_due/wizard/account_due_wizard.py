60b67186907cd056632f88ca13d5fd1f0b341654
fundacioncj <caminandojuntoproy@gmail.com>
2024-12-06 18:01:40 -0500

    fix-update: actualizando get residual totals

--- account_due/wizard/account_due_wizard.py
+++ account_due/wizard/account_due_wizard.py
@@ -78,7 +78,11 @@ class AccountDueWizard(models.TransientModel):
         ]
         
         if client_id:
-            domain.append(('partner_id', '=', client_id)) 
+            domain.append(('partner_id', '=', client_id))
+        if journal_id:
+            domain.append(('journal_id', '=', journal_id))
+        if comercial_id:
+            domain.append(('move_id.invoice_user_id', '=', comercial_id)) 
         
         invoice_details = self.env['account.move.line'].search(domain)
         
@@ -172,33 +176,33 @@ class AccountDueWizard(models.TransientModel):
             
             account_move_lines_filtered = account_move_lines
 
-            if journal_id and comercial_id:
-                # Filtrar por ambos campos
-                account_move_lines_filtered = list(
-                    filter(
-                        lambda x: x.get('journal') == journal_id and x.get('comercial') == comercial_id,
-                        account_move_lines
-                    )
-                )
-            elif journal_id:
-                # Filtrar solo por journal_id
-                account_move_lines_filtered = list(
-                    filter(
-                        lambda x: x.get('journal') == journal_id,
-                        account_move_lines
-                    )
-                )
-            elif comercial_id:
-                # Filtrar solo por comercial_id
-                account_move_lines_filtered = list(
-                    filter(
-                        lambda x: x.get('comercial') == comercial_id,
-                        account_move_lines
-                    )
-                )
+            # if journal_id and comercial_id:
+            #     # Filtrar por ambos campos
+            #     account_move_lines_filtered = list(
+            #         filter(
+            #             lambda x: x.get('journal') == journal_id and x.get('comercial') == comercial_id,
+            #             account_move_lines
+            #         )
+            #     )
+            # elif journal_id:
+            #     # Filtrar solo por journal_id
+            #     account_move_lines_filtered = list(
+            #         filter(
+            #             lambda x: x.get('journal') == journal_id,
+            #             account_move_lines
+            #         )
+            #     )
+            # elif comercial_id:
+            #     # Filtrar solo por comercial_id
+            #     account_move_lines_filtered = list(
+            #         filter(
+            #             lambda x: x.get('comercial') == comercial_id,
+            #             account_move_lines
+            #         )
+            #     )
                 
-            if not account_move_lines_filtered:
-                raise ValidationError("¡No se encontraron registros para los criterios dados!")  
+            # if not account_move_lines_filtered:
+            #     raise ValidationError("¡No se encontraron registros para los criterios dados!")  
                 
             accounts_receivable_data = {
                 'client': client.name,
