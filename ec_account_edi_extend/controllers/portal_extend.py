76c27ca7db58f757d34d5cf445e68deea3a8563b
midrovom <midrovo@callphoneecuador.com>
2024-12-12 09:47:34 -0500

    fix-update: actualizando template home_refund

--- ec_account_edi_extend/controllers/portal_extend.py
+++ ec_account_edi_extend/controllers/portal_extend.py
@@ -15,7 +15,7 @@ class CustomPortalEcAccountEdi(CustomerPortal):
         if 'refund_count' in counters:
             refund_count = request.env['account.move'].search_count(self._get_out_refund_domain()) \
                 if request.env['account.move'].check_access_rights('read', raise_exception=False) else 0
-            values['refund_count'] = 5
+            values['refund_count'] = refund_count
         return values
     
     def _get_out_refund_domain(self):
