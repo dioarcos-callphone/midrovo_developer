2c4cfd87a11ca19c869be417c7e329e79375db60
midrovom <midrovo@callphoneecuador.com>
2024-12-18 16:12:52 -0500

    fix-update: actualizando portal_my_withholding_detail

--- ec_account_edi_extend/controllers/portal_retencion_controller.py
+++ ec_account_edi_extend/controllers/portal_retencion_controller.py
@@ -8,6 +8,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal, pager as porta
 from odoo.exceptions import AccessError, MissingError
 from collections import OrderedDict
 from odoo.http import request
+import base64
 
 class PortalWithholding(CustomerPortal):
     
@@ -30,6 +31,13 @@ class PortalWithholding(CustomerPortal):
         }
         return self._get_page_view_values(withholding, access_token, values, 'my_withholdings_history', False, **kwargs)
     
+    def _get_account_searchbar_sortings(self):
+        return {
+            'date': {'label': _('Date'), 'order': 'creation_date desc'},
+            'name': {'label': _('Reference'), 'order': 'l10n_latam_document_number desc'},
+            'state': {'label': _('Status'), 'order': 'state'},
+        }
+    
     # metodo que genera el contenido de retenciones
     @http.route(['/my/withholding', '/my/withholding/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_withholding(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, **kw):
@@ -48,10 +56,43 @@ class PortalWithholding(CustomerPortal):
         })
         return request.render("ec_account_edi_extend.portal_my_withholdings", values)
     
+    @http.route(['/my/withholdings/<int:withholding_id>'], type='http', auth="public", website=True)
+    def portal_my_withholding_detail(self, withholding_id, access_token=None, report_type=None, download=False, **kw):
+        try:
+            # Verifica el acceso al documento
+            withhold_sudo = self._document_check_access('account.withhold', withholding_id, access_token)
+            
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+
+        # Cambia el reporte referenciado
+        if report_type in ('html', 'pdf', 'text'):
+            return self._show_report(
+                model=withhold_sudo,
+                report_type=report_type,
+                report_ref='ec_account_edi.e_invoice_qweb',  # Nuevo valor para report_ref
+                download=download
+            )
+            
+        if report_type == 'xml':
+            xml_name = withhold_sudo.xml_name
+            xml_bytes = withhold_sudo.xml_report
+            
+            # Si el XML está en base64, lo decodificamos
+            xml_decode = base64.b64decode(xml_bytes)
+             
+            filename = xml_name
+            headers = [
+                ('Content-Type', 'application/xml'),
+                ('Content-Disposition', f'attachment; filename={filename}')
+            ]
+            
+            return request.make_response(xml_decode, headers=headers)
+    
     def _prepare_my_withholding_values(self, page, date_begin, date_end, sortby, filterby, domain=None, url="/my/withholdings"):
         values = self._prepare_portal_layout_values()
         
-        AccountRefund = request.env['account.move']
+        AccountWithhold = request.env['account.withhold']
 
         domain = expression.AND([
             domain or [],
@@ -70,17 +111,17 @@ class PortalWithholding(CustomerPortal):
         values.update({
             'date': date_begin,
             # content according to pager and archive selected
-            # lambda function to get the invoices recordset when the pager will be defined in the main method of a route
+            # lambda function to get the withholdings recordset when the pager will be defined in the main method of a route
             'withholdings': lambda pager_offset: (
-                AccountRefund
-                if AccountRefund.check_access_rights('read', raise_exception=False) else
-                AccountRefund
+                AccountWithhold.search(domain, order=order, limit=self._items_per_page, offset=pager_offset)
+                if AccountWithhold.check_access_rights('read', raise_exception=False) else
+                AccountWithhold
             ),
             'page_name': 'withholding',
             'pager': {  # vals to define the pager.
                 "url": url,
                 "url_args": {'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
-                "total": AccountRefund.search_count(domain) if AccountRefund.check_access_rights('read', raise_exception=False) else 0,
+                "total": AccountWithhold.search_count(domain) if AccountWithhold.check_access_rights('read', raise_exception=False) else 0,
                 "page": page,
                 "step": self._items_per_page,
             },
@@ -90,3 +131,5 @@ class PortalWithholding(CustomerPortal):
         })
         
         return values
+    
+    
