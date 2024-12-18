e9f13d45a181942492674633bfb676f18e46a1bd
midrovom <midrovo@callphoneecuador.com>
2024-12-18 17:03:47 -0500

    fix-update: actualizando get_portal_url

--- ec_account_edi_extend/models/account_withhold_extend.py
+++ ec_account_edi_extend/models/account_withhold_extend.py
@@ -1,6 +1,9 @@
 from odoo import models
 
 class AccountWithholdExtend(models.Model):
-    _inherit = ['account.withhold', 'portal.mixin']
+    _inherit = 'account.withhold'
+    
+    def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
+        return self.env['portal.mixin'].get_portal_url(self, suffix, report_type, download, query_string, anchor)
     
     
