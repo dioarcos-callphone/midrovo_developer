83447ab4d931a260633b688f9667e969500c6c6c
midrovom <midrovo@callphoneecuador.com>
2024-12-19 10:10:31 -0500

    fix-update: cambios en _get_report_base_filename

--- ec_account_edi_extend/models/withhold_extend.py
+++ ec_account_edi_extend/models/withhold_extend.py
@@ -8,3 +8,8 @@ class WithholdExtend(models.Model):
         super()._compute_access_url()
         for withhold in self:
             withhold.access_url = '/my/withholdings/%s' % (withhold.id)
+      
+    # Genera el nombre del archivo PDF del reporte  
+    def _get_report_base_filename(self):
+        self.ensure_one()
+        return 'Retencion-%s' % (self.l10n_latam_document_number)
