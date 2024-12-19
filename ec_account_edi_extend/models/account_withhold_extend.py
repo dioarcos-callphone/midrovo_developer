29546b953b1140de888eb09e587cb47ef0cdfedd
midrovom <midrovo@callphoneecuador.com>
2024-12-19 09:21:50 -0500

    fix-update: cambios en get_portal_url

--- ec_account_edi_extend/models/account_withhold_extend.py
+++ ec_account_edi_extend/models/account_withhold_extend.py
@@ -4,6 +4,6 @@ class AccountWithholdExtend(models.Model):
     _inherit = 'account.withhold'
     
     def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
-        return self.env['portal.mixin'].get_portal_url(self, suffix, report_type, download, query_string, anchor)
+        return self.env['portal.mixin'].get_portal_url(suffix, report_type, download, query_string, anchor)
     
     
