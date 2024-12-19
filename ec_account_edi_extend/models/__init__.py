33d272adf704ddc793ac3e9d81f5b9b11bc86211
midrovom <midrovo@callphoneecuador.com>
2024-12-19 09:36:12 -0500

    fix-update: cambios en get_portal_url

--- ec_account_edi_extend/models/__init__.py
+++ ec_account_edi_extend/models/__init__.py
@@ -1,3 +1,2 @@
 from . import sri_xml_data_extend
 from . import sri_key_extend
-from . import account_withhold_extend
--- ec_account_edi_extend/models/account_withhold_extend.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class AccountWithholdExtend(models.Model):
-    _inherit = 'account.withhold'
-    
-    def get_portal_url(self, suffix=None, report_type=None, download=None, query_string=None, anchor=None):
-        return self.env['portal.mixin'].get_portal_url(suffix, report_type, download, query_string, anchor)
-    
-    
