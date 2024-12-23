7d76dd1764c8ab7fd1a54555494863bd57c69e6e
midrovom <midrovo@callphoneecuador.com>
2024-12-23 12:16:34 -0500

    fix-update: aplicando cambios en UserExtend

--- ec_account_edi_extend/models/user_extend.py
+++ ec_account_edi_extend/models/user_extend.py
@@ -6,12 +6,21 @@ _logger = logging.getLogger(__name__)
 class UserExtend(models.Model):
     _inherit = 'res.users'
     
+    shop_ids = fields.Many2many(
+        'sale.shop',
+        'rel_user_shop',
+        'user_id',
+        'shop_id',
+        u'Estalecimientos Permitidos', 
+    )
+    
     printer_default_id = fields.Many2one(
         'sri.printer.point',
         u'Emisión por Defecto',
         required=False,
         index=True,
         auto_join=True,
+        domain=[('shop_id','in', shop_ids)],  
     )
     
     filter_orders = fields.Boolean(
