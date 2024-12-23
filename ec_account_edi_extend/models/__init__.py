d1bbf6a0aabc53fb5164fe89303443b6bace9d12
midrovom <midrovo@callphoneecuador.com>
2024-12-23 15:12:08 -0500

    fix-update: aplicando cambios en printer_default_ids

--- ec_account_edi_extend/models/__init__.py
+++ ec_account_edi_extend/models/__init__.py
@@ -2,5 +2,5 @@ from . import sri_xml_data_extend
 from . import sri_key_extend
 from . import withhold_extend
 from . import remision_extend
-# from . import user_extend
+from . import user_extend
 from . import sale_shop_extend
