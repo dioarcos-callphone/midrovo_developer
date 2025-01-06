2b43ea3040d6341ec2d664fb729e62dadb5f8c2f
midrovom <midrovo@callphoneecuador.com>
2025-01-06 10:57:31 -0500

    fix-add: añadiendo modulo para el reporte de cotizacion

---
+++ add_fields_report_sale/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "Add Fields Report Sale",
+    
+    "summary": "Columna Adicional en el Reporte de Cotizaciones",
+    "description": """
+
+        (Odoo 16 Enterprise)
+        Este modulo personaliza el reporte de cotizaciones, añadiendo una columna de las imágenes del producto.
+    
+    """,
+    
+    "category": "Sale Report",
+    "version": "16.0.1.0.0",
+    "author": "Mauricio Idrovo",
+    "company": "Callphone S.A.",
+    "website": "https://www.callphoneecuador.com",
+    
+    "depends": [ "sale", "base" ],
+    "data": [
+        "report/sale_report_template.xml",
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': True,
+    
+}
