f8e80c714865b2fe14eb529a2917c3cec543757f
midrovom <midrovo@callphoneecuador.com>
2025-01-06 14:26:11 -0500

    fix-update: actualizando sale order extend

--- add_fields_report_sale/models/sale_order.py
+++ add_fields_report_sale/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import _, api, fields, models
 class SaleOrderExtend(models.Model):
     _inherit = 'sale.order'
     
-    # report_grids = fields.Boolean(string="Print Variant Grids", default=False)
+    report_grids = fields.Boolean(string="Print Variant Grids", default=False)
 
     def get_report_matrixes(self):
         result = super(SaleOrderExtend, self).get_report_matrixes()
