18dfcd550d488aa274790719e8a8ea16f2e8912d
midrovom <midrovo@callphoneecuador.com>
2025-01-16 15:30:43 -0500

    fix-add: Se agrega modulo para el diseno de reporte de factura

---
+++ invoice_report_design/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "Plantilla de Reportes de Facturas",
+    "summary": "Diseño mejorado de los reportes de factura",
+    "description": """
+        - Modulo desarrollado para Odoo 16 Enterprise
+        - Actualización del template de los reportes de factura
+        - Mejorar la vista del archivo PDF
+        - Dependencias requeridas: account.move
+    """,
+    
+    "category": "Invoice Report",
+    "version": "16.0.1.0.0",
+    "author": "Mauricio Idrovo",
+    'company': 'Callphone S.A.',
+    'website': "https://www.callphoneecuador.com",
+    'depends': [ 'account', 'point_of_sale', ],
+    'data': [
+        "views/invoice_report.xml"
+    ],
+    
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
