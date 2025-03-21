f50987d368b51d3f22b77166fdac20de2fc80c05
midrovom <midrovo@callphoneecuador.com>
2025-03-21 10:57:30 -0500

    fix-update:[midrovo] aplicando cambios

--- ec_account_edi_extend/controllers/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import portal_factura_extend_controller
-from . import portal_guia_remision_controller
-from . import portal_liquidacion_compra_controller
-from . import portal_retencion_controller
