904934d656307c075b6f4c417a38d6b4631a9a36
midrovom <midrovo@callphoneecuador.com>
2025-03-24 11:31:58 -0500

    fix-update:[midrovo] aplicando cambios en vistas portal

--- electronic_document_portal/__manifest__.py
+++ electronic_document_portal/__manifest__.py
@@ -31,12 +31,12 @@
         'views/user_extend_view.xml',
         'views/portal_inherited_templates.xml',
         # 'views/portal_custom_templates.xml',
-        'views/invoice_portal_template.xml'
-        'views/credit_note_portal_template.xml'
-        'views/debit_note_portal_template.xml'
-        'views/liquidation_portal_template.xml'
-        'views/remission_portal_template.xml'
-        'views/retention_portal_template.xml'
+        'views/invoice_portal_template.xml',
+        'views/credit_note_portal_template.xml',
+        'views/debit_note_portal_template.xml',
+        'views/liquidation_portal_template.xml',
+        'views/remission_portal_template.xml',
+        'views/retention_portal_template.xml',
         
     ],
     'assets': {
