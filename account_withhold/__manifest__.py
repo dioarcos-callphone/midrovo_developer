af931d2d1142bc110b136cbecbcc68d657461f44
midrovom <midrovo@callphoneecuador.com>
2025-03-24 13:01:07 -0500

    fix-update:[midrovo] anadiendo modulo account_withhold

---
+++ account_withhold/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Account Withhold Util',
+    'version': '1.0',
+    'description': '''
+        - Modulo desarrollado para Odoo 16 EE
+        - Soluciona el problema con totals.subtotals_order cuando se realiza retenciones de facturas de clientes
+    ''',
+    'summary': 'Retenciones de Facturas de Clientes',
+    'author': 'Mauricio Idrovo',
+    'company': 'Callphone S.A.',
+    'website': 'https://www.callphoneecuador.com',
+    'license': 'LGPL-3',
+    'category': 'account',
+    'depends': [
+        'account'
+    ],
+    'data': [
+        ''
+    ],
+    'demo': [
+        ''
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+    'assets': {
+        'web.assets_backend': [
+            'account_withhold/static/src/components/**/*',
+        ],
+    }
+}
---
+++ account_withhold/static/src/components/tax_totals/tax_totals.js
@@ -0,0 +1,25 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { TaxTotalsComponent } from "@account/static/src/components/tax_totals.js";
+
+class CustomTaxTotalsComponent extends TaxTotalsComponent {
+    setup() {
+        super.setup(); // Llamamos a la configuración original
+        console.log("CustomTaxTotalsComponent cargado correctamente.");
+    }
+
+    /**
+     * Sobreescribimos el método para agregar una nueva lógica
+     */
+    formatData(props) {
+        let totals = JSON.parse(JSON.stringify(props.value));
+
+        console.log(`MOSTRAR TOTALS >>> ${ totals }`)
+
+        super().formatData(props)
+    }
+}
+
+// Registramos el nuevo componente en Odoo
+registry.category("fields").add("custom-account-tax-totals-field", CustomTaxTotalsComponent);
