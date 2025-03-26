02e93ee6135f44360f42525b46e83e8fd4b12998
midrovom <midrovo@callphoneecuador.com>
2025-03-26 11:01:15 -0500

    fix-update:[midrovo] actualizando controladores

--- electronic_document_portal/controllers/__init__.py
+++ electronic_document_portal/controllers/__init__.py
@@ -1,4 +1,7 @@
-from . import portal_factura_extend_controller
+from . import invoice_portal
+from . import credit_note_portal
+from . import debit_note_portal
+from . import liquidation_portal
+
 from . import portal_guia_remision_controller
-from . import portal_liquidacion_compra_controller
 from . import portal_retencion_controller
