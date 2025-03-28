f48a39883ad5837871126762a73087884eb60329
midrovom <midrovo@callphoneecuador.com>
2025-03-28 15:32:25 -0500

    fix-update:[midrovo] actualizando controladores

--- electronic_document_portal/models/remision.py
+++ electronic_document_portal/models/remision.py
@@ -15,6 +15,7 @@ class Remission(models.Model):
     ], string=u'Estado', readonly=True, related='xml_data_id.state')
 
     remission_send_ids = fields.Many2many('account.remission.send', 'account_remission_account_remission_send_rel')
+    user_id = fields.Many2one(string='User', default=lambda self: self.env.user)
 
     # EXTENDS portal portal.mixin
     def _compute_access_url(self):
