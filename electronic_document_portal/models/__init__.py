87d456945b3e156cde944176e73de6f259a5239a
midrovom <midrovo@callphoneecuador.com>
2025-03-28 15:55:34 -0500

    fix-update:[midrovo] actualizando controladores

--- electronic_document_portal/models/__init__.py
+++ electronic_document_portal/models/__init__.py
@@ -3,3 +3,4 @@ from . import remision
 from . import res_user
 from . import retention
 from . import sale_shop
+from . import mail
---
+++ electronic_document_portal/models/mail.py
@@ -0,0 +1,28 @@
+from odoo import _, api, fields, models
+
+class MailTemplate(models.Model):
+    _inherit = 'mail.template'
+
+    def generate_email(self, res_ids, fields=None):
+        res = super().generate_email(res_ids, fields)
+        xml_model = self.env['sri.xml.data']
+        multi_mode = True
+        if isinstance(res_ids, int):
+            multi_mode = False
+        if multi_mode:
+            aux = res.copy()
+            for key in aux.keys():
+                r = aux[key]
+                if 'model' in r and 'res_id' in r:
+                    if r.get('model') in ['account.move', 'account.withhold', 'account.remision']: # Se anade account remision para mostrar xml cuando se envia al correo
+                        class_model = self.env[r.get('model')]
+                        record = class_model.browse(r.get('res_id'))
+                        if record.xml_data_id and record.xml_data_id.state == 'authorized':
+                            try:
+                                file_name = xml_model.with_context(only_file=True).generate_file_name(record.xml_data_id.id, 'file_authorized')
+                                r['attachments'].append((file_name, record.xml_data_id.xml_authorized))
+                            except:
+                                _logger.warning("Can't get xml file")
+                res.update(r)
+        return res
+    
