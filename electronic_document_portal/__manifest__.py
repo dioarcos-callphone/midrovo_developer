7ac921470086283af2122ee7250225ec3f007d15
midrovom <midrovo@callphoneecuador.com>
2025-04-03 11:35:35 -0500

    fix-update:[midrovo] actualizando sync_authorized

--- electronic_document_portal/__manifest__.py
+++ electronic_document_portal/__manifest__.py
@@ -5,11 +5,25 @@
     'description': """
         - Modulo creado para Odoo 16 Community
         - Lista los documentos del cliente en el portal web
-        - Documentos disponibles: facturas, notas de crédito, notas de debito, liquidaciones de compras, guías de remisión y retenciones.
-        - Opción para descargar facturas en formato PDF y XML.
-        - Se mejora el diseño del mensaje de aviso cuando no hay documentos disponibles.
-        - Filtrar por punto de emisión para los usuarios internos.
-        - Mostrar el estado del documento electronico emitido por el SRI.
+        - Documentos disponibles:
+            - Facturas,
+            - Notas de crédito,
+            - Notas de débito,
+            - Liquidaciones de compras
+            - Guías de remisión
+            - Retenciones
+        - Descarga de documentos en formato PDF y XML.
+        - Mejoras en el diseño del portal.
+        - Configuración de punto de emision a usuarios internos.
+        - Visualización de estados del SRI y de EMAIL.
+        - Grupos de acceso:
+            - Eliminar documentos ya publicados (usuario interno)
+            - No eliminar documentos (usuario interno)
+            - Enviar e imprimir documentos (usuario interno)
+        - Mejora de las vistas form de los documentos.
+        - Enviar e imprimir con los documentos estándar del modulo ec_account_edi.
+        - Visualización de mensajes informativos del SRI y mejora de la pestaña Documentos electrónicos de los notebook FORM
+        - Replicación de métodos para enviar e imprimir y estado de enviado a email en account.remision y account.withhold
     """,
     'category': 'Electronic',
     'author': 'Mauricio Idrovo',
