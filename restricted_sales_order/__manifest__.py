e09894c5a1c1856f573d7aa08452669bf446bb7e
midrovom <midrovo@callphoneecuador.com>
2025-05-20 17:43:54 -0500

    fix-add: agg modulo para restringir el boton sale

---
+++ restricted_sales_order/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Restricted Sales Order',
+    'version': '16.0.1.0.0',
+    'description': '''
+        Modulo desarrollado para Odoo 16 Enterprise
+        Especificaciones:
+            - Oculta el boton de cancelar
+    ''',
+    'summary': 'Restricción del pedido de venta',
+    'author': 'Mauricio Idrovo',
+    'website': 'https://www.callphoneecuador.com',
+    'license': 'LGPL-3',
+    'category': 'sale',
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'auto_install': True,
+    'installable': True,
+    'application': False,
+}
