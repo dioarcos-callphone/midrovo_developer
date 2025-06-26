d9284b42b7eacbc2eeeef205fb06e3f8099c6d58
midrovom <midrovo@callphoneecuador.com>
2025-06-26 14:45:26 -0500

    fix-update: template mail enviado

--- electronic_document_portal/__manifest__.py
+++ electronic_document_portal/__manifest__.py
@@ -55,7 +55,7 @@
         'templates/liquidation_portal_template.xml',
         'templates/remission_portal_template.xml',
         'templates/retention_portal_template.xml',
-        'templates/button_filter_template.xml',
+        # 'templates/button_filter_template.xml',
         'views/user_extend_view.xml',
         'views/invoice_view.xml',
         'views/remission_view.xml',
